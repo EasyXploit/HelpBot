07ba81588c2ce857734f41e300d82d049a569d97
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-09-07 10:17:01 +0200

    Mejoras en el comando $md
    Se añade una etiqueta de "Característica experimental" al inicio de la ejecución del comando.
    
    Se crea un sistema que permite discriminar entre distintos tipos de mensajes en función de uno de los argumentos introducidos.
    
    Se crean mecanismos para controlar el orden de los argumentos introducidos.

--- commands/ownerCommands/md.js
+++ commands/ownerCommands/md.js
@@ -1,9 +1,27 @@
 exports.run = async (discord, fs, config, token, bot, message, args, command, roles, loggingChannel) => {
     
+    let experimentalEmbed = new discord.RichEmbed()
+        .setColor(0xC6C9C6)
+        .setDescription('❕ **Función experimental**\nEstá ejecutando una versión inestable del código de esta función, por lo que esta podría sufrir modificaciones o errores antes de su lanzamiento final.');
+    message.channel.send(experimentalEmbed);
+    
     try {
         let noMentionEmbed = new discord.RichEmbed()
             .setColor(0xF12F49)
             .setDescription('❌ No has mencionado un usuario válido');
+        
+        let noTypeAndToDMEmbed = new discord.RichEmbed()
+            .setColor(0xF12F49)
+            .setDescription('❌ No has proporcionado ni el tipo de mensaje ni contenido del mensaje');
+        
+        let noToDMEmbed = new discord.RichEmbed()
+            .setColor(0xF12F49)
+            .setDescription('❌ No has proporcionado el contenido del mensaje');
+        
+        let noCorrectSyntaxEmbed = new discord.RichEmbed()
+            .setColor(0xF12F49)
+            .setTitle('❌ Ocurrió un error')
+            .setDescription('La sintaxis del comando es `' + config.ownerPrefix + 'md (@usuario) (autor/anonimo) (mensaje a enviar)`');
         
         let noBotsEmbed = new discord.RichEmbed()
             .setColor(0xF12F49)
@@ -13,17 +31,35 @@ exports.run = async (discord, fs, config, token, bot, message, args, command, ro
             .setColor(0xB8E986)
             .setDescription('✅ ¡Mensaje enviado!');
         
+        if (!args[1]) return message.channel.send(noTypeAndToDMEmbed);
+        if (!args[2]) return message.channel.send(noToDMEmbed);
+        
         let user = message.mentions.users.first();
-        let toDeleteCount = command.length - 2 + args[0].length + 2; 
+        let toDeleteCount = command.length - 2 + args[0].length + 1 + args[1].length + 2; 
         let toDM = message.content.slice(toDeleteCount)
         
         if (!user) return message.channel.send(noMentionEmbed);
         if (user.bot) return message.channel.send(noBotsEmbed);
+        if (!args[0].startsWith('<@!')) return message.channel.send(noCorrectSyntaxEmbed);
         
-        let resultEmbed = new discord.RichEmbed()
-            .setAuthor('Mensaje de: ' + message.author.username, message.author.avatarURL)
-            .setColor(0xFFC857)
-            .setDescription(toDM);
+        let resultEmbed = 'null';
+        
+        switch (args[1]) {
+            case 'autor':
+                resultEmbed = new discord.RichEmbed()
+                    .setAuthor('Mensaje de: ' + message.author.username, message.author.avatarURL)
+                    .setColor(0xFFC857)
+                    .setDescription(toDM);
+                break;
+            case 'anonimo':
+                resultEmbed = new discord.RichEmbed()
+                    .setColor(0xFFC857)
+                    .setDescription(toDM);
+                break;
+            default:
+                return message.channel.send(noCorrectSyntaxEmbed);
+                break;
+        }
         
         try {
             await user.send(resultEmbed);

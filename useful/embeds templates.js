275727a03a7b4ddb302739719f719d51386c0b99
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-09-07 10:41:31 +0200

    Cambios en las plantillas
    Se incorpora el embed de 'embed template.js' en el archivo 'embeds templates.js', se traduce al castellano y se cambian algunos detalles para ajustarlo a las pautas de desarrollo actuales.

--- useful/embeds templates.js
+++ useful/embeds templates.js
@@ -1,5 +1,34 @@
 return;
 
+//EMBED COMPLETO
+let embed = new discord.RichEmbed()
+      .setTitle('Este es el título, y puede contener 256 carácteres')
+      .setAuthor('Nombre del autor', 'https://i.imgur.com/lm8s41J.png')
+      /*
+       * Alternativamente, usa "#00AE86", [0, 174, 134] o un número integral.
+       */
+      .setColor(0x00AE86)
+      .setDescription('Este es el cuerpo principal del texto, puede contener 2048 carácteres.')
+      .setFooter('Este es el texto del pie de página, puede contener 2048 caracteres', 'http://i.imgur.com/w1vhFSR.png')
+      .setImage('http://i.imgur.com/yVpymuV.png')
+      .setThumbnail('http://i.imgur.com/UoPJjLZ.png')
+      /*
+       * Toma un objeto de fecha, por defecto a la fecha actual.
+       */
+      .setTimestamp()
+      .setURL('https://discord.js.org/#/docs/main/indev/class/RichEmbed')
+      .addField('Este es el título de un campo, puede contener 256 carácteres',
+        'Este es el valor de un campo, puede contener 2048 carácteres.')
+      /*
+       * Los campos en línea pueden no mostrarse como en línea si la miniatura y/o la imagen son demasiado grandes.
+       */
+      .addField('Campo alineado', 'También pueden estar en línea.', true)
+      /*
+       * Campo en blanco, útil para crear espacio.
+       */
+      .addBlankField(true)
+      .addField('Campo alineado 3', 'Puedes tener un máximo de 25 campos.' true);
+
 //Comando deshabilitado
 let disabledEmbed = new discord.RichEmbed()
     .setColor(0xC6C9C6)

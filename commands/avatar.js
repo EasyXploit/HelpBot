abb94583a44081d8548f8bc2d98b71f17d506d93
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-09-10 12:38:47 +0200

    Cambios menores
    Se mejora el sistema de argumento del comando 'fortnite' para permitir que los nombres de usuario puedan contener espacios.
    
    Se añaden más estadísticas a mostrar en todos los modos de juego.
    
    Se cambia la thumbnail.
    
    Se añade información acerca de como utilizar a @Rythm 2 en el comando 'musica'

--- commands/avatar.js
+++ commands/avatar.js
@@ -5,7 +5,7 @@ exports.run = (discord, fs, config, keys, bot, message, args, command, roles, lo
     const embed = new discord.RichEmbed()
         .setTitle('URL del Avatar')
         .setAuthor('Avatar de @' + user.username)
-        .setFooter('© 2018 República Gamer LLC', bot.user.avatarURL)
+        .setFooter('© 2018 República Gamer LLC', 'https://i.imgur.com/jvOFaux.png')
         .setColor(0xFFC857)
         .setImage(user.displayAvatarURL)
         .setURL(user.displayAvatarURL);
--- commands/fortnite.js
+++ commands/fortnite.js
@@ -7,19 +7,30 @@ exports.run = (discord, fs, config, keys, bot, message, args, command, roles, lo
 
     const fortniteClient = require('fortnite');
     const fortnite = new fortniteClient(keys.fortniteApiKey);
+    
+    let fields = message.content.slice(11).split('" "');
+    let lastField = fields.slice(-1).join();
 
-    let username = args[0];
-    let gamemode = args[1];
-    let platform = args[2];
+    lastField = lastField.substring(0, lastField.length - 1);
+    fields.splice(-1);
+    fields.push(lastField);
+    
+    console.log(fields);
+
+    let username = fields[0];
+    let gamemode = fields[1];
+    let platform = fields[2];
+    
+    let thumbnail = 'https://toppng.com/public/uploads/preview/fortnite-battle-royale-logo-11530963305mex6keb5qj.png';
     
     let noCorrectSyntaxEmbed = new discord.RichEmbed()
             .setColor(0xF04647)
             .setTitle('❌ Ocurrió un error')
-            .setDescription('La sintaxis del comando es `' + config.prefix + 'fortnite (usuario) (solo/duo/squad/lifetime) (pc/xb1/psn)`');
+            .setDescription('La sintaxis del comando es `' + config.prefix + 'fortnite ("usuario") ("solo/duo/squad/lifetime") ("pc/xbl/psn")`');
     
     if (!username || !gamemode || !platform) return message.channel.send(noCorrectSyntaxEmbed);
     if (gamemode !== 'solo' && gamemode !== 'duo' && gamemode !== 'squad' && gamemode !== 'lifetime') return message.channel.send(noCorrectSyntaxEmbed);
-    if (platform !== 'pc' && platform !== 'xb1' && platform !== 'psn') return message.channel.send(noCorrectSyntaxEmbed);
+    if (platform !== 'pc' && platform !== 'xbl' && platform !== 'psn') return message.channel.send(noCorrectSyntaxEmbed);
     
     let data = fortnite.user(username, platform).then(data => {
         
@@ -32,21 +43,25 @@ exports.run = (discord, fs, config, keys, bot, message, args, command, roles, lo
             let kd = solostats.kd;
             let matches = solostats.matches;
             let kills = solostats.kills;
+            let kills_per_match = solostats.kills_per_match;
+            let score_per_match = solostats.score_per_match;
             let wins = solostats.wins;
-            let top3 = solostats.top_3;
+            let top_5 = solostats.top_5;
             
             let resultEmbed = new discord.RichEmbed()
                 .setColor(0x8A2BE2)
                 .setAuthor('Estadísticas de Fortnite', 'http://s2.googleusercontent.com/s2/favicons?domain_url=https://www.epicgames.com/fortnite/')
                 .setTitle('Modo de juego: SOLO')
-                .setDescription('Mostrando las estadísticas de ' + data.username)
-                .setThumbnail('https://fortnitetracker.com/Images/General/logo.png')
+                .setDescription('Mostrando las estadísticas de __' + data.username + '__ en __' + platform.toUpperCase() + '__')
+                .setThumbnail(thumbnail)
                 .addField('Puntuación', score, true)
                 .addField('Partidas jugadas', matches, true) 
                 .addField('Victorias', wins, true)
-                .addField('Top 3s', top3, true)
                 .addField('Bajas', kills, true)
                 .addField('Ratio B/M', kd, true)
+                .addField('Bajas por partida', kills_per_match, true)
+                .addField('Puntuación por partida', score_per_match, true)
+                .addField('Top 5', top_5 + ' veces', true)
             message.channel.send(resultEmbed);
         } else if (gamemode === 'duo') {
             let duostats = stats.duo;
@@ -55,21 +70,25 @@ exports.run = (discord, fs, config, keys, bot, message, args, command, roles, lo
             let kd = duostats.kd;
             let matches = duostats.matches;
             let kills = duostats.kills;
-            let wins = duostats.wins;
-            let top3 = duostats.top_3;
+            let kills_per_match = duostats.kills_per_match;
+            let score_per_match = duostats.score_per_match;
+            let wins = duostats.wins;   
+            let top_5 = duostats.top_5;
             
             let resultEmbed = new discord.RichEmbed()
                 .setColor(0x8A2BE2)
                 .setAuthor('Estadísticas de Fortnite', 'http://s2.googleusercontent.com/s2/favicons?domain_url=https://www.epicgames.com/fortnite/')
                 .setTitle('Modo de juego: DUO')
-                .setDescription('Mostrando las estadísticas de ' + data.username)
-                .setThumbnail('https://fortnitetracker.com/Images/General/logo.png')
+                .setDescription('Mostrando las estadísticas de __' + data.username + '__ en __' + platform.toUpperCase() + '__')
+                .setThumbnail(thumbnail)
                 .addField('Puntuación', score, true)
                 .addField('Partidas jugadas', matches, true) 
                 .addField('Victorias', wins, true)
-                .addField('Top 3s', top3, true)
                 .addField('Bajas', kills, true)
                 .addField('Ratio B/M', kd, true)
+                .addField('Bajas por partida', kills_per_match, true)
+                .addField('Puntuación por partida', score_per_match, true)
+                .addField('Top 5', top_5 + ' veces', true)
             message.channel.send(resultEmbed);
         } else if (gamemode === 'squad') {
             let squadstats = stats.squad;
@@ -78,6 +97,8 @@ exports.run = (discord, fs, config, keys, bot, message, args, command, roles, lo
             let kd = squadstats.kd;
             let matches = squadstats.matches;
             let kills = squadstats.kills;
+            let kills_per_match = squadstats.kills_per_match;
+            let score_per_match = squadstats.score_per_match;
             let wins = squadstats.wins;
             let top3 = squadstats.top_3;
             
@@ -85,14 +106,16 @@ exports.run = (discord, fs, config, keys, bot, message, args, command, roles, lo
                 .setColor(0x8A2BE2)
                 .setAuthor('Estadísticas de Fortnite', 'http://s2.googleusercontent.com/s2/favicons?domain_url=https://www.epicgames.com/fortnite/')
                 .setTitle('Modo de juego: SQUAD')
-                .setDescription('Mostrando las estadísticas de ' + data.username)
-                .setThumbnail('https://fortnitetracker.com/Images/General/logo.png')
+                .setDescription('Mostrando las estadísticas de __' + data.username + '__ en __' + platform.toUpperCase() + '__')
+                .setThumbnail(thumbnail)
                 .addField('Puntuación', score, true)
                 .addField('Partidas jugadas', matches, true) 
                 .addField('Victorias', wins, true)
-                .addField('Top 3s', top3, true)
                 .addField('Bajas', kills, true)
                 .addField('Ratio B/M', kd, true)
+                .addField('Bajas por partida', kills_per_match, true)
+                .addField('Puntuación por partida', score_per_match, true)
+                .addField('Top 3', top3 + ' veces', true)
             message.channel.send(resultEmbed);
         } else if (gamemode === 'lifetime') {
             let lifetime = stats.lifetime;
@@ -103,19 +126,29 @@ exports.run = (discord, fs, config, keys, bot, message, args, command, roles, lo
             let winsper = lifetime[9] ['Win%'];
             let kills = lifetime[10] ['Kills'];
             let kd = lifetime[11] ['K/d'];
+            let top3 = lifetime[1] ['Top 3s'];
+            
+            let footer = '';
+            
+            if (wins >= 1000) {
+                thumbnail = 'https://image.fnbr.co/misc/5ab282b5ca60ff23958a3e77/icon.png';
+                footer = '¡Vaya! Al parecer estás muy enganchado a este juego ...';
+            };
 
             let resultEmbed = new discord.RichEmbed()
                 .setColor(0x8A2BE2)
                 .setAuthor('Estadísticas de Fortnite', 'http://s2.googleusercontent.com/s2/favicons?domain_url=https://www.epicgames.com/fortnite/')
                 .setTitle('Modo de juego: LIFETIME')
-                .setDescription('Mostrando las estadísticas de ' + data.username)
-                .setThumbnail('https://fortnitetracker.com/Images/General/logo.png')
+                .setDescription('Mostrando las estadísticas de __' + data.username + '__ en __' + platform.toUpperCase() + '__')
+                .setThumbnail(thumbnail)
+                .setFooter(footer)
                 .addField('Puntuación', score, true)
                 .addField('Partidas jugadas', matches, true) 
                 .addField('Victorias', wins, true)
                 .addField('Victorias (%)', winsper, true)
                 .addField('Bajas', kills, true)
                 .addField('Ratio B/M', kd, true)
+                .addField('Top 3', top3 + ' veces', true)
             message.channel.send(resultEmbed);
         } else {
             message.channel.send(noCorrectSyntaxEmbed);

5f2564d2e9b1c888b8e90b52d5bbccce584f02a9
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-09-13 15:07:58 +0200

    Agradecimientos
    Se añaden agradecimientos al comando 'info'

--- commands/info.js
+++ commands/info.js
@@ -9,6 +9,7 @@ exports.run = (discord, fs, config, keys, bot, message, args, command, roles, lo
         .setURL('https://discord.gg/eWx72Jy')
         .addField('El proyecto', 'La República Gamer nació de la curiosidad. El objetivo era crear un pequeño servidor tan solo para un pocos amigos, pero al ver las posibilidades que Discord ofrecía, el servidor fue escalado hasta convertirse en la comunidad que conocemos hoy en día.', true)
         .addField('La comunidad', 'La **República Gamer** es una comunidad creada _por y para jugadores_.\nNuestro objetivo es darnos a conocer entre toda la comunidad gamer a fin de conectar a jugadores con otros jugadores. Realizamos sorteos, troneos y proporcionamos sistemas para conectar a los usuarios.', true)
-        .addField('PilkoBot', '**' + bot.user.username + '** es un bot multifuncional desarrollado por el Staff de la comunidad, cuyo uso es exclusivo de los usuarios de la **República Gamer**, por lo que no está permitido su uso fuera de los servidores administrador por la **República Gamer LLC**.\n\n_Para más información relativa a las funcionalidades de este bot, escribe `' + config.prefix + 'ayuda`_', true);
+        .addField('PilkoBot', '**' + bot.user.username + '** es un bot multifuncional desarrollado por el Staff de la comunidad, cuyo uso es exclusivo de los usuarios de la **República Gamer**, por lo que no está permitido su uso fuera de los servidores administrador por la **República Gamer LLC**.\n\n_Para más información relativa a las funcionalidades de este bot, escribe `' + config.prefix + 'ayuda`_', true)
+        .addField('Agradecimientos', '● Maria José Lozano\n● Amish Shah\n● El Pilko\n', true)
     message.channel.send(successEmbed);
 }

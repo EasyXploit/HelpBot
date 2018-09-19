d3c2fb786b98135e37a689565c9e9cf5c69fe96d
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-09-19 12:54:36 +0200

    Emojis globales
    Se crea el archivo 'emojis.js' que permite globalizar todos aquellos emojis personalizados que se declaren en este archivos, para que de esta forma se puedan sutilizar en todos los comandos.

---
+++ resources/emojis.js
@@ -0,0 +1,10 @@
+exports.run = (discord, bot) => {
+    
+    const GreenTick = bot.emojis.get('490135111912914964');
+    const GrayTick = bot.emojis.get('490135111589953537');
+    const RedTick = bot.emojis.get('490135111938080768');
+    
+    module.exports.GreenTick = GreenTick;
+    module.exports.GrayTick = GrayTick;
+    module.exports.RedTick = RedTick;
+}

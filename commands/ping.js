594979a17d9f65ec095e723969b9447c16e6ab26
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-09-21 22:09:13 +0200

    Ping con el websocket y uptime
    Ahora el comando 'ping' mostrará el tiempo de respuesta con el websocket, y también el tiempo de actividad del bot.

--- commands/ping.js
+++ commands/ping.js
@@ -1,24 +1,69 @@
-exports.run = (discord, fs, config, keys, bot, message, args, command, roles, loggingChannel, emojis) => {
+exports.run = async (discord, fs, config, keys, bot, message, args, command, roles, loggingChannel, emojis) => {
 
-    const ping = new Date().getTime() - message.createdTimestamp;
-
-    if (ping <= 180) {
-        let greenEmbed = new discord.RichEmbed()
-          .setTitle('Tiempo de respuesta: ')
+    //TIEMPO DE RESPUESTA DEL BOT
+    let botPing = new Date().getTime() - message.createdTimestamp;
+    let botPingEmbed;
+    
+    if (botPing <= 180) {
+        botPingEmbed = new discord.RichEmbed()
+          .setTitle('Tiempo de respuesta del bot: ')
           .setColor(0x7ED321)
-          .setDescription(':stopwatch: | ' + ping + ' ms');
-        message.channel.send(greenEmbed);
-    } else if (ping > 180 && ping <= 250) {
-        let orangeEmbed = new discord.RichEmbed()
-          .setTitle('Tiempo de respuesta: ')
+          .setDescription(':stopwatch: | ' + botPing + ' ms');
+    } else if (botPing > 180 && botPing <= 250) {
+        botPingEmbed = new discord.RichEmbed()
+          .setTitle('Tiempo de respuesta del bot: ')
           .setColor(0xF5A623)
-          .setDescription(':stopwatch: | ' + ping + ' ms');
-        message.channel.send(orangeEmbed);
+          .setDescription(':stopwatch: | ' + botPing + ' ms');
     } else {
-        let redEmbed = new discord.RichEmbed()
-          .setTitle('Tiempo de respuesta: ')
+        botPingEmbed = new discord.RichEmbed()
+          .setTitle('Tiempo de respuesta del bot: ')
           .setColor(0xF12F49)
-          .setDescription(':stopwatch: | ' + ping + ' ms');
-        message.channel.send(redEmbed);
+          .setDescription(':stopwatch: | ' + botPing + ' ms');
     }
+    
+    //TIEMPO DE RESPUESTA DEL WEBSOCKET
+    let websocketPing = Math.floor(bot.ping);
+    let websocketPingEmbed;
+    
+    if (websocketPing <= 180) {
+        websocketPingEmbed = new discord.RichEmbed()
+          .setTitle('Tiempo de respuesta del Websocket: ')
+          .setColor(0x7ED321)
+          .setDescription(':stopwatch: | ' + websocketPing + ' ms');
+    } else if (websocketPing > 180 && websocketPing <= 250) {
+        websocketPingEmbed = new discord.RichEmbed()
+          .setTitle('Tiempo de respuesta del Websocket: ')
+          .setColor(0xF5A623)
+          .setDescription(':stopwatch: | ' + websocketPing + ' ms');
+    } else {
+        websocketPingEmbed = new discord.RichEmbed()
+          .setTitle('Tiempo de respuesta del Websocket: ')
+          .setColor(0xF12F49)
+          .setDescription(':stopwatch: | ' + websocketPing + ' ms');
+    }
+    
+    //TIEMPO DE ACTIVIDAD
+    let uptimeEmbed;
+    
+    async function convertMS( milliseconds ) {
+        var day, hour, minute, seconds;
+        seconds = Math.floor(milliseconds / 1000);
+        minute = Math.floor(seconds / 60);
+        seconds = seconds % 60;
+        hour = Math.floor(minute / 60);
+        minute = minute % 60;
+        day = Math.floor(hour / 24);
+        hour = hour % 24;
+
+        uptimeEmbed = new discord.RichEmbed()
+            .setColor(0xFFC857)
+            .setTitle('Tiempo de actividad: ')
+            .setDescription(':stopwatch: | ' + day + ' días, ' + hour + ' horas, ' + minute + ' minutos y ' + seconds + ' segundos');
+    }
+    convertMS(bot.uptime);
+    
+    //ENVIO DE EMBEDS
+    await message.channel.send(botPingEmbed);
+    await message.channel.send(websocketPingEmbed);
+    await message.channel.send(uptimeEmbed);
 }

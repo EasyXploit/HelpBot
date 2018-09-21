e4a4255278f90a0e9e540b280cd9c2bfd8d78c9b
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-09-21 22:07:47 +0200

    Return if member.bot
    Se añaden líneas a los comandos 'silenciar' y 'dessilenciar' que impiden silenciar bots.
    
    Se añaden puntos y comas adicionales

--- commands/supervisorsCommands/silenciar.js
+++ commands/supervisorsCommands/silenciar.js
@@ -5,12 +5,18 @@ exports.run = async (discord, fs, config, keys, bot, message, args, command, rol
     let notToMuteEmbed = new discord.RichEmbed()
         .setColor(0xF12F49)
         .setDescription('❌ Debes mencionar a un miembro o escribir su id');
+    
+    let noBotsEmbed = new discord.RichEmbed()
+        .setColor(0xF12F49)
+        .setDescription('❌ No puedes silenciar  a un bot');
 
     //Esto comprueba si se ha mencionado a un usuario o se ha proporcionado su ID
     let member = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
     if (!member) return message.channel.send(notToMuteEmbed);
     let toDeleteCount = command.length - 2 + args[0].length + 2; 
-    let reason = message.content.slice(toDeleteCount) || 'Indefinida'
+    let reason = message.content.slice(toDeleteCount) || 'Indefinida';
+    
+    if (member.bot) return message.channel.send('noBotsEmbed');
 
     let role = message.guild.roles.find(r => r.name === 'Silenciado');
     if (!role) {
@@ -52,7 +58,7 @@ exports.run = async (discord, fs, config, keys, bot, message, args, command, rol
         .addField('Miembro', '<@' + member.id + '>', true)
         .addField('Moderador', '<@' + message.author.id + '>', true)
         .addField('Razón', reason, true)
-        .addField('Duración', '∞', true)
+        .addField('Duración', '∞', true);
     
     let toDMEmbed = new discord.RichEmbed()
         .setColor(0xEF494B)
@@ -60,9 +66,9 @@ exports.run = async (discord, fs, config, keys, bot, message, args, command, rol
         .setDescription('<@' + member.id + '>, has sido silenciado en ' + message.guild.name)
         .addField('Moderador', message.author.tag, true)
         .addField('Razón', reason, true)
-        .addField('Duración', '∞', true)
+        .addField('Duración', '∞', true);
     
-    if (member.roles.has(role.id)) return message.channel.send(alreadyMutedEmbed)
+    if (member.roles.has(role.id)) return message.channel.send(alreadyMutedEmbed);
     
     await member.addRole(role);
     await message.channel.send(successEmbed);

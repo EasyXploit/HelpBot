b2670dbe36104303425c0e8f8df6088f2b58feee
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-09-23 11:38:06 +0200

    Referencias a los nuevos comandos
    Se crean líneas para detallar el uso de los nuevos comandos '8ball', 'ppt', 'pptls', 'elige' y 'revés'.
    
    Además se corrige la sintaxis de otros comandos

--- commands/pilko.js
+++ commands/pilko.js
@@ -11,11 +11,16 @@ exports.run = (discord, fs, config, keys, bot, message, args, command, roles, lo
         .setFooter('© 2018 República Gamer LLC', bot.user.avatarURL)
         .setThumbnail('https://i.imgur.com/cTW63kf.png')
 
-        .addField('👦 ' + config.prefix + 'avatar',  'Muestra tu avatar o el de cualquier usuario.')
+        .addField('👦 ' + config.prefix + 'avatar <@usuario>',  'Muestra tu avatar o el de cualquier usuario.')
         .addField('🐈 ' + config.prefix + 'catfacts', 'Muestra un dato curioso sobre los gatos.')
         .addField('🐕 ' + config.prefix + 'dogfacts', 'Muestra un dato curioso sobre los perros.')
         .addField(':game_die: ' + config.prefix + 'dado', 'Lanzará un dado.')
         .addField(coin + ' ' + config.prefix + 'moneda', 'Lanzará una moneda.')
-        .addField('💭 ' + config.prefix + 'di', 'Hará que ' + bot.user.username + ' repita lo que escribas.')
+        .addField(':8ball: ' + config.prefix + '8ball <pregunta>', 'La bola mágica te dará una respuesta.')
+        .addField('✂ ' + config.prefix + 'ppt <piedra | papel | tijeras>', 'Juega a Piedra Papel y Tijeras con ' + bot.user.username + '.')
+        .addField('🦎 ' + config.prefix + 'pptls <piedra | papel | tijeras | lagarto | spock>', 'Juega a Piedra Papel, Tijeras, Lagarto y Spock con ' + bot.user.username + '.')
+        .addField('🎯 ' + config.prefix + 'elige "opción1" "opción2" ...', bot.user.username + ' elegirá por ti de entre las opciones que le facilites.')
+        .addField('📝 ' + config.prefix + 'reves <texto>', bot.user.username + ' le dará la vuelta al texto que especifiques.')
+        .addField('💭 ' + config.prefix + 'di <texto>', 'Hará que ' + bot.user.username + ' repita lo que escribas.')
     message.channel.send(successEmbed);
 }

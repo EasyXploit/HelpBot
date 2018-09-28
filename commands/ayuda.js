b27dda9b0b6f9d1e24d180ba319dfc7ffc09c7bf
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-09-29 01:35:52 +0200

    Manejador de errores, descripción de sintaxis y nuevas entradas
    Ahora el código se ejecuta bajo un bloque try con un bloque catch
    
    Se ha detallado la sintaxis del comando mediante un comentario
    
    Se han añadido referencias a comandos de InviteManager y PilkoBot

--- commands/ayuda.js
+++ commands/ayuda.js
@@ -1,22 +1,26 @@
-exports.run = (discord, fs, config, keys, bot, message, args, command, roles, loggingChannel, emojis) => {
-
-    const beta = bot.emojis.find('name', 'beta');   
-    const fortnite = bot.emojis.find('name', 'fortnite')
-
-    let embed = new discord.RichEmbed()
+exports.run = (discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, emojis) => {
+    
+    //!ayuda
+    
+    try {
+        let embed = new discord.RichEmbed()
+        .setColor(0xFFC857)
         .setAuthor('AYUDA', 'http://i.imgur.com/sYyH2IM.png')
         .setTitle('Sistema de ayuda del servidor')
-
-        .setColor(0xFFC857)
-        .setFooter('© 2018 República Gamer LLC', bot.user.avatarURL)
         .setThumbnail('http://i.imgur.com/sYyH2IM.png')
+        .setFooter('© 2018 República Gamer LLC', message.guild.iconURL)
 
         .addField(':grey_question: ' + config.prefix + 'normas', 'Muestra las normas del servidor.', true)
+        .addField(emojis.pilkobot + ' ' + config.prefix + 'pilko', 'Muestra los comandos de <@446041159853408257> ', true)
         .addField(':robot: ' + config.prefix + 'comandos', 'Muestra los comandos de los bots.', true)
-        .addField(':medal: ' + config.prefix + 'rangos', 'Muestra la lista de rangos ' + beta + '.', true)
-        .addField(fortnite + ' ' + config.prefix + 'fortnite ("usuario") ("solo/duo/squad/lifetime") ("pc/xbl/psn")', 'Muestra estadísticas de Fortnite Battle Royale.', true)
+        .addField(':medal: ' + config.prefix + 'rangos', 'Muestra la lista de rangos ' + emojis.beta + '.', true)
         .addField(':medal: !rank', 'Muestra tu rango en el servidor (o el de otro usuario).', true)
         .addField(':trophy: !levels', 'Muestra la tabla de clasificación del servidor.', true)
+        .addField(':ticket: +invites', 'Muestra a cuentas personas has invitado.', true)
+        .addField('📈 +leaderboard', 'Muestra la tabla de clasificación de invitaciones.', true)
         .addField(':stopwatch: ' + config.prefix + 'ping', 'Comprueba el tiempo de respuesta entre el cliente y ' + bot.user.username, true)
     message.channel.send(embed);
+    } catch (e) {
+        const handler = require(`../../errorHandler.js`).run(discord, config, bot, message, args, command, e);
+    }
 }

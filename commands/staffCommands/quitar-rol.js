5aef9f4ce145a1aabb3edf96dd0fc585927f668b
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-09-29 01:56:22 +0200

    Mejora del comando "quitar-rol"
    Se mejora el comando "quitar-rol" para permitir retirar roles de videojuegos, discriminar entre rangos, se simplifica la ejecución del código, se define la sintaxis y se añade el manejador de eventos

---
+++ commands/staffCommands/quitar-rol.js
@@ -0,0 +1,104 @@
+exports.run = async (discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, emojis, supervisorsRole, noPrivilegesEmbed) => {
+    
+    //-quitar-rol (@rol | "rol" | id) (@usuario | id)
+
+    let experimentalEmbed = new discord.RichEmbed()
+        .setColor(0xC6C9C6)
+        .setDescription(emojis.GrayTick + ' **Función experimental**\nEstá ejecutando una versión inestable del código de esta función, por lo que esta podría sufrir modificaciones o errores antes de su lanzamiento final.');
+    await message.channel.send(experimentalEmbed);
+    
+    try {
+        let noCorrectSyntaxEmbed = new discord.RichEmbed()
+            .setColor(0xF12F49)
+            .setDescription(emojis.RedTick + ' La sintaxis de este comando es `' + config.staffPrefix + 'quitar-rol (@rol | "rol" | id) (@usuario | id)`');
+
+        if (args.length < 2) return message.channel.send(noCorrectSyntaxEmbed);
+        
+        const guild = message.guild;
+        let role = message.mentions.roles.first() || message.guild.roles.get(args[0]);
+        if (!role) {
+            let newArgs = message.content.slice(13).split('" ').slice(0, 1).join();
+            role = message.guild.roles.find('name', newArgs)
+        }
+        let member = message.mentions.members.first() || message.guild.members.get(args[1]);
+        
+        if (!role || !member) return message.channel.send(noCorrectSyntaxEmbed);
+        
+        let author = message.guild.member(message.author.id)
+        
+        //Se comprueba si puede banear al usuario
+        if (author.id !== member.id && author.id !== message.guild.owner.id) {
+            if (author.highestRole.position <= member.highestRole.position) return message.channel.send(noPrivilegesEmbed)
+        }
+        
+        let notAssignedEmbed = new discord.RichEmbed()
+            .setColor(0xF12F49)
+            .setDescription(emojis.RedTick + ' Este usuario no dispone del rol `' + role.name + '`');
+        
+        if (!member.roles.has(role.id)) return message.channel.send(notAssignedEmbed);
+
+        if(message.author.id === config.botOwner) {
+
+            await member.removeRole(role);
+
+            let successEmbed = new discord.RichEmbed()
+                .setColor(0xB8E986)
+                .setTitle(emojis.GreenTick + ' Operación completada')
+                .setDescription('Retiraste el rol **' +  role.name + '** al usuario **' + member.displayName + '**.');
+
+            let loggingEmbed = new discord.RichEmbed()
+                .setColor(0x4A90E2)
+                .setTitle('📑 Auditoría')
+                .setDescription('Se ha retirado un rol.')
+                .setTimestamp()
+                .setFooter(bot.user.username, bot.user.avatarURL)
+                .addField('Fecha:', new Date().toUTCString(), true)
+                .addField('Emisor:', '<@' + message.author.id + '>', true)
+                .addField('Rol:', role.name, true)
+                .addField('Destino:', member.displayName, true)
+            
+            await message.channel.send(successEmbed);
+            await loggingChannel.send(loggingEmbed);
+
+            console.log('\n 》' + message.author.username + ' retiró el rol ' + role.name + ' al usuario ' + member.displayName + '.');
+            
+        } else if (message.member.roles.has(config.botStaff)) {
+            
+            let permittedRoles = ['ES', 'LATAM', 'CS:GO', 'RAINBOW SIX', 'FORTNITE', 'ROCKET LEAGUE', 'LOL', 'MINECRAFT', 'BATTLEFIELD', 'PUBG', 'GTA V', 'ROBLOX', 'OVERWATCH']
+            
+            if (message.member.roles.has(config.botSupervisor)) {permittedRoles.push('NOVATOS', 'INICIADOS', 'PROFESIONALES', 'VETERANOS', 'EXPERTOS')}
+    
+            if (permittedRoles.some(roleName => role.name === roleName)) {
+                await member.removeRole(role);
+            } else {
+                let noPrivilegesEmbed = new discord.RichEmbed()
+                    .setColor(0xF12F49)
+                    .setDescription(emojis.RedTick + ' ' + message.author.username + ', no dispones de privilegios suficientes para retirar este rol');
+                message.channel.send(noPrivilegesEmbed);
+            }
+
+            let successEmbed = new discord.RichEmbed()
+                .setColor(0xB8E986)
+                .setTitle(emojis.GreenTick + ' Operación completada')
+                .setDescription('Retiraste el rol ' +  role.name + ' al usuario **' + member.displayName + '**.');
+
+            let loggingEmbed = new discord.RichEmbed()
+                .setColor(0x4A90E2)
+                .setTimestamp()
+                .setFooter(bot.user.username, bot.user.avatarURL)
+                .setTitle('📑 Auditoría')
+                .setDescription('Se ha retirado un rol.')
+                .addField('Fecha:', new Date().toUTCString(), true)
+                .addField('Emisor:', '<@' + message.author.id + '>', true)
+                .addField('Rol:', role.name, true)
+                .addField('Destino:', member.displayName, true)
+            
+            await message.channel.send(successEmbed);
+            await loggingChannel.send(loggingEmbed);
+
+            console.log('\n 》' + message.author.username + ' retiró el rol ' + role.name + ' al usuario ' + member.displayName + '.');
+        }
+    } catch (e) {
+        const handler = require(`../../errorHandler.js`).run(discord, config, bot, message, args, command, e);
+    }
+}

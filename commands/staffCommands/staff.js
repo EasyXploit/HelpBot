e5b0c3f43a69b3d4e7b362ec064f6835d83ca8a8
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-09-29 01:32:30 +0200

    Manejo de errores, definición de sintaxis, notificación y adiciones
    Ahora el código se ejecuta bajo un bloque try con un bloque catch
    
    Se ha detallado la sintaxis del comando mediante un comentario
    
    Ahora se notificará al usuario de que ha rcibido un mensaje via MD, que se borrará pasado 1 segundo
    
    Se han añadido nuevas entradas para detallar la sintaxis de los nuevos comandos

--- commands/staffCommands/staff.js
+++ commands/staffCommands/staff.js
@@ -1,32 +1,41 @@
-exports.run = (discord, fs, config, keys, bot, message, args, command, roles, loggingChannel, emojis) => {
+exports.run = async (discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, emojis) => {
+    
+    //-staff
+    
+    try {
+        message.delete();
+        
+        let successEmbed = new discord.RichEmbed()
+            .setColor(0xB8E986)
+            .setDescription(emojis.GreenTick + ' ¡Te he enviado los detalles por Mensaje Directo!');
 
-    message.delete();
-
-    const pokeball = bot.emojis.find('name', 'pokeball');
-    const beta = bot.emojis.find('name', 'beta');
-
-    let successEmbed = new discord.RichEmbed()
-        .setAuthor('STAFF', 'http://i.imgur.com/l0EinOe.png')
-        .setTitle('Comandos para el Staff del servidor')
-
-        .setColor(0x1A936F)
-        .setFooter('© 2018 República Gamer LLC', bot.user.avatarURL)
-        .setThumbnail('http://i.imgur.com/l0EinOe.png')
-
-        .addField('🔄 ' + config.staffPrefix + 'reiniciar', 'Reinicia a ' + bot.user.username, true)
-        .addField(':stop_button: ' + config.supervisorsPrefix + 'detener', 'Detiene a ' + bot.user.username + ' :key:', true)
-        .addField('🕐 ' + config.staffPrefix + 'uptime', 'Muestra el tiempo que ' + bot.user.username + ' ha permanecido en línea', true)
-        .addField('🙍 ' + config.staffPrefix + 'infousuario <@usuario>', 'Muestra información acerca del usuario mencionado', true)
-        .addField('🖥 ' + config.staffPrefix + 'infoservidor', 'Muestra información acerca de la guild actual', true)
-        .addField('🔖 ' + config.staffPrefix + 'inforol <@rol/nombre/id>', 'Muestra información acerca de un rol', true)
-        .addField('🔈 ' + config.supervisorsPrefix + 'silenciar (@mención/id) (motivo)', 'Silencia a un usuario :key:', true)
-        .addField('🔊 ' + config.supervisorsPrefix + 'dessilenciar (@mención/id) (motivo)', 'Des-silencia a un usuario :key:', true)
-        .addField('🔧 ' + config.staffPrefix + 'herramientas', 'Muestra las herramientas de moderación que proporciona <@159985870458322944>', true)
-        .addField('🔖 ' + config.supervisorsPrefix + 'añadir-rol <@rol> <@usuario>', 'Añade un determinado rol a un determinado usuario :key:', true)
-        .addField('🔖 ' + config.supervisorsPrefix + 'quitar-rol <@rol> <@usuario>', 'Retira un determinado rol a un determinado usuario :key:', true)
-        .addField('💭 ' + config.staffPrefix + 'envia <mensaje>', 'Envía un mensaje desde ' + bot.user.username, true)
-        .addField('📊 ' + config.staffPrefix + 'encuesta "título" "campo1" "campo2" ...', 'Envia una encuesta al canal actual', true)
-        .addField('👁 ' + config.staffPrefix + 'registra (#canal) (xS/xM/xH)', 'Hará que ' + bot.user.username + ' registre los mensajes enviados durante el tiempo especificado.' + beta + '.')
-        .addField(pokeball + ' ' + config.staffPrefix + 'spawn <"url">', 'Spawnea un pokémon en base a la URL de imágen proporcionada', true);
-    message.author.send(successEmbed);
+        let helpEmbed = new discord.RichEmbed()
+            .setColor(0x1A936F)
+            .setThumbnail('http://i.imgur.com/l0EinOe.png')
+            .setAuthor('STAFF', 'http://i.imgur.com/l0EinOe.png')
+            .setTitle('Comandos para el Staff del servidor')
+            .setFooter('© 2018 República Gamer LLC | Llave: Solo para Supervisores', message.guild.iconURL)
+            .addField('🔄 ' + config.staffPrefix + 'reiniciar', 'Reinicia a ' + bot.user.username, true)
+            .addField(':stop_button: ' + config.staffPrefix + 'detener', 'Detiene a ' + bot.user.username + ' :key:', true)
+            .addField('🙍 ' + config.staffPrefix + 'infousuario (@usuario | id)', 'Muestra información acerca del usuario mencionado' + emojis.beta, true)
+            .addField('🖥 ' + config.staffPrefix + 'infoservidor', 'Muestra información acerca de la guild actual' + emojis.beta, true)
+            .addField('🔖 ' + config.staffPrefix + 'inforol (@rol | "rol" | id)', 'Muestra información acerca de un rol' + emojis.beta, true)
+            .addField('🔈 ' + config.staffPrefix + 'silenciar (@usuario | id) (motivo)', 'Silencia a un usuario :key:' + emojis.beta, true)
+            .addField('🔊 ' + config.staffPrefix + 'dessilenciar (@usuario | id) (motivo)', 'Des-silencia a un usuario :key:' + emojis.beta, true)
+            .addField('🔄 ' + config.staffPrefix + 'expulsar (@usuario | id)) (motivo)', 'Expulsa a un usuario :key:' + emojis.beta, true)
+            .addField('🛡 ' + config.staffPrefix + 'banear (@usuario | id) (motivo)', 'Banea a un usuario :key:' + emojis.beta, true)
+            .addField('🛡 ' + config.staffPrefix + 'desbanear (@usuario | id) (motivo)', 'Desbanea a un usuario :key:' + emojis.beta, true)
+            .addField('🔧 ' + config.staffPrefix + 'herramientas', 'Muestra las herramientas de moderación que proporciona <@159985870458322944>', true)
+            .addField('🔖 ' + config.staffPrefix + 'añadir-rol (@rol | "rol" | id) (@usuario | id)', 'Añade un determinado rol a un determinado usuario ' + emojis.beta, true)
+            .addField('🔖 ' + config.staffPrefix + 'quitar-rol (@rol | "rol" | id) (@usuario | id)', 'Retira un determinado rol a un determinado usuario ' + emojis.beta, true)
+            .addField('💭 ' + config.staffPrefix + 'envia <mensaje>', 'Envía un mensaje desde ' + bot.user.username, true)
+            .addField('📊 ' + config.staffPrefix + 'encuesta "título" "campo1" "campo2" ...', 'Envia una encuesta al canal actual', true)
+            .addField('👁 ' + config.staffPrefix + 'registra (#canal) (xS/xM/xH)', 'Hará que ' + bot.user.username + ' registre los mensajes enviados durante el tiempo especificado.' + emojis.beta + '.')
+            .addField(emojis.pokeball + ' ' + config.staffPrefix + 'spawn <"url">', 'Spawnea un pokémon en base a la URL de imágen proporcionada', true);
+        
+        message.channel.send(successEmbed).then(msg => {msg.delete(1000)});
+        message.author.send(helpEmbed);
+    } catch (e) {
+        const handler = require(`../../errorHandler.js`).run(discord, config, bot, message, args, command, e);
+    }
 }

23d058b7dfcb7ac54581307dcb3c0bda5b6d5016
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-09-29 01:21:34 +0200

    Nuevo manejador de errores
    Se implementa un nuevo manejador de errores que permite gestionar de forma efectiva cualquier incidencia que se produza en la ejecución de cualquier comando
    
    Este vuelca el error en la consola, y posteriormente muestra una notificación al ejecutor del comando y un mensaje en el canal de depuración definido.

---
+++ errorHandler.js
@@ -0,0 +1,42 @@
+exports.run = async (discord, config, bot, message, args, command, e) => {
+    
+    //Se declara el canal de depuración
+    const debuggingChannel = bot.channels.get(config.debuggingChannel);
+    
+    //Se declara el archivo de emojis
+    const emojis = require(`./resources/emojis.js`);
+    
+    //Se comprueba si se han proporcionado argumentos
+    let arguments;
+    if (args.length < 1) {
+        arguments = 'Ninguno';
+    } else {
+        arguments = args.join(' ');
+    }
+    
+    //Se muestra el error en consola
+    console.error('\n' + new Date().toUTCString() + ' 》' + e + '\n');
+
+    //Se muestra el error en el canal de depuración
+    let debuggEmbed = new discord.RichEmbed()
+        .setColor(0xCBAC88)
+        .setTimestamp()
+        .setFooter('© 2018 República Gamer LLC', bot.user.avatarURL)
+        .setTitle('📋 Depuración')
+        .setDescription('Se declaró un error durante la ejecución de un comando')
+        .addField('Comando:', command.slice(-0, -3), true)
+        .addField('Argumentos:', arguments, true)
+        .addField('Origen:', message.guild.name, true)
+        .addField('Canal:', message.channel, true)
+        .addField('Autor:', '<@' + message.author.id + '>', true)
+        .addField('Fecha:', new Date().toUTCString(), true)
+        .addField('Error:', e, true);
+    
+    let reportedEmbed = new discord.RichEmbed()
+        .setColor(0xF04647)
+        .setTitle(emojis.RedTick + '¡Vaya! Algo fue mal ...')
+        .setDescription('Lo hemos reportado al equipo de desarrollo');
+    
+    await message.channel.send(reportedEmbed);
+    await debuggingChannel.send(debuggEmbed);
+}

58e19f1284c40288d5b361929064046125f3f906
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-09-29 01:34:50 +0200

    Nuevo comando psychopass
    Se ha creado un nuevo comando llamado "psychopass" que "mide" el coeficiente criminal de un usuario
    
    Se prevee que este comando permanezca oculto

---
+++ commands/psychopass.js
@@ -0,0 +1,27 @@
+exports.run = (discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, emojis) => {
+    
+    //!psychopass (@usuario | nada)
+    
+    try {
+        const member = message.mentions.users.first() || message.member.displayName;
+        const coefficient = Math.floor(Math.random() * 400);
+        let result;
+
+        if (coefficient < 100) {
+            result = ' no es un criminal en potencia.'
+        } else if (coefficient >= 100 && coefficient <= 300) {
+            result = ' es un criminal que necesita ser paralizado.'
+        } else if (coefficient > 300) {
+            result = ' es un criminal que necesita ser ejecutado mortalmente.'
+        }
+
+        const resultEmbed = new discord.RichEmbed()
+            .setColor(0xDDDDDD)
+            .setTitle(':gun: | Coeficiente criminal: **' + coefficient + '%**')
+            .setDescription(member + result)
+
+        message.channel.send(resultEmbed);
+    } catch (e) {
+        const handler = require(`../../errorHandler.js`).run(discord, config, bot, message, args, command, e);
+    }
+}

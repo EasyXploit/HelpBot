bab9de6ec6dec3a693aa682f2b67786d23896855
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-09-29 01:46:18 +0200

    Simplificación de código, manejo de errores y definición de sintaxis
    Se simplifica la ejecución del código
    
    Ahora el código se ejecuta bajo un bloque try con un bloque catch
    
    Se ha detallado la sintaxis del comando mediante un comentario

--- commands/ownerCommands/depuracion.js
+++ commands/ownerCommands/depuracion.js
@@ -1,63 +1,48 @@
-exports.run = (discord, fs, config, keys, bot, message, args, command, roles, loggingChannel, emojis) => {
-
-    const debuggingChannel = bot.channels.get(config.debuggingChannel);
-
-    if(args.length >= 1) {
-        if (args.length <= 1) {
-            if (message.mentions.channels.first()) {
-
-                async function changeLoggingChannel() {
-                    try {
-                        let channelMention = args[0];
-                        channelMention = channelMention.substring(2, channelMention.length - 1);
-                        config.debugging = channelMention;
-                        await fs.writeFile('./config.json', JSON.stringify(config), (err) => console.error);
-
-                        let completedEmbed = new discord.RichEmbed()
-                            .setColor(0xB8E986)
-                            .setTitle('✅ Operación completada')
-                            .setDescription('Cambiaste el canal de depuración a ' + args[0]);
-                        message.channel.send(completedEmbed);
-
-                        let loggingEmbed = new discord.RichEmbed()
-                            .setColor(0x4A90E2)
-                            .setTimestamp()
-                            .setFooter(bot.user.username, bot.user.avatarURL)
-                            .setTitle('📑 Auditoría')
-                            .setDescription(message.author.username + ' cambió el canal de depuración a ' + args[0]);
-                        loggingChannel.send(loggingEmbed);
-                    } catch (e) {
-                        console.error(new Date().toUTCString() + ' 》' + e);
-                        let errorEmbed = new discord.RichEmbed()
-                            .setColor(0xF12F49)
-                            .setTitle('❌ Ocurrió un error')
-                            .addField('Se declaró el siguiente error durante la ejecución del comando:', e, true);
-                        message.channel.send(errorEmbed);
-                    }
-                }
-                changeLoggingChannel();
-            } else {
-                console.log (new Date().toUTCString() + ' 》' + message.author.username + ' no proporcionó un canal de texto válido para ejecutar el comando: ' + message.content + ' en ' + message.guild.name);
-
-                let errorEmbed = new discord.RichEmbed()
-                    .setColor(0xF12F49)
-                    .setDescription('❌ No has mencionado un canal de texto válido');
-                message.channel.send(errorEmbed);
-            }
-        } else {
-            console.log (new Date().toUTCString() + ' 》' + message.author.username + ' proporcionó demasiados argumentos para ejecutar el comando: ' + message.content + ' en ' + message.guild.name);
-
-            let errorEmbed = new discord.RichEmbed()
-                .setColor(0xF12F49)
-                .setDescription('❌ Proporcionaste demasiados argumentos.\nTan solo debes mencionar un canal de texto');
-            message.channel.send(errorEmbed);
-        }
-    } else {
-        console.log (new Date().toUTCString() + ' 》' + message.author.username + ' no proporcionó suficientes argumentos para ejecutar el comando: ' + message.content + ' en ' + message.guild.name);
-
-        let errorEmbed = new discord.RichEmbed()
+exports.run = async (discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, emojis) => {
+    
+    //depuracion (#canal | id)
+    
+    try {
+        let noCorrectSyntaxEmbed = new discord.RichEmbed()
             .setColor(0xF12F49)
-            .setDescription('❌ Debes mencionar un canal de texto');
-        message.channel.send(errorEmbed);
+            .setDescription(emojis.RedTick + ' La sintaxis de este comando es `' + config.prefix + 'depuracion (#canal | id)`');
+    
+        //Comprueba si se ha proporcionado el argumento
+        if (args.length < 1) return message.channel.send(noCorrectSyntaxEmbed);
+        
+        //Comprueba si se ha proporcionado un canal de texto válido (mención/id)
+        let channelMention = message.mentions.channels.first() || message.guild.channels.get(args[0]);
+        if (!channelMention) return message.channel.send(noCorrectSyntaxEmbed);
+        
+        //Almacena el ID del canal
+        let channel = channelMention.id;
+        
+        let alreadyConfiguredEmbed = new discord.RichEmbed()
+            .setColor(0xF12F49)
+            .setDescription(emojis.RedTick + ' Este canal de depuración ya ha sido configurado');
+        
+        //Comprueba si este canal ya está configurado
+        if (channel === config.debuggingChannel) return message.channel.send(alreadyConfiguredEmbed);
+
+        //Graba el nuevo canal de depuración 
+        config.debuggingChannel = channel;
+        await fs.writeFile('./config.json', JSON.stringify(config), (err) => console.error);
+
+        let completedEmbed = new discord.RichEmbed()
+            .setColor(0xB8E986)
+            .setTitle(emojis.GreenTick + ' Operación completada')
+            .setDescription('Cambiaste el canal de depuración a <#' + channel + '>');
+
+        let loggingEmbed = new discord.RichEmbed()
+            .setColor(0x4A90E2)
+            .setTimestamp()
+            .setFooter(bot.user.username, bot.user.avatarURL)
+            .setTitle('📑 Auditoría')
+            .setDescription(message.author.username + ' cambió el canal de depuración a <#' + channel + '>');
+        
+        await message.channel.send(completedEmbed);
+        await loggingChannel.send(loggingEmbed);
+    } catch (e) {
+        const handler = require(`../../errorHandler.js`).run(discord, config, bot, message, args, command, e);
     }
 }
--- commands/ownerCommands/nombre.js
+++ commands/ownerCommands/nombre.js
@@ -1,49 +1,45 @@
-exports.run = (discord, fs, config, keys, bot, message, args, command, roles, loggingChannel, emojis) => {
-
-    async function changeNickname() {
-        let nickname = bot.user.username;
-        nickname = args.join(' ');
+exports.run = async (discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, emojis) => {
+    
+    //$nombre (nombre)
 
         try {
-            console.log(new Date().toUTCString() + ' 》Se procederá a cambiar el  nombre de usuario ' + bot.user.username + ' a ' + nickname);
+            let noCorrectSyntaxEmbed = new discord.RichEmbed()
+                .setColor(0xF04647)
+                .setDescription(emojis.RedTick + ' La sintaxis del comando es `' + config.ownerPrefix + 'nombre (nombre)`');
+            
+            //Comprueba si se ha proporcionado argumento
+            if (args.length < 1) return message.channel.send(noCorrectSyntaxEmbed);
+            
+            //Se graba el nombre de usuario en una variable
+            let nickname = args.join(' '); //Se graba el nuevo username
+            
+            let actuallyConfiguredEmbed = new discord.RichEmbed()
+                .setColor(0xF12F49)
+                .setDescription(emojis.RedTick + ' Este nombre de usuario ya ha sido configurado');
+            
+            //Se comprueba si el nombre de usuario proporcionado es igual al ya configurado
+            if (nickname === bot.user.username) return message.channel.send(actuallyConfiguredEmbed);
+            
+            console.log('\n' + new Date().toUTCString() + ' 》Se procederá a cambiar el nombre de usuario ' + bot.user.username + ' a ' + nickname);
 
+            //Se cambia el username
             await bot.user.setUsername(nickname);
+              
+            let successEmbed = new discord.RichEmbed()
+                .setColor(0xB8E986)
+                .setTitle(emojis.GreenTick + ' Operación completada')
+                .setDescription('Cambiaste el nombre del bot a ' + nickname);
 
-            try {
-                if(bot.user.username === nickname) {
-                    let successEmbed = new discord.RichEmbed()
-                        .setColor(0xB8E986)
-                        .setTitle('✅ Operación completada')
-                        .setDescription('Cambiaste el nombre del bot a ' + nickname);
-                    message.channel.send(successEmbed);
+            let loggingEmbed = new discord.RichEmbed()
+                .setColor(0x4A90E2)
+                .setTimestamp()
+                .setFooter(bot.user.username, bot.user.avatarURL)
+                .setTitle('📑 Auditoría')
+                .setDescription(message.author.username + ' cambió el nombre del bot a ' + nickname);
 
-                    let loggingEmbed = new discord.RichEmbed()
-                        .setColor(0x4A90E2)
-                        .setTimestamp()
-                        .setFooter(bot.user.username, bot.user.avatarURL)
-                        .setTitle('📑 Auditoría')
-                        .setDescription(message.author.username + ' cambió el nombre del bot a ' + nickname);
-                    loggingChannel.send(loggingEmbed);
-                } else {
-                    message.channel.send('Hubo un problema durante el cambio de nombre: `nickname` no es igual a `bot.user.username`');
-                    return;
-                }
-            } catch (e) {
-            console.error(new Date().toUTCString() + ' 》' + e);
-
-            let errorEmbed = new discord.RichEmbed()
-                .setColor(0xF12F49)
-                .addField('❌ Se declaró el siguiente error durante la ejecución del comando:', e, true);
-            message.channel.send(errorEmbed);
-            }
+            await message.channel.send(successEmbed);
+            await loggingChannel.send(loggingEmbed);
         } catch (e) {
-            console.error(new Date().toUTCString() + ' 》' + e);
-
-            let errorEmbed = new discord.RichEmbed()
-                .setColor(0xF12F49)
-                .addField('❌ Se declaró el siguiente error durante la ejecución del comando:', e, true);
-            message.channel.send(errorEmbed);
+            const handler = require(`../../errorHandler.js`).run(discord, config, bot, message, args, command, e);
         }
-    }
-    changeNickname();
 }

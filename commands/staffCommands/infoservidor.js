9971592aa76a74fe816ffbfb1835b2e90b4d8a20
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-09-29 01:59:14 +0200

    Mejora del comando "infoservidor"
    Se mejora el comando "infoservidor" para mostrar los detalles faltantes, se simplifica la ejecución del código, se define la sintaxis y se añade el manejador de eventos

--- commands/staffCommands/infoservidor.js
+++ commands/staffCommands/infoservidor.js
@@ -1,13 +1,33 @@
-exports.run = (discord, fs, config, keys, bot, message, args, command, roles, loggingChannel, emojis) => {
+exports.run = (discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, emojis) => {
     
     let experimentalEmbed = new discord.RichEmbed()
         .setColor(0xC6C9C6)
-        .setDescription('❕ **Función experimental**\nEstá ejecutando una versión inestable del código de esta función, por lo que esta podría sufrir modificaciones o errores antes de su lanzamiento final.');
+        .setDescription(emojis.GrayTick + ' **Función experimental**\nEstá ejecutando una versión inestable del código de esta función, por lo que esta podría sufrir modificaciones o errores antes de su lanzamiento final.');
     message.channel.send(experimentalEmbed);
     
-    try {
-        
+    //-infoservidor
+    
+    try {    
         const guild = message.guild;
+        let minutes = (guild.afkTimeout / 60);
+        let large = 'No';
+        let verified;
+        
+        if (guild.large === true) {large = 'Si'};
+        if (guild.verified === true) {
+            verified = 'Si';
+        } else if (guild.verified === false) {
+            verified = 'No';
+        } else if (guild.verified === undefined) {
+            verified = 'No';
+        }
+        
+        let categories = new Set();
+        
+        let categoriesCount = guild.channels.filter(c => {
+            if (categories.has(c.parent)) return;
+            categories.add(c.parent);
+        });
         
         let resultEmbed = new discord.RichEmbed()
             .setColor(0xFFC857)
@@ -19,21 +39,16 @@ exports.run = (discord, fs, config, keys, bot, message, args, command, roles, lo
             .addField('Propietario', guild.owner + ' (ID: ' + guild.ownerID + ')', true)
             .addField('Fecha de creación', guild.createdAt.toUTCString(), true)
             .addField('Región', guild.region, true)
-            .addField('Canal de AFK', guild.afkChannel.name + '\nTimeout: ' + guild.afkTimeout + ' segundos', true)
-            .addField('Large guild (+250)', guild.large, true)
+            .addField('Canal de AFK', guild.afkChannel.name + '\nTimeout: ' + minutes + ' minutos', true)
+            .addField('Large guild (+250)', large, true)
             .addField('Nivel de verificación', guild.verificationLevel, true)
             .addField('Roles', guild.roles.size, true)
             .addField('Miembros', guild.memberCount + ' miembros\n' + guild.members.filter(m => m.user.presence.status == 'online').size + ' online\n' + guild.members.filter(m => m.user.bot).size + ' bots, ' + guild.members.filter(m => !m.user.bot).size + ' humanos', true)
-            .addField('Canales', guild.channels.filter(c => c.channels).size + ' canales\n'/* + guild.channels.filter(c => c.channels.type == 'text').size + ' de txto, ' + guild.channels.filter(c => c.channels.type == 'voice').size + ' de voz'*/, true)
+            .addField('Canales', guild.channels.size + ' canales en total:\n' + (categories.size - 1) + ' categorías\n' + guild.channels.filter(c => c.type === 'text').size + ' de texto, ' + guild.channels.filter(c => c.type === 'voice').size + ' de voz', true)
+            .addField('Verificado', verified, true)
             
         message.channel.send(resultEmbed);
-        
     } catch (e) {
-        console.error(new Date().toUTCString() + ' 》' + e);
-        let errorEmbed = new discord.RichEmbed()
-            .setColor(0xF12F49)
-            .setTitle('❌ Ocurrió un error')
-            .addField('Se declaró el siguiente error durante la ejecución del comando:', e, true);
-        message.channel.send(errorEmbed);
+        const handler = require(`../../errorHandler.js`).run(discord, config, bot, message, args, command, e);
     }
 }

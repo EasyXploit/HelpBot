b13f4317f3342184004c685e2c64de7e4ab6f484
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-09-29 21:38:44 +0200

    guildCreate mejorado
    Se mejora el diseño general de los embeds del evento guildCreate, se modifica su ubicación y se crea uno nuevo para dar las gracias al propietario de la guild que originó el evento

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,15 +1,58 @@
-exports.run = (event, discord, fs, config, keys, bot) => {
+exports.run = async (event, discord, fs, config, keys, bot, emojis) => {
     
-    event.leave();
+    if (event.id === '374945492133740544' || event.id === '430436498937217036') {
+        
+        console.log('\n' + new Date() + ' 》' + bot.user.username + ' se unió a la guild: ' + event.name + '\n')
+        
+        const debuggingChannel = bot.channels.get(config.debuggingChannel);
+        const guild = event;
+        
+        let large = 'No';
+        let verified;
+        
+        if (guild.large === true) {large = 'Si'};
+        if (guild.verified === true) {
+            verified = 'Si';
+        } else if (guild.verified === false) {
+            verified = 'No';
+        } else if (guild.verified === undefined) {
+            verified = 'No';
+        }
 
-    const loggingChannel = bot.channels.get(config.loggingChannel);
-
-    console.log(new Date() + ' 》' + bot.user.username + ' se unió a la guild: ' + event.name)
-
-    let embed = new discord.RichEmbed()
-        .setColor(0xF74022)
-        .setTimestamp()
-        .setFooter('© 2018 República Gamer LLC', bot.user.avatarURL)
-        .addField('📑 Auditoría', bot.user.username + ' se unió a la guild: **' + event.name + '**\nPor el momento, ' + bot.user.username + ' no está preparado para ser ejecutado en más de una guild, por lo que este podría no responder correctamente. Se recomienda que ' + bot.user.username + ' permaneza en un único servidor.');
-    loggingChannel.send(embed);
+        let debuggingEmbed = new discord.RichEmbed()
+            .setColor(0x7CD6F9)
+            .setThumbnail(guild.iconURL)
+            .setTimestamp()
+            .setAuthor(bot.user.username + ' ha sido añadido a una nueva guild', bot.user.displayAvatarURL)
+            .addField('Nombre', guild.name, true)
+            .addField('ID', guild.id, true)
+            .addField('Propietario', guild.owner + ' (ID: ' + guild.ownerID + ')', true)
+            .addField('Fecha de creación', guild.createdAt.toUTCString(), true)
+            .addField('Región', guild.region, true)
+            .addField('Large guild (+250)', large, true)
+            .addField('Miembros', guild.members.filter(m => !m.user.bot).size, true)
+            .addField('Verificado', verified, true)
+        
+        let thanksEmbed = new discord.RichEmbed()
+            .setColor(0xFFC857)
+            .setAuthor('¡Gracias por añadirme!', bot.user.displayAvatarURL)
+            .setDescription('¡Hola **' + event.owner.user.username + '**! Mi nombre es ' + bot.user.username + ', y he venido a ayudar en tu servidor, el que llaman **' + event.name + '**. Espero ser de tu agrado, a pesar de que soy una versión _beta_. Aprende a utilizarme usando el comando `!ayuda`')
+            .addField('⚙ Configuración', '`!configurar`', true)
+            .addField('📊 Estadísticas', bot.guilds.size + ' servidores\n' + bot.users.size + ' usuarios', true)
+            .addField('🔗 Añádeme', 'https://goo.gl/QAvgsQ', true)
+            .addField('💬 Servidor', 'https://discord.gg/eWx72Jy', true)
+            .addField('📣 Twitter', '___Aún no disponible___', true)
+            .setFooter('© 2018 República Gamer LLC', bot.user.avatarURL)
+        
+        await debuggingChannel.send(debuggingEmbed);
+        await event.owner.send(thanksEmbed)
+        
+    } else {
+        const cantJoinEmbed = new discord.RichEmbed()
+            .setColor(0xC6C9C6)
+            .setDescription(emojis.GrayTick + ' | Por el momento, ' + bot.user.username + ' solo está disponible en la República Gamer.');
+        
+        event.owner.send(cantJoinEmbed)
+        event.leave();
+    }
 }

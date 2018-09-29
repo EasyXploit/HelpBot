2be502765440e694bc7eb2f5e508a31d9bff1110
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-09-30 00:02:20 +0200

    Rediseño del comando "info"
    Se rediseña el comando "info" para mostrar detalles extra y se le aporta una nueva estética

--- commands/info.js
+++ commands/info.js
@@ -2,6 +2,8 @@ exports.run = (discord, fs, config, keys, bot, message, args, command, loggingCh
     
     //!info
     
+    const package = require('../package.json');
+    
     try {
         let resultEmbed = new discord.RichEmbed()
             .setColor(0XFFC857)
@@ -10,10 +12,15 @@ exports.run = (discord, fs, config, keys, bot, message, args, command, loggingCh
             .setURL('https://discord.gg/eWx72Jy')
             .setTitle('República Gamer LLC')
             .setFooter('©2018 República Gamer LLC', bot.user.avatarURL)
-            .addField('El proyecto', 'La República Gamer nació de la curiosidad. El objetivo era crear un pequeño servidor tan solo para un pocos amigos, pero al ver las posibilidades que Discord ofrecía, el servidor fue escalado hasta convertirse en la comunidad que conocemos hoy en día.', true)
-            .addField('La comunidad', 'La **República Gamer** es una comunidad creada _por y para jugadores_.\nNuestro objetivo es darnos a conocer entre toda la comunidad gamer a fin de conectar a jugadores con otros jugadores. Realizamos sorteos, troneos y proporcionamos sistemas para conectar a los usuarios.', true)
-            .addField('PilkoBot', '**' + bot.user.username + '** es un bot multifuncional desarrollado por el Staff de la comunidad, cuyo uso es exclusivo de los usuarios de la **República Gamer**, por lo que no está permitido su uso fuera de los servidores administrador por la **República Gamer LLC**.\n\n_Para más información relativa a las funcionalidades de este bot, escribe `' + config.prefix + 'ayuda`_', true)
-            .addField('Agradecimientos', '● Maria José Lozano\n● Amish Shah\n● El Pilko\n', true);
+            .addField('🧬 El proyecto', 'La República Gamer nació de la curiosidad. El objetivo era crear un pequeño servidor tan solo para un pocos amigos, pero al ver las posibilidades que Discord ofrecía, el servidor fue escalado hasta convertirse en la comunidad que conocemos hoy en día.', true)
+            .addField('👨‍👨‍👧 La comunidad', 'La **República Gamer** es una comunidad creada _por y para jugadores_.\nNuestro objetivo es darnos a conocer entre toda la comunidad gamer a fin de conectar a jugadores con otros jugadores. Realizamos sorteos, troneos y proporcionamos sistemas para conectar a los usuarios.', true)
+            .addField(emojis.pilkobot + ' PilkoBot', '**' + bot.user.username + '** es un bot multifuncional desarrollado por el Staff de la comunidad, cuyo uso es exclusivo de los usuarios de la **República Gamer**, por lo que no está permitido su uso fuera de los servidores administrador por la **República Gamer LLC**.\n\n_Para más información relativa a las funcionalidades de este bot, usa el comando `' + config.prefix + 'ayuda`_', true)
+            .addField('✉ Agradecimientos', '● Maria José Lozano\n● Amish Shah\n● El Pilko (Carlos)\n', true)
+            .addField('📊 Estadísticas', bot.guilds.size + ' servidores\n' + bot.users.size + ' usuarios\nV ' + package.version, true)
+            .addField('🔗 Añádeme', '___Aún no disponible___', true)
+            .addField('💬 Servidor', config.serverInvite, true)
+            .addField('📣 Twitter', '___Aún no disponible___', true)
+            .addField('🗂 Librerias', '● discord.js\n● flaticon', true);
         message.channel.send(resultEmbed);
     } catch (e) {
         const handler = require(`../../errorHandler.js`).run(discord, config, bot, message, args, command, e);

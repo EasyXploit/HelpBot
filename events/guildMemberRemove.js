4853b194f4845ea6490ea2862eb151d147b5a52e
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-10-01 14:09:54 +0200

    Tag en lugar de username
    Ahora se muestra el tag en lugar del nombre de usuario cuando el usuario que abandona la guild es una cuenta de usuario normal.

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -3,13 +3,13 @@ exports.run = (event, discord, fs, config, keys, bot, emojis) => {
     const loggingChannel = bot.channels.get(config.loggingChannel);
     
     if (!event.user.bot) {
-        console.log(new Date() + ' 》@' + event.user.username + ' abandonó la República Gamer');
+        console.log(new Date() + ' 》@' + event.user.tag + ' abandonó la República Gamer');
 
         let embed = new discord.RichEmbed()
             .setColor(0xDB904D)
             .setTimestamp()
             .setFooter('© 2018 República Gamer LLC', bot.user.avatarURL)
-            .addField('📑 Auditoría', '@' + event.user.username + ' abandonó la República Gamer ↗');
+            .addField('📑 Auditoría', '@' + event.user.tag + ' abandonó la República Gamer ↗');
         loggingChannel.send(embed);
     } else {
         if (event.user.id === bot.user.id) return;

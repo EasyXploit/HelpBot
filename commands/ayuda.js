10ab5e683fdcbc8e1a3ba16a68f2a5de73a94d87
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-10-01 14:13:02 +0200

    Menú navegable
    Se experimenta con un colector que permite cargar otros comandos en base a la primera reacción aportada por el usuario que invocó el comando original

--- commands/ayuda.js
+++ commands/ayuda.js
@@ -1,9 +1,11 @@
-exports.run = (discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, emojis) => {
+exports.run = async (discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, emojis) => {
     
     //!ayuda
     
     try {
-        let embed = new discord.RichEmbed()
+        const userID = message.author.id;
+        
+        let helpEmbed = new discord.RichEmbed()
         .setColor(0xFFC857)
         .setAuthor('AYUDA', 'http://i.imgur.com/sYyH2IM.png')
         .setTitle('Sistema de ayuda del servidor')
@@ -13,15 +15,54 @@ exports.run = (discord, fs, config, keys, bot, message, args, command, loggingCh
         .addField(':grey_question: ' + config.prefix + 'normas', 'Muestra las normas del servidor.', true)
         .addField(emojis.pilkobot + ' ' + config.prefix + 'pilko', 'Muestra los comandos de <@446041159853408257> ', true)
         .addField(':robot: ' + config.prefix + 'comandos', 'Muestra los comandos de los bots.', true)
-        .addField(':medal: ' + config.prefix + 'rangos', 'Muestra la lista de rangos ' + emojis.beta + '.', true)
         .addField(':medal: !rank', 'Muestra tu rango en el servidor (o el de otro usuario).', true)
         .addField(':trophy: !levels', 'Muestra la tabla de clasificación del servidor.', true)
         .addField(':ticket: +invites', 'Muestra a cuentas personas has invitado.', true)
         .addField('📈 +leaderboard', 'Muestra la tabla de clasificación de invitaciones.', true)
         .addField(':stopwatch: ' + config.prefix + 'ping', 'Comprueba el tiempo de respuesta entre el cliente y ' + bot.user.username, true)
         .addField('ℹ ' + config.prefix + 'info', 'Muestra información acerca del proyecto', true);
-    message.channel.send(embed);
+        
+    await message.channel.send(helpEmbed).then(async function (message) {
+        await message.react('❔');
+        await message.react('🤖');
+        await message.react('⏱');
+        await message.react('ℹ');
+
+        const filter = (reaction, user) => {
+            return ['❔', '🤖', '⏱', 'ℹ'].includes(reaction.emoji.name) && user.id === userID;
+        };
+
+        message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+            .then(async collected => {
+                const reaction = collected.first();
+
+                if (reaction.emoji.name === '❔') {
+                    await message.delete()
+                    
+                    let commandFile = require(`../commands/normas.js`).run(discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, emojis);
+                    
+                } else if (reaction.emoji.name === '🤖') {
+                    await message.delete()
+                    
+                    let commandFile = require(`../commands/comandos.js`).run(discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, emojis);
+                    
+                } else if (reaction.emoji.name === '⏱') {
+                    await message.delete()
+                    
+                    let commandFile = require(`../commands/ping.js`).run(discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, emojis);
+                    
+                } else if (reaction.emoji.name === 'ℹ') {
+                    await message.delete()
+                    
+                    let commandFile = require(`../commands/info.js`).run(discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, emojis);
+                    
+                }
+            })
+            .catch(collected => {
+                return;
+            });
+    });
     } catch (e) {
-        const handler = require(`../../errorHandler.js`).run(discord, config, bot, message, args, command, e);
+        const handler = require(`../errorHandler.js`).run(discord, config, bot, message, args, command, e);
     }
 }

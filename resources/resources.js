5ee69007a394973146d66993335490c48d57a9d2
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-10-04 00:06:11 +0200

    Cambio de nombre del recurso y reorganización
    Se cambia el nombre del recurso "emojis.js" a "resources.js" a fin de usarlo como recurso disponible para exportar gran cantidad de variables a las ejecuciones.

---
+++ resources/resources.js
@@ -0,0 +1,55 @@
+exports.run = (discord, bot) => {
+    
+    //SERVIDOR PRINCIPAL
+    const server = bot.guilds.get('374945492133740544');
+    module.exports.server = server;
+    
+    //EMOJIS
+    const GreenTick = bot.emojis.get('496633289726099478');
+    module.exports.GreenTick = GreenTick;
+    
+    const GrayTick = bot.emojis.get('496633289809854474');
+    module.exports.GrayTick = GrayTick;
+    
+    const RedTick = bot.emojis.get('496633289528836108');
+    module.exports.RedTick = RedTick;
+    
+    const pokeball = bot.emojis.get('496633450540040212');
+    module.exports.pokeball = pokeball;
+    
+    const beta = bot.emojis.get('496633935174828034');
+    module.exports.beta = beta;
+    
+    const fortnite = bot.emojis.get('496633644954419210');
+    module.exports.fortnite = fortnite;
+    
+    const pilkobot = bot.emojis.get('496633714802032655');
+    module.exports.pilkobot = pilkobot;
+    
+    const coin = bot.emojis.get('496634668758859786');
+    module.exports.coin = coin;
+    
+    const nitro = bot.emojis.get('496633448686157826');
+    module.exports.nitro = nitro;
+    
+    const verified = bot.emojis.get('496633324010471424');
+    module.exports.verified = verified;
+    
+    const chevron10 = bot.emojis.get('497133468535226389');
+    module.exports.chevron10 = chevron10;
+    
+    const chevron = bot.emojis.get('497133469110108189');
+    module.exports.chevron = chevron;
+    
+    const chevron5 = bot.emojis.get('497133468791341116');
+    module.exports.chevron5 = chevron5;
+    
+    const chevron9 = bot.emojis.get('497133468741009411');
+    module.exports.chevron9 = chevron9;
+    
+    const chevron15 = bot.emojis.get('497133469059645460');
+    module.exports.chevron15 = chevron15;
+    
+    const chevron18 = bot.emojis.get('497133469529538560');
+    module.exports.chevron18 = chevron18;
+}

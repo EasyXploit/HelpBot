b01a5a3c85256f891f542e465f93ecd184ec0f92
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-10-04 00:26:05 +0200

    Filtrado de nombres de usuario, regla de exclusión, recurso renombrado y otros cambios
    Se evalua si el nombre de usuario contiene una url.
    
    Se crea una regla provisional para impedir que el evento se dispare en el caso de que la guild que lo generó no fuese el servidor de la República Gamer.
    
    Se retira el anuncio del sorteo.
    
    Se retira una variable innecesaria.
    
    También se ha cambiado el nombre del recurso que contiene los emojis.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,48 +1,47 @@
-exports.run = async (event, discord, fs, config, keys, bot, emojis) => {
+exports.run = async (event, discord, fs, config, keys, bot, resources) => {
+    
+    //Previene que continue la ejecución si el servidor no es la República Gamer
+    if (event.guild.id !== '374945492133740544') return;
     
     const loggingChannel = bot.channels.get(config.loggingChannel);
     const welcomeChannel = bot.channels.get(config.welcomeChannel);
     const welcomeRole = event.guild.roles.find('name', 'NOVATOS');
-    
-    //Previene que continue la ejecución si el servidor no es la República Gamer
-    if (event.guild.id !== '374945492133740544') return;
 
     if (!event.user.bot) {
-        
-        const beta = bot.emojis.find('name', 'beta');
+
 
         var images = ['https://i.imgur.com/lDuMuSb.png', 'https://i.imgur.com/MJvm6NC.png', 'https://i.imgur.com/nPsX8TS.png', 'https://i.imgur.com/0dNTAZn.png', 'https://i.imgur.com/jiPcHl5.png', 'https://i.imgur.com/MQzdNf3.png', 'https://i.imgur.com/Z8MUU6q.png', 'https://i.imgur.com/XhHJfq0.png', 'https://i.imgur.com/O8TfwdN.png', 'https://i.imgur.com/sWbMJK2.png', 'https://i.imgur.com/ex700S4.png', 'https://i.imgur.com/GsLLEng.png'];
 
-        var welcomes = ['Hola <@' + event.user.id + '> 🎇, bienvenid@ a la **República Gamer**. Recuerda leer la <#426464733764386828>\nPor lo demás, ¡Pásalo bien :wink:!  ' + beta, 'Hola <@' + event.user.id + '> ⚡, bienvenido a la comunidad. Esperamos que lo pases bien, pero antes de nada, recuerda echar un vistazo a la <#426464733764386828> 🔥 ' + beta, 'Hola <@' + event.user.id + '> 🎊, ¡gracias por entrar a formar parte de nuestra comunidad!. Antes de continuar, recuerda leer la <#426464733764386828>. ¡Cada vez somos más! 🏆 ' + beta, 'Hola <@' + event.user.id + '> 🏅, bienvenid@ a la **República Gamer**. Recuerda leer la <#426464733764386828>, ¡y no te olvides de invitar a tus amigos!\nPor lo demás, ¡Pásalo bien 🎮!  ' + beta]
-
-        const newUser = event.user.username;
+        var welcomes = ['Hola <@' + event.user.id + '> 🎇, bienvenid@ a la **República Gamer**. Recuerda leer la <#426464733764386828>\nPor lo demás, ¡Pásalo bien :wink:!  ' + resources.beta, 'Hola <@' + event.user.id + '> ⚡, bienvenido a la comunidad. Esperamos que lo pases bien, pero antes de nada, recuerda echar un vistazo a la <#426464733764386828> 🔥 ' + resources.beta, 'Hola <@' + event.user.id + '> 🎊, ¡gracias por entrar a formar parte de nuestra comunidad!. Antes de continuar, recuerda leer la <#426464733764386828>. ¡Cada vez somos más! 🏆 ' + resources.beta, 'Hola <@' + event.user.id + '> 🏅, bienvenid@ a la **República Gamer**. Recuerda leer la <#426464733764386828>, ¡y no te olvides de invitar a tus amigos!\nPor lo demás, ¡Pásalo bien 🎮!  ' + resources.beta]
         
-        if (newUser.startsWith('discord.gg/')) {
+        const prohibitedNames = ['http://', 'https://', 'discord.gg/', 'www.'];
+        
+        if(prohibitedNames.some(word => event.user.username.toLowerCase().includes(word))) {
             
-            event.guild.member(event.user).ban('No está permitido utilizar invitaciones de Discord como nombre de usuario')
+            event.guild.member(event.user).ban('No está permitido utilizar enlaces como nombre de usuario')
 
             .catch ((err) => {
                 console.error(new Date() + ' 》' + err);
 
                 let errorEmbed = new discord.RichEmbed()
                     .setColor(0xF12F49)
-                    .setTitle('❌ Ocurrió un error')
+                    .setTitle(resources.RedTick + ' Ocurrió un error')
                     .setDescription('Ocurrió un error durante la ejecución del evento ' + event + '\nEl usuario ' + event.user.username + ' no fue expulsado automáticamente de la comunidad, por lo que será necesario emprender acciones de forma manual.');
                 loggingChannel.send(errorEmbed);
             })
             
-            console.log(new Date() + ' 》@' + event.user.username + ' intentó unirse a la República Gamer, pero fue baneado por que no está permitido utilizar invitaciones de Discord como nombre de usuario')
+            console.log(new Date() + ' 》@' + event.user.username + ' intentó unirse a la República Gamer, pero fue baneado por que no está permitido utilizar enlaces como nombre de usuario')
 
             let preventAccessEmbed = new discord.RichEmbed()
                 .setColor(0xF12F49)
                 .setTimestamp()
                 .setFooter('© 2018 República Gamer LLC', bot.user.avatarURL)
-                .addField('📑 Auditoría', '@' + event.user.username + ' intentó unirse a la República Gamer, pero fue baneado por que no está permitido utilizar invitaciones de Discord como nombre de usuario');
+                .addField('📑 Auditoría', '@' + event.user.username + ' intentó unirse a la República Gamer, pero fue baneado por que no está permitido utilizar enlaces como nombre de usuario');
             loggingChannel.send(preventAccessEmbed);
             
         } else  {
 
-            console.log(new Date() + ' 》@' + event.user.username + ' se unió a la República Gamer')
+            console.log(new Date() + ' 》@' + event.user.username + ' se unió a la guild: ' + event.guild.name)
 
             event.guild.member(event.user).addRole(welcomeRole);
 
@@ -52,14 +51,12 @@ exports.run = async (event, discord, fs, config, keys, bot, emojis) => {
 
                 .setDescription(welcomes[Math.floor(Math.random() * welcomes.length)])
                 .setThumbnail(images[Math.floor(Math.random() * images.length)]);
-            welcomeChannel.send(channelWelcomeEmbed);
 
             let loggingWelcomeEmbed = new discord.RichEmbed()
                 .setColor(0x98E646)
                 .setTimestamp()
                 .setFooter('© 2018 República Gamer LLC', bot.user.avatarURL)
-                .addField('📑 Auditoría', '@' + event.user.tag + ' se unió a la República Gamer ↙');
-            loggingChannel.send(loggingWelcomeEmbed);
+                .addField('📑 Auditoría', '@' + event.user.tag + ' se unió al servidor ↙');
 
             let dmWelcomeEmbed = new discord.RichEmbed()
                 .setColor(0xFFC857)
@@ -67,14 +64,11 @@ exports.run = async (event, discord, fs, config, keys, bot, emojis) => {
                 .setFooter('© 2018 República Gamer LLC', bot.user.avatarURL)
                 .setTitle('Hola **' + event.user.username + '**, bienvenido a la __República Gamer__ :tada:')
                 .setDescription('**¡Nos alegra que hayas decidido unirte a nuestra comunidad!**\nA continuación, te mostramos una breve guía sobre como empezar a participar en nuestro servidor. __¡Esperamos que lo pases bien!__')
-                .addField('Guía de inicio rápido:', ':one: Entra en <#426464733764386828> y dedica unos segundos a leer las breves normas que rigen nuestra comunidad. Además, aprenderás a usar a los bots, a como obtener ayuda y a como subir de nivel.\n:two: Entra en <#440905255073349635> y elige los roles de tu preferencia. Esto desbloqueará catacterísticas especiales para determinados videojuegos. ' + beta + '\n:three: Entra en <#388699973866225676> y escribe `/create` para crear ¡tu propia sala temporal! (recuerda que desparecerá si no hay nadie en ella).\n:four: ¡Tan solo diviértete y trae a tus amigos para que nos conozcan! Mándales este enlace de invitación: https://discord.gg/eWx72Jy', true);
+                .addField('Guía de inicio rápido:', ':one: Entra en <#426464733764386828> y dedica unos segundos a leer las breves normas que rigen nuestra comunidad. Además, aprenderás a usar a los bots, a como obtener ayuda y a como subir de nivel.\n:two: Entra en <#440905255073349635> y elige los roles de tu preferencia. Esto desbloqueará catacterísticas especiales para determinados videojuegos. ' + resources.beta + '\n:three: Entra en <#388699973866225676> y escribe `/create` para crear ¡tu propia sala temporal! (recuerda que desparecerá si no hay nadie en ella).\n:four: ¡Tan solo diviértete y trae a tus amigos para que nos conozcan! Mándales este enlace de invitación: https://discord.gg/eWx72Jy', true);
             
-            let giveawayEmbed = new discord.RichEmbed()
-                .setColor(0xFFC857)
-                .setDescription(':information_source: **NUEVO SORTEO** :tada: :confetti_ball:\n__¡Hay disponible un nuevo sorteo de 1.000 paVos de Fortnite!.__\nAverigua como participar en <#494050354418417675> ');
-            
+            await welcomeChannel.send(channelWelcomeEmbed);
+            await loggingChannel.send(loggingWelcomeEmbed);
             await event.user.send(dmWelcomeEmbed);
-            await event.user.send(giveawayEmbed);
         }
     } else {
         let loggingWelcomeBotEmbed = new discord.RichEmbed()

b210d297b937dc58636b47d1e34d9174bfe79684
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-10-04 00:28:05 +0200

    Corrección de errores y recurso renombrado
    Se corrigen errores que impedían la correcta ejecución del comando.
    
    También se ha cambiado el nombre del recurso que contiene los emojis.

--- commands/staffCommands/desbanear.js
+++ commands/staffCommands/desbanear.js
@@ -1,9 +1,9 @@
-exports.run = async (discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, emojis, supervisorsRole, noPrivilegesEmbed) => {
+exports.run = async (discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, resources, supervisorsRole, noPrivilegesEmbed) => {
     
     let experimentalEmbed = new discord.RichEmbed()
         .setColor(0xC6C9C6)
-        .setDescription(emojis.GrayTick + ' **Función experimental**\nEstá ejecutando una versión inestable del código de esta función, por lo que esta podría sufrir modificaciones o errores antes de su lanzamiento final.');
-    message.channel.send(experimentalEmbed);
+        .setDescription(resources.GrayTick + ' **Función experimental**\nEstás ejecutando una versión inestable del código de esta función, por lo que esta podría sufrir modificaciones o errores antes de su lanzamiento final.');
+    await message.channel.send(experimentalEmbed).then(msg => {msg.delete(5000)});
     
     //-desbanear (id) (motivo)
     
@@ -12,15 +12,15 @@ exports.run = async (discord, fs, config, keys, bot, message, args, command, log
 
         let notToUnbanEmbed = new discord.RichEmbed()
             .setColor(0xF12F49)
-            .setDescription(emojis.RedTick + ' Debes mencionar a un miembro o escribir su id');
+            .setDescription(resources.RedTick + ' Debes escribir el id del miembro a desbanear');
 
         let noReasonEmbed = new discord.RichEmbed()
             .setColor(0xF12F49)
-            .setDescription(emojis.RedTick + ' Debes proporcionar un motivo');
+            .setDescription(resources.RedTick + ' Debes proporcionar un motivo');
 
         //Esto comprueba si se ha mencionado a un usuario o se ha proporcionado su ID
-        let user = args[0];
-        if (!user) return message.channel.send(notToUnbanEmbed);
+        let user = await message.mentions.users.first() || await bot.fetchUser(args[0]);
+        if (!user) return message.channel.send(notToBanEmbed);
 
         let toDeleteCount = command.length - 2 + args[0].length + 2;
 
@@ -32,27 +32,36 @@ exports.run = async (discord, fs, config, keys, bot, message, args, command, log
 
         let successEmbed = new discord.RichEmbed()
             .setColor(0xB8E986)
-            .setTitle(emojis.GreenTick + ' Operación completada')
+            .setTitle(resources.GreenTick + ' Operación completada')
             .setDescription('El usuario ' + user.tag + ' ha sido desbaneado');
 
         let loggingEmbed = new discord.RichEmbed()
             .setColor(0x3EB57B)
             .setAuthor(user.tag + ' ha sido DESBANEADO', user.displayAvatarURL)
-            .addField('Miembro', '<@' + user + '>', true)
+            .addField('Usuario', '@' + user.tag, true)
             .addField('Moderador', '<@' + message.author.id + '>', true)
             .addField('Razón', reason, true)
 
-        let toDMEmbed = new discord.RichEmbed()
-            .setColor(0x3EB57B)
-            .setAuthor('[DESBANEADO]', message.guild.iconURL)
-            .setDescription('<@' + user + '>, has sido desbaneado en ' + message.guild.name)
-            .addField('Moderador', message.author.tag, true)
-            .addField('Razón', reason, true)
-
         await loggingChannel.send(loggingEmbed);
-        await user.send(toDMEmbed);
         await message.channel.send(successEmbed);
     } catch (e) {
-        const handler = require(`../../errorHandler.js`).run(discord, config, bot, message, args, command, e);
+        if (e.toString().includes('Unknown Ban')) {
+            let notBannedEmbed = new discord.RichEmbed()
+                .setColor(0xF12F49)
+                .setDescription(resources.RedTick + ' Este usuario no ha sido baneado');
+            message.channel.send(notBannedEmbed);
+        } else if (e.toString().includes('Invalid Form Body')) {
+            let notBannedEmbed = new discord.RichEmbed()
+                .setColor(0xF12F49)
+                .setDescription(resources.RedTick + ' Si el usuario no está en el servidor, has de especificar su ID');
+            message.channel.send(notBannedEmbed);
+        }  else if (e.toString().includes('Unknown User')) {
+            let notBannedEmbed = new discord.RichEmbed()
+                .setColor(0xF12F49)
+                .setDescription(resources.RedTick + ' El ID proporcionado no es válido');
+            message.channel.send(notBannedEmbed);
+        } else {
+            let handler = require(`../../errorHandler.js`).run(discord, config, bot, message, args, command, e);
+        }
     }
 }

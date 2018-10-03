8acac6d86de8faf050622f72ff771ade39cb5ed7
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-10-04 01:37:12 +0200

    Comando "rangos" finalizado
    Se finaliza el comando "rangos", aportándole una nueva estética y actualizando el contenido del mensaje

--- commands/rangos.js
+++ commands/rangos.js
@@ -1,21 +1,30 @@
-exports.run = async (discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, emojis) => {
+exports.run = async (discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, resources) => {
     
     //!rangos
     
     try {
-        let experimentalEmbed = new discord.RichEmbed()
-            .setColor(0xC6C9C6)
-            .setDescription('❕ **Función experimental**\nEstá ejecutando una versión inestable del código de esta función, por lo que esta podría sufrir modificaciones o errores antes de su lanzamiento final.');
-        await message.channel.send(experimentalEmbed);
-
-        let helpEmbed = new discord.RichEmbed()
+        let helpEmbed1 = new discord.RichEmbed()
             .setColor(16762967)
-            .setThumbnail('https://i.imgur.com/IXN6UT2.png')
-            .setAuthor('RANGOS', 'https://i.imgur.com/IXN6UT2.png')
-            .setTitle('Rangos del servidor')
-            .setDescription('**Recompensas por participación:** ' + emojis.beta + '\nLos usuarios que participan __activamente__ en la comunidad adquieren puntos de **EXP**, y al alcanzar determinados niveles se obtienen rangos que aportan la siguientes _ventajas_:\n\n● Nivel **0**  ‣ **NOVATOS**: Es el primer rol que recibes al unirte a la comunidad.\n● Nivel **3**  ‣ **INICIADOS**: Permite usar emojis externos' + emojis.nitro + ' y acceder a sorteos públicos\n● Nivel **5**  ‣ **PROFESIONALES**: Permite adjuntar archivos.\n● Nivel **8**  ‣ **VETERANOS**: Permite publicar en <#472491041602404362>  y cambiar tu propio apodo.\n● Nivel **10**  ‣ **EXPERTOS**: Te permite mencionar a todos y controlar la música.\n● **SUPPORTERS**: Demuestra tu apoyo invitando a 5 personas (más info con el comando !comando).\n\nUsa `!rank` para conocer tu nivel, y `!levels` para ver la tabla de clasificación.\n\n```El sistema de recompensas se encuentra en desarrollo.\nPronto añadiremos más ventajas a aquellos que sean ascendidos.```\n\n► **RECUERDA ELEGIR TUS ROLES EN #🔖▸roles** ◄  _(deshabilitado temporalmente)_ Para ver los roles autoasignables, escrib el comando !comando')
+            .setThumbnail('https://i.imgur.com/vDgiPwT.png')
+            .setAuthor('RANGOS', 'https://i.imgur.com/vDgiPwT.png')
+            .setDescription('Los usuarios que participan __activamente__ en la comunidad adquieren puntos de **EXP**, y al alcanzar determinados niveles se obtienen rangos que aportan la siguientes _ventajas_:')
+            .addField(resources.chevron + ' Nivel 0 ‣ NOVATOS', 'Es el primer rol que recibes al unirte a la comunidad.', true)
+            .addField(resources.chevron5 + ' Nivel 3 ‣ INICIADOS', 'Permite usar emojis externos' + resources.nitro + ' y acceder a sorteos públicos.', true)
+            .addField(resources.chevron9 + ' Nivel 5 ‣ PROFESIONALES', 'Permite publicar en <#472491041602404362> y en <#488662764081119233>.', true)
+            .addField(resources.chevron15 + ' Nivel 8 ‣ VETERANOS', 'Permite **adjuntar archivos** y **cambiar tu propio apodo**.', true)
+            .addField(resources.chevron18 + ' Nivel 10 ‣ EXPERTOS', 'Te permite **mencionar a todos** y **controlar la música**.', true)
+            .setFooter('Usa !rank para conocer tu nivel, y !levels para ver la tabla de clasificación.', resources.server.iconURL);
+        
+        let helpEmbed2 = new discord.RichEmbed()
+            .setColor(16762967)
+            .setThumbnail('https://i.imgur.com/TU8U8wq.png')
+            .setAuthor('ROLES ASIGNABLES (deshabilitado temporalmente)', 'https://i.imgur.com/TU8U8wq.png')
+            .addField('● ¿Que son?', 'Los roles asignables te permiten añadir __tus propios roles__ basados en tus **intereses**, **videojuegos** y **región**.', true)
+            .addField('● ¿Como los uso?', 'Desde el canal <#440905255073349635>, puedes reaccionar al mensaje de configuración con los emojis correspondientes a los roles que quieres asignarte', true)
             .setFooter('© 2018 República Gamer LLC', message.guild.iconURL);
-        message.channel.send(helpEmbed);
+        
+        await message.channel.send(helpEmbed1);
+        await message.channel.send(helpEmbed2);
     } catch (e) {
         const handler = require(`../errorHandler.js`).run(discord, config, bot, message, args, command, e);
     }

cff61c178416d73a0dc770bafcca1b67f8a2050e
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-10-04 00:27:02 +0200

    Pre-baneo y recurso renombrado
    Se mejora el comando "banear" para permitir banear usuario que no se encuentran en la guild.
    
    También se ha cambiado el nombre del recurso que contiene los emojis.

--- commands/staffCommands/banear.js
+++ commands/staffCommands/banear.js
@@ -1,9 +1,9 @@
-exports.run = async (discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, emojis, supervisorsRole, noPrivilegesEmbed) => {
+exports.run = async (discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, resources, supervisorsRole, noPrivilegesEmbed) => {
     
     let experimentalEmbed = new discord.RichEmbed()
         .setColor(0xC6C9C6)
-        .setDescription(emojis.GrayTick + ' **Función experimental**\nEstá ejecutando una versión inestable del código de esta función, por lo que esta podría sufrir modificaciones o errores antes de su lanzamiento final.');
-    message.channel.send(experimentalEmbed);
+        .setDescription(resources.GrayTick + ' **Función experimental**\nEstás ejecutando una versión inestable del código de esta función, por lo que esta podría sufrir modificaciones o errores antes de su lanzamiento final.');
+    await message.channel.send(experimentalEmbed).then(msg => {msg.delete(5000)});
     
     //-banear (@usuario | id) (motivo)
     
@@ -12,26 +12,43 @@ exports.run = async (discord, fs, config, keys, bot, message, args, command, log
         
         let notToBanEmbed = new discord.RichEmbed()
             .setColor(0xF12F49)
-            .setDescription(emojis.RedTick + ' Debes mencionar a un miembro o escribir su id');
+            .setDescription(resources.RedTick + ' Debes mencionar a un usuario o escribir su id');
 
         let noReasonEmbed = new discord.RichEmbed()
             .setColor(0xF12F49)
-            .setDescription(emojis.RedTick + ' Debes proporcionar un motivo');
+            .setDescription(resources.RedTick + ' Debes proporcionar un motivo');
 
         let noBotsEmbed = new discord.RichEmbed()
             .setColor(0xF12F49)
-            .setDescription(emojis.RedTick + ' No puedes banear a un bot');
-
+            .setDescription(resources.RedTick + ' No puedes banear a un bot');
+        
+        let alreadyBannedEmbed = new discord.RichEmbed()
+                .setColor(0xF12F49)
+                .setDescription(resources.RedTick + ' Este usuario ya ha sido baneado');
+        
         //Esto comprueba si se ha mencionado a un usuario o se ha proporcionado su ID
-        let member = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-        if (!member) return message.channel.send(notToBanEmbed);
+        let user = await message.mentions.users.first() || await bot.fetchUser(args[0]);
+        if (!user) return message.channel.send(notToBanEmbed);
+        
+        if (user.bot) return message.channel.send(noBotsEmbed);
         
         let author = message.guild.member(message.author.id)
         
-        //Se comprueba si puede banear al usuario
-        if (author.id !== message.guild.owner.id) {
-            if (author.highestRole.position <= member.highestRole.position) return message.channel.send(noPrivilegesEmbed)
+        let member = message.guild.members.get(user.id);
+        if (member) {
+            //Se comprueba si puede banear al usuario
+            if (author.id !== message.guild.owner.id) {
+                if (author.highestRole.position <= member.highestRole.position) return message.channel.send(noPrivilegesEmbed)
+            }
         }
+        
+        let bans = await message.guild.fetchBans();
+        let isBanned;
+        
+        await bans.forEach( async ban => {
+            if(ban.id === user.id) return isBanned = ban.id;
+        });
+        if (isBanned) return message.channel.send(alreadyBannedEmbed);
 
         let toDeleteCount = command.length - 2 + args[0].length + 2;
 
@@ -39,17 +56,15 @@ exports.run = async (discord, fs, config, keys, bot, message, args, command, log
         let reason = message.content.slice(toDeleteCount)
         if (!reason) return message.channel.send(noReasonEmbed);
 
-        if (member.bot) return message.channel.send('noBotsEmbed');
-
         let successEmbed = new discord.RichEmbed()
             .setColor(0xB8E986)
-            .setTitle(emojis.GreenTick + ' Operación completada')
-            .setDescription('El usuario <@' + member.id + '> ha sido baneado, ¿alguien más?');
+            .setTitle(resources.GreenTick + ' Operación completada')
+            .setDescription('El usuario <@' + user.id + '> ha sido baneado, ¿alguien más?');
 
         let loggingEmbed = new discord.RichEmbed()
             .setColor(0xEF494B)
-            .setAuthor(member.user.tag + ' ha sido BANEADO', member.user.displayAvatarURL)
-            .addField('Miembro', '<@' + member.id + '>', true)
+            .setAuthor(user.tag + ' ha sido BANEADO', user.displayAvatarURL)
+            .addField('Miembro', '<@' + user.id + '>', true)
             .addField('Moderador', '<@' + message.author.id + '>', true)
             .addField('Razón', reason, true)
             .addField('Duración', '∞', true);
@@ -57,16 +72,26 @@ exports.run = async (discord, fs, config, keys, bot, message, args, command, log
         let toDMEmbed = new discord.RichEmbed()
             .setColor(0xEF494B)
             .setAuthor('[BANEADO]', message.guild.iconURL)
-            .setDescription('<@' + member.id + '>, has sido baneado en ' + message.guild.name)
-            .addField('Moderador', message.author.tag, true)
+            .setDescription('<@' + user.id + '>, has sido baneado en ' + message.guild.name)
+            .addField('Moderador', '@' + message.author.tag, true)
             .addField('Razón', reason, true)
             .addField('Duración', '∞', true);
 
-        await member.send(toDMEmbed);
-        await member.ban(7, reason);
+        if (member) {
+            await user.send(toDMEmbed);
+        }
+        await message.guild.ban(user, {reason: reason});
         await loggingChannel.send(loggingEmbed);
         await message.channel.send(successEmbed);
     } catch (e) {
-        const handler = require(`../../errorHandler.js`).run(discord, config, bot, message, args, command, e);
+        if (e.toString().includes('Invalid Form Body')) {
+            let notToBanEmbed = new discord.RichEmbed()
+                .setColor(0xF12F49)
+                .setDescription(resources.RedTick + ' Si el usuario no está en el servidor, has de especificar su ID');
+            message.channel.send(notToBanEmbed);
+            console.log(e);
+        } else {
+            const handler = require(`../../errorHandler.js`).run(discord, config, bot, message, args, command, e);
+        }
     }
 }

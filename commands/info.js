342709b944bb741f8c951ddb06b4b98703c4fbe8
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-10-05 13:23:02 +0200

    Enlace enmascarado y cambio de emoji
    Se enmascara el enlace de invitación del servidor y se sustituye un emoji

--- commands/info.js
+++ commands/info.js
@@ -12,13 +12,13 @@ exports.run = (discord, fs, config, keys, bot, message, args, command, loggingCh
             .setURL('https://discord.gg/eWx72Jy')
             .setTitle('República Gamer LLC')
             .setFooter('©2018 República Gamer LLC', resources.server.iconURL)
-            .addField('🧬 El proyecto', 'La República Gamer nació de la curiosidad. El objetivo era crear un pequeño servidor tan solo para un pocos amigos, pero al ver las posibilidades que Discord ofrecía, el servidor fue escalado hasta convertirse en la comunidad que conocemos hoy en día.', true)
+            .addField('⚗ El proyecto', 'La República Gamer nació de la curiosidad. El objetivo era crear un pequeño servidor tan solo para un pocos amigos, pero al ver las posibilidades que Discord ofrecía, el servidor fue escalado hasta convertirse en la comunidad que conocemos hoy en día.', true)
             .addField('👨‍👨‍👧 La comunidad', 'La **República Gamer** es una comunidad creada _por y para jugadores_.\nNuestro objetivo es darnos a conocer entre toda la comunidad gamer a fin de conectar a jugadores con otros jugadores. Realizamos sorteos, torneos y proporcionamos sistemas para conectar a los usuarios.', true)
             .addField(resources.pilkobot + ' PilkoBot', '**' + bot.user.username + '** es un bot multifuncional desarrollado por el Staff de la comunidad, cuyo uso es exclusivo de los usuarios de la **República Gamer**, por lo que no está permitido su uso fuera de los servidores administrados por la **República Gamer LLC**.\n\n_Para más información relativa a las funcionalidades de este bot, usa el comando `' + config.prefix + 'ayuda`_', true)
             .addField('✉ Agradecimientos', '● Maria José Lozano\n● Amish Shah\n● El Pilko (Carlos)\n', true)
             .addField('📊 Estadísticas', bot.guilds.size + ' servidores\n' + bot.users.size + ' usuarios\nV ' + package.version, true)
             .addField('🔗 Añádeme', '___Aún no disponible___', true)
-            .addField('💬 Servidor', config.serverInvite, true)
+            .addField('💬 Servidor', '[Únete](' + config.serverInvite + ')', true)
             .addField('📣 Twitter', '___Aún no disponible___', true)
             .addField('🗂 Librerias', '● discord.js\n● flaticon', true);
         message.channel.send(resultEmbed);

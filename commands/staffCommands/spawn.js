b73a1c2c9b2773cbd70121995bdc4caac489085a
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-10-09 14:17:51 +0200

    Webhooks para el comando "spawn"
    Se crea un sistema de Webhooks para hacer que el comando parezca aún más realista

--- commands/staffCommands/spawn.js
+++ commands/staffCommands/spawn.js
@@ -12,13 +12,26 @@ exports.run = async (discord, fs, config, keys, bot, message, args, command, log
         if (url.length < 1) return message.channel.send(noCorrectSyntaxEmbed);
 
         message.delete();
-
+        
+        const avatar = "https://cdn.discordapp.com/avatars/365975655608745985/a6cfb55514ce22cd720d37d1c572a5e7.png?";
+        
         let resultEmbed = new discord.RichEmbed()
             .setColor(0x00AF85)
             .setTitle('A wild pokémon has appeared!')
             .setDescription('Guess the pokémon and type p!catch <pokémon> to catch it!')
             .setImage(url);
-        message.channel.send(resultEmbed);
+        
+        message.channel.fetchWebhooks().then(webhook => {
+            let foundWebhook = webhook.find('name', 'Pokécord')
+            
+            if (!foundWebhook) {
+                message.channel.createWebhook('Pokécord', avatar).then(webhook => {
+                webhook.send(resultEmbed);
+                })
+            } else {
+                foundWebhook.send(resultEmbed);
+            }
+        })
     } catch (e) {
         const handler = require(`../../errorHandler.js`).run(discord, config, bot, message, args, command, e);
     }

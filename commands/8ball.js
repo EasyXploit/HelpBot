e714133f7f965c777d4a6e1a6f93b1ed7e0b8241
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-10-09 21:55:29 +0200

    Modifición de dato
    Se cambia el contenido de un dato

--- commands/8ball.js
+++ commands/8ball.js
@@ -10,7 +10,7 @@ exports.run = (discord, fs, config, keys, bot, message, args, command, loggingCh
     
         if (!args[0]) return message.channel.send(notToAnswerEmbed);
 
-        const data = ['Concéntrate y pregunta otra vez', 'Ya está decidido', 'Mis fuentes me indican que no', 'Mi respuesta es no', 'Así como lo veo, si', 'Las señales indican que si', 'Lo más probable es que si', 'Lo verdad, no me importa lo más mínimo', '¡Ayúdame! Me tienen encerrado escribiendo frases de mi******', 'Quizás sea posible ... en otro universo', 'Solo si lo pides por favor', 'Para obtener una respuesta, introduzca 1 eur.', 'Pinta chungo', 'GL', 'Tan solo se que no se nada', 'Prueba otra vez', '¿Me lo preguntas enserio?', '¿Te enfadarás si te digo que no?', 'Si no me lo llegas a decir, no me la sudaba tanto'];
+        const data = ['Concéntrate y pregunta otra vez', 'Ya está decidido', 'Mis fuentes me indican que no', 'Mi respuesta es no', 'Así como lo veo, si', 'Las señales indican que si', 'Lo más probable es que si', 'Lo verdad, no me importa lo más mínimo', '¡Ayúdame! Me tienen encerrado escribiendo frases de mi******', 'Quizás sea posible ... en otro universo', 'Solo si lo pides por favor', 'Para obtener una respuesta, introduzca 1 eur.', 'Pinta chungo', 'GL', 'Tan solo se que no se nada', 'Prueba otra vez', '¿Me lo preguntas enserio?', '¿Te enfadarás si te digo que no?', 'Fuas, si no me lo llegas a decir, no me la sudaba tanto'];
 
         const resultEmbed = new discord.RichEmbed()
             .setColor(0xDDDDDD)

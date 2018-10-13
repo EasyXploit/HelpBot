7984fa9e280d918779310b8d703ca81e554034ea
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-10-13 16:15:26 +0200

    Nuevo comando "warn"
    Se añade un nuevo comando para realizar advertencias a los usuarios de la comunidad

--- commands/staffCommands/warn.js
+++ commands/staffCommands/warn.js
@@ -65,6 +65,80 @@ exports.run = async (discord, fs, config, keys, bot, message, args, command, log
             await loggingChannel.send(loggingEmbed);
             await member.send(toDMEmbed);
         });
+        
+        
+        //MODERACIÓN AUTOMÁTICA
+        
+        async function mute(duration) {
+            //Comprueba si existe el rol silenciado, y de no existir, lo crea
+            let role = message.guild.roles.find(r => r.name === 'Silenciado');
+            if (!role) {
+                role = await message.guild.createRole({
+                    name: 'Silenciado',
+                    color: '#818386',
+                    permissions: []
+                });
+                
+                let botMember = message.guild.members.get(bot.user.id);
+                await message.guild.setRolePosition(role, botMember.highestRole.position - 1);
+
+                message.guild.channels.forEach(async (channel, id) => {
+                    await channel.overwritePermissions (role, {
+                        SEND_MESSAGES: false,
+                        ADD_REACTIONS: false,
+                        SPEAK: false
+                    });
+                });
+            }
+
+            let loggingEmbed = new discord.RichEmbed()
+                .setColor(0xEF494B)
+                .setAuthor(member.user.tag + ' ha sido SILENCIADO', member.user.displayAvatarURL)
+                .addField('Miembro', '<@' + member.id + '>', true)
+                .addField('Moderador', '<@' + bot.user.id + '>', true)
+                .addField('Razón', 'Demasiadas advertencias', true)
+                .addField('Duración', duration, true);
+
+            let toDMEmbed = new discord.RichEmbed()
+                .setColor(0xEF494B)
+                .setAuthor('[SILENCIADO]', message.guild.iconURL)
+                .setDescription('<@' + member.id + '>, has sido silenciado en ' + message.guild.name)
+                .addField('Moderador', '<@' + bot.user.id + '>', true)
+                .addField('Razón', 'Demasiadas advertencias', true)
+                .addField('Duración', duration, true);
+
+            //Comprueba si este susuario ya estaba silenciado
+            if (!member.roles.has(role.id)) {
+                await member.addRole(role);
+            }
+            
+            await loggingChannel.send(loggingEmbed);
+            await member.send(toDMEmbed);
+        }
+
+        //Según las reglas aplicadas, se aplica una amonestación diferente para cada caso
+        if (bot.warns[member.id].warns >= 3 && bot.warns[member.id].warns < 5  && !bot.mutes.hasOwnProperty(member.id)) {
+        
+            mute('12h');
+
+            bot.mutes[member.id] = {
+                guild: message.guild.id,
+                time: Date.now() + 43200000
+            }
+
+            fs.writeFile('./mutes.json', JSON.stringify(bot.mutes, null, 4), async err => {
+                if (err) throw err;
+            })
+            
+        } else if (bot.warns[member.id].warns >= 5) {
+            if (bot.mutes.hasOwnProperty(member.id)) {
+                await delete bot.mutes[member.id];
+                await fs.writeFile('./mutes.json', JSON.stringify(bot.mutes), async err => {
+                    if (err) throw err;
+                });
+            };
+            mute('∞');
+        }
     } catch (e) {
         const handler = require(`../../errorHandler.js`).run(discord, config, bot, message, args, command, e);
     }

c93ce50bb45fc10962e3b4a2432d06f56a817b98
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-10-19 13:32:29 +0200

    Colores globales e indentación
    Se sustituyen todas las ' por `
    
    Ahora los colores de los embeds se obtienen de un recurso global
    
    Se cambia la indentación.

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,26 +1,25 @@
 exports.run = (event, discord, fs, config, keys, bot, resources) => {
     
     //Previene que continue la ejecución si el servidor no es la República Gamer
-    if (event.guild.id !== '374945492133740544') return;
+    if (event.guild.id !== `374945492133740544`) return;
 
     const loggingChannel = bot.channels.get(config.loggingChannel);
     
     if (!event.user.bot) {
-        console.log(new Date().toUTCString() + ' 》@' + event.user.tag + ' abandonó la guild: ' + event.guild.name);
+        console.log(`${new Date().toUTCString()} 》@${event.user.tag} abandonó la guild: ${event.guild.name}`);
 
         let embed = new discord.RichEmbed()
-            .setColor(0xDB904D)
-            .setTimestamp()
-            .setFooter('© 2018 República Gamer LLC', bot.user.avatarURL)
-            .addField('📑 Auditoría', '@' + event.user.tag + ' abandonó el servidor ↗');
+            .setColor(resources.orange)
+            .addField(`📑 Auditoría`, `@${event.user.tag} abandonó el servidor ↗`)
+            .setFooter(`© 2018 República Gamer LLC`, resources.server.iconURL)
+            .setTimestamp();
         loggingChannel.send(embed);
     } else {
         if (event.user.id === bot.user.id) return;
         let loggingGoodbyeBotEmbed = new discord.RichEmbed()
-            .setColor(0xDB904D)
-            .setTimestamp()
-            .setFooter('© 2018 República Gamer LLC', bot.user.avatarURL)
-            .addField('📑 Auditoría', 'El **BOT** @' + event.user.tag + ' fue eliminado del servidor ↗');
+            .setColor(resources.orange)
+            .addField(`📑 Auditoría`, `El **BOT** @${event.user.tag} fue eliminado del servidor ↗`)
+            .setFooter(`© 2018 República Gamer LLC`, resources.server.iconURL).setTimestamp();
         loggingChannel.send(loggingGoodbyeBotEmbed)
         return;
     }

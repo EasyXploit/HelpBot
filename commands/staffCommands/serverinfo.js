a5497ef33235913c3f57197ae292ca2ce161fdeb
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-10-19 13:25:50 +0200

    Colores globales, indentación y emojis
    Se sustituyen todas las ' por `
    
    Ahora los colores de los embeds se obtienen de un recurso global
    
    Se cambia la indentación.
    
    Se añaden emojis a los campos

--- commands/staffCommands/serverinfo.js
+++ commands/staffCommands/serverinfo.js
@@ -1,47 +1,50 @@
 exports.run = async (discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, resources) => {
-    
+
     //-serverinfo
-    
-    try {    
+
+    try {
         const guild = message.guild;
         let minutes = (guild.afkTimeout / 60);
-        let large = 'No';
+        let large = `No`;
         let verified;
-        
-        if (guild.large === true) {large = 'Si'};
+
+        if (guild.large === true) {
+            large = `Si`
+        };
+
         if (guild.verified === true) {
-            verified = 'Si';
+            verified = `Si`;
         } else if (guild.verified === false) {
-            verified = 'No';
+            verified = `No`;
         } else if (guild.verified === undefined) {
-            verified = 'No';
+            verified = `No`;
         }
-        
+
         let categories = new Set();
-        
+
         let categoriesCount = guild.channels.filter(c => {
             if (categories.has(c.parent)) return;
             categories.add(c.parent);
         });
-        
+
         let resultEmbed = new discord.RichEmbed()
-            .setColor(0xFFC857)
-            .setAuthor('Información del servidor', guild.iconURL)
-            .setDescription('Mostrando información acerca de la guild ' + guild.name)
+            .setColor(resources.gold)
+            .setAuthor(`Información del servidor`, guild.iconURL)
+            .setDescription(`Mostrando información acerca de la guild ${guild.name}`)
             .setThumbnail(guild.iconURL)
-            .addField('Nombre', guild.name, true)
-            .addField('ID', guild.id, true)
-            .addField('Propietario', guild.owner + ' (ID: ' + guild.ownerID + ')', true)
-            .addField('Fecha de creación', guild.createdAt.toUTCString(), true)
-            .addField('Región', guild.region, true)
-            .addField('Canal de AFK', guild.afkChannel.name + '\nTimeout: ' + minutes + ' minutos', true)
-            .addField('Large guild (+250)', large, true)
-            .addField('Nivel de verificación', guild.verificationLevel, true)
-            .addField('Roles', guild.roles.size, true)
-            .addField('Miembros', guild.memberCount + ' miembros\n' + guild.members.filter(m => m.user.presence.status == 'online').size + ' online\n' + guild.members.filter(m => m.user.bot).size + ' bots, ' + guild.members.filter(m => !m.user.bot).size + ' humanos', true)
-            .addField('Canales', guild.channels.size + ' canales en total:\n' + (categories.size - 1) + ' categorías\n' + guild.channels.filter(c => c.type === 'text').size + ' de texto, ' + guild.channels.filter(c => c.type === 'voice').size + ' de voz', true)
-            .addField('Verificado', verified, true)
-            
+            .addField(`🏷 Nombre`, guild.name, true)
+            .addField(`🆔 ID`, guild.id, true)
+            .addField(`👑 Propietario`, `${guild.owner} (ID: ${guild.ownerID})`, true)
+            .addField(`📝 Fecha de creación`, guild.createdAt.toUTCString(), true)
+            .addField(`🌍 Región`, guild.region, true)
+            .addField(`🕗 Canal de AFK`, `${guild.afkChannel.name}\nTimeout: ${minutes} minutos`, true)
+            .addField(`🆙 Large guild (+250)`, large, true)
+            .addField(`👮 Nivel de verificación`, guild.verificationLevel, true)
+            .addField(`🔖 Roles`, guild.roles.size, true)
+            .addField(`👥 Miembros`, `${guild.memberCount} miembros\n${guild.members.filter(m => m.user.presence.status == 'online').size} online\n${guild.members.filter(m => m.user.bot).size} bots, ${guild.members.filter(m => !m.user.bot).size} humanos`, true)
+            .addField(`💬 Canales`, `${guild.channels.size} canales en total:\n${(categories.size - 1)} categorías\n${guild.channels.filter(c => c.type === 'text').size} de texto, ${guild.channels.filter(c => c.type === 'voice').size} de voz`, true)
+            .addField(`${resources.GreenTick} Verificado`, verified, true)
+
         message.channel.send(resultEmbed);
     } catch (e) {
         const handler = require(`../../errorHandler.js`).run(discord, config, bot, message, args, command, e);
--- commands/staffCommands/userinfo.js
+++ commands/staffCommands/userinfo.js
@@ -1,16 +1,16 @@
 exports.run = async (discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, resources) => {
-    
+
     //-userinfo (@usuario | id)
-    
+
     try {
         let noUserEmbed = new discord.RichEmbed()
             .setColor(0xF12F49)
-            .setDescription(resources.RedTick + ' No has proporcionado un usuario válido');
-        
+            .setDescription(`${resources.RedTick} No has proporcionado un usuario válido`);
+
         let noBotsEmbed = new discord.RichEmbed()
             .setColor(0xF12F49)
-            .setDescription(resources.RedTick + ' No puedes obtener información de un bot');
-        
+            .setDescription(`${resources.RedTick} No puedes obtener información de un bot`);
+
         //let toInfo;
         let member;
 
@@ -19,29 +19,42 @@ exports.run = async (discord, fs, config, keys, bot, message, args, command, log
         } else {
             member = message.mentions.members.first() || message.guild.members.get(args[0]);
         }
-        
+
         if (!member) return message.channel.send(noUserEmbed);
-        
+
         let user = member.user;
         if (user.bot) return message.channel.send(noBotsEmbed);
-        
+
         //Comprueba si el usuario es baneable
-        let bannable = 'No';
-        if (member.bannable === true) {bannable = 'Si'};
-        
+        let bannable = `No`;
+
+        if (member.bannable === true) {
+            bannable = `Si`
+        };
+
         //Comprueba los permisos del usuario
         let status = [];
-        if (member.id === message.guild.owner.id) {status.push('Propietario')};
-        if (member.hasPermission('ADMINISTRATOR')) {status.push('Administrador')};
-        if (member.hasPermission('MANAGE_MESSAGES')) {status.push('Moderador')};
+        if (member.id === message.guild.owner.id) {
+            status.push(`Propietario`)
+        };
         
-        if (status.length < 1) {status.push('Usuario regular')};
+        if (member.hasPermission(`ADMINISTRATOR`)) {
+            status.push(`Administrador`)
+        };
         
+        if (member.hasPermission(`MANAGE_MESSAGES`)) {
+            status.push(`Moderador`)
+        };
+
+        if (status.length < 1) {
+            status.push(`Usuario regular`)
+        };
+
         let perms = member.permissions.serialize();
-        let permsArray = Object.keys(perms).filter(function(x) { 
-            return perms[x] !== false; 
+        let permsArray = Object.keys(perms).filter(function (x) {
+            return perms[x] !== false;
         });
-        
+
         //Comprueba el número de warns del usuario
         let warns;
         if (!bot.warns[member.id]) {
@@ -49,23 +62,23 @@ exports.run = async (discord, fs, config, keys, bot, message, args, command, log
         } else {
             warns = bot.warns[member.id].warns
         }
-        
+
         let resultEmbed = new discord.RichEmbed()
             .setColor(member.displayHexColor)
-            .setTitle('🙍 Información de usuario')
-            .setDescription('Mostrando información acerca del usuario <@' + member.id + '>')
+            .setTitle(`🙍 Información de usuario`)
+            .setDescription(`Mostrando información acerca del usuario <@${member.id}>`)
             .setThumbnail(user.displayAvatarURL)
-            .addField('Nickname', member.displayName, true)
-            .addField('TAG completo', user.tag, true)
-            .addField('ID del usuario', member.id, true)
-            .addField('Permisos', permsArray.join(', ').toLowerCase(),true)
-            .addField('Baneable', bannable, true)
-            .addField('Fecha de registro', user.createdAt.toUTCString(), true)
-            .addField('Unido al servidor', member.joinedAt.toUTCString(), true)
-            .addField('Estatus', status.join(', '), true)
-            .addField('Rol más alto', member.highestRole.name, true)
-            .addField('Último mensaje', member.lastMessage + ' (ID: ' + member.lastMessageID + ')', true)
-            .addField('Infracciones', warns, true)
+            .addField(`ℹ Nickname`, member.displayName, true)
+            .addField(`🏷 TAG completo`, user.tag, true)
+            .addField(`🆔 ID del usuario`, member.id, true)
+            .addField(`👮 Permisos`, permsArray.join(', ').toLowerCase(), true)
+            .addField(`⚒ Baneable`, bannable, true)
+            .addField(`📝 Fecha de registro`, user.createdAt.toUTCString(), true)
+            .addField(`↙ Unido al servidor`, member.joinedAt.toUTCString(), true)
+            .addField(`👑 Estatus`, status.join(', '), true)
+            .addField(`🎖 Rol más alto`, member.highestRole.name, true)
+            .addField(`💬 Último mensaje`, `${member.lastMessage} (ID: ${member.lastMessageID})`, true)
+            .addField(`⚖ Infracciones`, warns, true)
         message.channel.send(resultEmbed);
     } catch (e) {
         const handler = require(`../../errorHandler.js`).run(discord, config, bot, message, args, command, e);

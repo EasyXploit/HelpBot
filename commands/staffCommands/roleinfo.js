616bf96d9a47a26b000a443f815f5ff9f420c40c
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-10-19 13:26:25 +0200

    Colores globales, emojis en campos, indentación y argumentos mejorados
    Se cambia la forma en la que se procesan los argumentos.
    
    Se sustituyen todas las ' por `
    
    Ahora los colores de los embeds se obtienen de un recurso global
    
    Se cambia la indentación.
    
    Se añaden emojis a los campos

--- commands/staffCommands/roleinfo.js
+++ commands/staffCommands/roleinfo.js
@@ -1,42 +1,52 @@
 exports.run = async (discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, resources) => {
-    
-    //-roleinfo (@rol | "rol" | id)
-    
+
+    //-roleinfo (@rol | rol | id)
+
     try {
         let noCorrectSyntaxEmbed = new discord.RichEmbed()
             .setColor(0xF12F49)
-            .setDescription(resources.RedTick + ' La sintaxis de este comando es `' + config.staffPrefix + 'roleinfo (@rol | "rol" | id)`');
-        
+            .setDescription(`${resources.RedTick} La sintaxis de este comando es ` + '`' + config.staffPrefix + 'roleinfo (@rol | rol | id)`');
+
+        if (!args[0]) return message.channel.send(noCorrectSyntaxEmbed);
+
         let guild = message.guild;
-        let role = message.mentions.roles.first() || guild.roles.get(args[0]) || message.guild.roles.find('name', args[0]);
-        if (!role) {
-            let newArgs = message.content.slice(10).split('"').slice(0, 1).join();
-            role = message.guild.roles.find('name', newArgs)
-        }
-        
-        if (!role) return message.channel.send(noCorrectSyntaxEmbed);
+        let role = message.mentions.roles.first() || guild.roles.get(args[0]) || message.guild.roles.find('name', args.join(` `));
+
+        let roleNotFoundEmbed = new discord.RichEmbed()
+            .setColor(0xF12F49)
+            .setDescription(`${resources.RedTick} El rol no existe`);
+
+        if (!role) return message.channel.send(roleNotFoundEmbed);
 
         let membersWithRole = message.guild.roles.get(role.id).members.size;
-        let mentionable = 'No';
-        let hoisted = 'Oculto';
-        let managed = 'Local';
+        let mentionable = `No`;
+        let hoisted = `Oculto`;
+        let managed = `Local`;
+
+        if (role.mentionable === true) {
+            mentionable = `Si`
+        };
         
-        if (role.mentionable === true) {mentionable = 'Si'};
-        if (role.hoist === true) {hoisted = 'Visible'};
-        if (role.managed === true) {managed = 'Externa'};
+        if (role.hoist === true) {
+            hoisted = `Visible`
+        };
         
+        if (role.managed === true) {
+            managed = `Externa`
+        };
+
         let resultEmbed = new discord.RichEmbed()
             .setColor(role.hexColor)
-            .setTitle('🔖 Información de rol')
-            .setDescription('Mostrando información acerca del rol <@&' + role.id + '>')
-            .addField('Nombre del rol', role.name, true)
-            .addField('ID del rol', role.id, true)
-            .addField('Miembros con el rol', membersWithRole, true)
-            .addField('Mencionable', mentionable, true)
-            .addField('Se muestra', hoisted, true)
-            .addField('Color', role.hexColor, true)
-            .addField('Fecha de creación', role.createdAt.toUTCString(), true)
-            .addField('Administración', managed, true)
+            .setTitle(`🔖 Información de rol`)
+            .setDescription(`Mostrando información acerca del rol <@&${role.id}>`)
+            .addField(`🏷 Nombre del rol`, role.name, true)
+            .addField(`🆔 ID del rol`, role.id, true)
+            .addField(`👥 Miembros con el rol`, membersWithRole, true)
+            .addField(`🗣 Mencionable`, mentionable, true)
+            .addField(`👁️‍ Se muestra`, hoisted, true)
+            .addField(`🔰 Color`, role.hexColor, true)
+            .addField(`📝 Fecha de creación`, role.createdAt.toUTCString(), true)
+            .addField(`⚙ Administración`, managed, true)
 
         message.channel.send(resultEmbed);
     } catch (e) {

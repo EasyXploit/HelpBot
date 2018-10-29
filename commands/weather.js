94b71996eee8cdfa26aa91f6eb95bb955cc6788e
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-10-30 00:44:56 +0100

    Nuevo comando "weather"
    Se añade un nuevo comando que permite consultar el clima de una ubicación especificada por el usuario

---
+++ commands/weather.js
@@ -0,0 +1,37 @@
+exports.run = (discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, resources) => {
+    
+    //!weather (ubicación)
+    
+    try {
+        const weather = module.require('weather-js');
+        
+        let notToSearchEmbed = new discord.RichEmbed()
+            .setColor(0xF12F49)
+            .setTitle(resources.RedTick + ' Debes proporcionarme un término de búsqueda')
+            .setDescription('La sintaxis de este comando es `' + config.prefix +'weather (ubicación)`');
+    
+        if (!args[0]) return message.channel.send(notToSearchEmbed);
+        
+        const searchTerm = args.join(` `);
+        
+        weather.find({search: searchTerm, degreeType: 'C'}, function(err, result) {
+            if(err) console.log(err);
+            
+            const current = result[0].current;
+            const location = result[0].location;
+            
+            const resultEmbed = new discord.RichEmbed()
+                .setColor(0x93BEDF)
+                .setThumbnail(current.imageUrl)
+                .setTitle(`🌦 | El tiempo en __${current.observationpoint}__`)
+                .setDescription(`Tipo de tiempo: **${current.skytext}**`)
+                .addField(`🌡 Temperatura`, `${current.temperature}º${location.degreetype}`, true)
+                .addField(`❄ Sensación térmica`, `${current.feelslike}º${location.degreetype}`, true)
+                .addField(`🌀 Viento`, current.winddisplay, true)
+                .addField(`💧 Humedad`, `${current.humidity}%`, true);
+            message.channel.send(resultEmbed);
+        });
+    } catch (e) {
+        const handler = require(`../errorHandler.js`).run(discord, config, bot, message, args, command, e);
+    }
+}

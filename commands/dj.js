abdb5cecdefb93ece4dcfad8de0d29b627d640b9
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-10-30 00:52:33 +0100

    Función para "dj"
    Ahora el comando djusa una función para encontar el rol

--- commands/dj.js
+++ commands/dj.js
@@ -8,7 +8,7 @@ exports.run = async (discord, fs, config, keys, bot, message, args, command, log
             .setTitle(resources.RedTick + ' Ocurrió un error')
             .setDescription(message.author.username + ', no dispones de privilegios suficientes para ejecutar este comando.\nSolo aquellos que hayan alcanzado el rango **EXPERTOS** pueden usar esta característica.');
     
-        if(!message.member.roles.find("name", "DJ")) return message.channel.send(noPrivilegesEmbed);
+        if(!message.member.roles.find(r=> r.name === `DJ`)) return message.channel.send(noPrivilegesEmbed);
 
         let successEmbed = new discord.RichEmbed()
             .setColor(0xB8E986)

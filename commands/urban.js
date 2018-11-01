9a4ea7fe4e55da1db1baa815a898cca9f467b79e
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-11-01 02:11:35 +0100

    Cambio de sintaxis
    Se modifica la sintaxis del comando

--- commands/urban.js
+++ commands/urban.js
@@ -19,10 +19,10 @@ exports.run = (discord, fs, config, keys, bot, message, args, command, loggingCh
             
             const resultEmbed = new discord.RichEmbed()
                 .setColor(0x1D2339)
-                .setThumbnail('https://i.imgur.com/ftJ1lnP.png')
+                .setThumbnail(`https://i.imgur.com/ftJ1lnP.png`)
                 .setTitle(`📔 | Definición de: __${data[0].word.toUpperCase()}__`)
                 .setDescription(data[0].definition)
-                .addField('Valoraciones:', 'Likes: ' + data[0].thumbs_up + ' | Dislikes: ' + data[0].thumbs_down, true)
+                .addField(`Valoraciones`, `Likes: ${data[0].thumbs_up} | Dislikes: ${data[0].thumbs_down}`, true)
                 .setFooter(`Escrito por: ${data[0].author}`);
             message.channel.send(resultEmbed);
         }).catch((error) => {
@@ -30,7 +30,7 @@ exports.run = (discord, fs, config, keys, bot, message, args, command, loggingCh
                 .setColor(0xF12F49)
                 .setDescription(resources.RedTick + ' No se ha encontrado ningún resultado que coincida con `' + searchTerm + '`');
             
-            if (error.toString().includes('is undefined')) {
+            if (error.toString().includes(`is undefined`)) {
                 message.channel.send(noDataEmbed);
             }
         })

8d492febc0a1d1f555ade2006e19d046cc070542
Daniel Perales Mauriz <danielperales555@gmail.com>
2018-11-02 13:05:44 +0100

    Nuevo comando "np"
    Se añade un nuevo comando para visualizar el estado actual de la canción.

---
+++ commands/np.js
@@ -0,0 +1,47 @@
+exports.run = async (discord, fs, config, keys, bot, message, args, command, loggingChannel, debuggingChannel, resources) => {
+    
+    const noPrivilegesEmbed = new discord.RichEmbed()
+        .setColor(resources.red)
+        .setDescription(`${resources.RedTick} ${message.author.username}, no dispones de privilegios suficientes para realizar esta operación`);
+
+    if (!message.member.roles.has(config.botStaff) && !message.member.roles.has(`375376646771048449`)) return message.channel.send(noPrivilegesEmbed)
+    
+    //!np
+
+    try {
+        let noQueueEmbed = new discord.RichEmbed()
+            .setColor(resources.red)
+            .setDescription(`${resources.RedTick} El bot no tiene ninguna canción en la cola.`);
+        
+        if (!bot.servers[message.guild.id]) return message.channel.send(noQueueEmbed);
+        
+        const ytdl = require(`ytdl-core`);
+        const moment = require(`moment`);
+        const randomColor = require('randomcolor');
+        
+        let info = await ytdl.getInfo(bot.servers[message.guild.id].nowplaying.link);
+        let server = bot.servers[message.guild.id];
+        let progress = await bot.voiceDispatcher.time;
+        
+        let total = info.length_seconds * 1000;
+        let percentage = Math.floor((progress * 100) / total);
+        
+        let progressBar = [`▬`, `▬`, `▬`, `▬`, `▬`, `▬`, `▬`, `▬`, `▬`, `▬`, `▬`];
+        
+        if (percentage <= 10) {
+            progressBar[0] = `🔘`;
+        } else if (percentage > 10) {
+            progressBar[percentage.toString().slice(0, 1)] = `🔘`;
+        }
+        
+        let progressEmbed = new discord.RichEmbed()
+            .setColor(randomColor())
+            .setAuthor(`Ahora mismo:`, `https://i.imgur.com/lvShSwa.png`)
+            .setDescription(`[${server.nowplaying.title}](${server.nowplaying.link})\n${progressBar.join(``)} ${percentage}%\n` + '`' + moment().startOf('day').milliseconds(progress).format('H:mm:ss') + ' / ' + moment().startOf('day').milliseconds(total).format('H:mm:ss') + '`')
+            .setFooter(`© 2018 República Gamer LLC | BETA Pública`, resources.server.iconURL);
+        
+        message.channel.send(progressEmbed);
+    } catch (e) {
+        const handler = require(`../errorHandler.js`).run(discord, config, bot, message, args, command, e);
+    }
+}

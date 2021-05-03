5f2ed566446863c010edb2d6cddaaacdeed9362c
Daniel Perales Mauriz <dperales555@gmail.com>
2021-05-03 19:01:56 +0200

    Simplificación del comando "presencia"
    Se simplifica el código del comando de ajuste de presencia del bot.

--- commands/ownerCommands/presence.js
+++ commands/ownerCommands/presence.js
@@ -1,60 +1,55 @@
 exports.run = async (discord, fs, config, keys, client, message, args, command, loggingChannel, debuggingChannel, resources) => {
 
-    //$presence (estatus | actividad) (online | offline | idle | dnd - nombreDeLaAtividad)
+    //$presence (status | activity) (online | offline | idle | dnd - nombre de la actividad)
     
     try {
-        let fields = message.content.slice(11).split('" "');
-        let lastField = fields.slice(-1).join();
-
-        lastField = lastField.substring(0, lastField.length - 1);
-        fields.splice(-1);
-        fields.push(lastField);
-
-        let toModify = fields[0].toLowerCase();
-        let content = fields[1];
-        let changed;
 
         let noCorrectSyntaxEmbed = new discord.MessageEmbed()
             .setColor(resources.red2)
-            .setDescription(`${resources.RedTick} La sintaxis del comando es \`${config.ownerPrefix}presence ("estatus" | "actividad") ("online" | "offline" | "idle" | "dnd" - "nombreDeLaAtividad")\``);
+            .setDescription(`${resources.RedTick} La sintaxis del comando es \`${config.ownerPrefix}presence (status | activity) (online | offline | idle | dnd - nombreDeLaAtividad)\``);
+
+        if (!args[0] || !args[1]) return message.channel.send(noCorrectSyntaxEmbed);
+
+        let toModify = args[0];
+        let newValue = args.slice(1).join(' ');
+        let changed;
+
+        if (toModify !== 'status' && toModify !== 'activity') return message.channel.send(noCorrectSyntaxEmbed);
 
         let actuallyConfiguredEmbed = new discord.MessageEmbed()
             .setColor(resources.red2)
             .setDescription(`${resources.RedTick} Esta configuración ya ha sido aplicada`);
 
-        if (!args[0] || !args[1]) return message.channel.send(noCorrectSyntaxEmbed);
-        if (toModify !== 'estatus' && toModify !== 'actividad') return message.channel.send(noCorrectSyntaxEmbed);
-
-        if (toModify === 'estatus') {
-            if (content !== 'online' && content !== 'invisible' && content !== 'idle' && content !== 'dnd') return message.channel.send(noCorrectSyntaxEmbed);
-            if (content === config.status) return message.channel.send(actuallyConfiguredEmbed);
-            config.status = content;
+        if (toModify === 'status') {
+            if (newValue !== 'online' && newValue !== 'invisible' && newValue !== 'idle' && newValue !== 'dnd') return message.channel.send(noCorrectSyntaxEmbed);
+            if (newValue === config.status) return message.channel.send(actuallyConfiguredEmbed);
+            config.status = newValue;
 
             //Graba la configuración
             await fs.writeFile('./configs/config.json', JSON.stringify(config, null, 4), (err) => console.error);
             await client.user.setStatus(config.status);
 
             changed = 'el estado';
-        } else if (toModify === 'actividad') {
-            if (content === config.game) return message.channel.send(actuallyConfiguredEmbed);
-            config.game = content;
+        } else if (toModify === 'activity') {
+            if (newValue === config.game) return message.channel.send(actuallyConfiguredEmbed);
+            config.game = newValue;
 
             //Graba la configuración
             await fs.writeFile('./configs/config.json', JSON.stringify(config, null, 4), (err) => console.error);
             await client.user.setPresence({game: {name: config.game, type: config.type}});
 
             changed = 'la actividad';   
-        }
+        };
 
         let resultEmbed = new discord.MessageEmbed()
             .setColor(resources.green2)
             .setTitle(`${resources.GreenTick} Operación en marcha`)
-            .setDescription(`Cambiaste ${changed} del bot a \`${content}\`.\nEsta operación podría tardar unos minutos en completarse.`)
+            .setDescription(`Cambiaste ${changed} del bot a \`${newValue}\`.\nEsta operación podría tardar unos minutos en completarse.`)
 
         let loggingEmbed = new discord.MessageEmbed()
             .setColor(resources.blue)
             .setTitle('📑 Auditoría - [PRESENCIA]')
-            .setDescription(`${message.author.tag} cambió ${changed} del bot a \`${content}\``);
+            .setDescription(`${message.author.tag} cambió ${changed} del bot a \`${newValue}\`.`);
 
         await loggingChannel.send(loggingEmbed);
         await message.channel.send(resultEmbed)

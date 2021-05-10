2aebda9943960d13ef3c305ded261f296ac452ad
Daniel Perales Mauriz <dperales555@gmail.com>
2021-05-10 11:16:55 +0200

    Urban Dictionary rediseñado
    Se rediseña el comando "urban" para adaptarse a la versión 3.0.0.

--- commands/urban.js
+++ commands/urban.js
@@ -1,40 +1,40 @@
-exports.run = (discord, fs, client, message, args, command) => {
+exports.run = async (discord, fs, client, message, args, command) => {
     
     //!urban (término)
     
     try {
-        const urban = module.require('urban-dictionary');
-        
+
         let notToSearchEmbed = new discord.MessageEmbed()
             .setColor(client.colors.red2)
-            .setTitle(`${client.emotes.redTick} Debes proporcionarme un término de búsqueda`)
-            .setDescription(`La sintaxis de este comando es \`${client.config.prefixes.mainPrefix}urban (término)\``);
+            .setTitle(`${client.emotes.redTick} Debes proporcionar un término de búsqueda.`)
+            .setDescription(`La sintaxis de este comando es \`${client.config.prefixes.mainPrefix}urban (término)\`.`);
     
         if (!args[0]) return message.channel.send(notToSearchEmbed);
-        
-        const searchTerm = args.join(` `);
-        
-        urban.term(searchTerm).then((result) => {
-            const data = result.entries;
-            
+
+        const searchTerm = args.join(' ');
+        const urbanDictionary = require('urban-dictionary');
+
+        urbanDictionary.define(searchTerm).then(async (results) => {
+            const result = results[0];
+
             const resultEmbed = new discord.MessageEmbed()
                 .setColor(0x1D2339)
-                .setThumbnail(`https://i.imgur.com/ftJ1lnP.png`)
-                .setTitle(`📔 | Definición de: __${data[0].word.toUpperCase()}__`)
-                .setDescription(data[0].definition)
-                .addField(`Valoraciones`, `Likes: ${data[0].thumbs_up} | Dislikes: ${data[0].thumbs_down}`, true)
-                .setFooter(`Escrito por: ${data[0].author}`);
-            message.channel.send(resultEmbed);
+                .setThumbnail('https://i.imgur.com/ftJ1lnP.png')
+                .setTitle(`📔 | Definición de: __${result.word.toUpperCase()}__`)
+                .setDescription(result.definition)
+                .addField('Valoraciones:', `Likes: ${result.thumbs_up} | Dislikes: ${result.thumbs_down}`, true)
+                .setFooter(`Escrito por: ${result.author}`);
+
+            await message.channel.send(resultEmbed);
         }).catch((error) => {
-            let noDataEmbed = new discord.MessageEmbed()
+            const noResultEmbed = new discord.MessageEmbed()
                 .setColor(client.colors.red2)
-                .setDescription(`${client.emotes.redTick} No se ha encontrado ningún resultado que coincida con \`${searchTerm}\``);
+                .setDescription(`${client.emotes.redTick} No se ha encontrado ningún resultado que coincida con \`${searchTerm}\`.`);
             
-            if (error.toString().includes(`is undefined`)) {
-                message.channel.send(noDataEmbed);
-            }
-        })
+            if (error.toString().includes('No results found')) return message.channel.send(noResultEmbed);
+            require('../utils/errorHandler.js').run(discord, client, message, args, command, error);
+        });
     } catch (e) {
         require('../utils/errorHandler.js').run(discord, client, message, args, command, e);
-    }
-}
+    };
+};

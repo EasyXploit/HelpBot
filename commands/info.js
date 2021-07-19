e22c0aea7447ded91ffdd9bb5cf747d457fe8e63
Daniel Perales Mauriz <dperales555@gmail.com>
2021-07-19 03:35:39 +0200

    Información mejorada
    Se mejora el contenido del comando de información, obteniendo varios de sus campos del fichero package.json, además de perfeccionar el conteo de usuarios.

--- commands/info.js
+++ commands/info.js
@@ -6,14 +6,15 @@ exports.run = async (discord, client, message, args, command, commandConfig) => 
     
     try {
         let resultEmbed = new discord.MessageEmbed()
-            .setAuthor('La República Gamer', client.user.avatarURL())
-            .setThumbnail('https://i.imgur.com/cTW63kf.png')
-            .setURL(client.config.guild.serverInvite)
-            .setTitle('República Gamer S.L.')
-            .addField(`${client.emotes.pilkobot} ${client.user.username}`, `**${client.user.username}** es un bot multifuncional para Discord desarrollado por el Staff de **República Gamer**, cuyo uso a fecha de publicación de este documento es exclusivo de los usuarios de dicha comunidad.\n\nEste proyecto no tiene licencia, por lo que se aplican las leyes de copyright predeterminadas, lo que significa que conservamos todos los derechos de nuestro código fuente y que nadie puede reproducir, distribuir o crear trabajos derivados de este.\n\n_Para más información relativa a las funcionalidades de este bot, usa el comando \`${client.config.prefixes.mainPrefix}ayuda\`_`)
             .setColor(client.colors.primary)
+            .setAuthor(packageInfo.normalizedName, client.user.avatarURL())
+            .setTitle(packageInfo.normalizedName)
+            .setURL(packageInfo.homepage)
+            .setThumbnail(client.user.avatarURL())
+            .setDescription(packageInfo.description)
+            .addField('✉ Licencia', 'Este proyecto no tiene licencia, por lo que se aplican las leyes de copyright predeterminadas, lo que significa que conservamos todos los derechos de nuestro código fuente y que nadie puede reproducir, distribuir o crear trabajos derivados de este.')
             .addField('✉ Agradecimientos', '● Maria José Lozano\n● Amish Shah\n● El Pilko (Carlos)', true)
-            .addField('📊 Estadísticas', `${client.guilds.cache.size} servidores\n${client.users.cache.filter(user => !user.bot).size} miembros\nV ${package.version}`, true)
+            .addField('📊 Estadísticas', `${client.homeGuild.members.cache.filter(member => !member.user.bot).size} miembros\nV ${packageInfo.version}`, true)
             .addField('🗂 Librerias', '● discord.js\n● flaticon', true);
         message.channel.send(resultEmbed);
     } catch (error) {

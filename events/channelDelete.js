9b573dafe2b17e000281e50cd65247dbbac91b96
Daniel Perales Mauriz <dperales555@gmail.com>
2021-07-19 03:41:36 +0200

    Manejo de canales configurados borrados
    Se maneja cuando un canal configurado en el bot ha sido borrado por algún motivo.

---
+++ events/channelDelete.js
@@ -0,0 +1,29 @@
+exports.run = async (channel, client, discord) => {
+    if (channel.type === 'text') {
+        if (channel.id === client.config.guild.debuggingChannel) {
+
+            //Borra la configuración y descarga el canal
+            client.config.guild.debuggingChannel = '';
+            client.debuggingChannel = null;
+
+            //Graba la nueva configuración en el almacenamiento
+            await client.fs.writeFile('./configs/guild.json', JSON.stringify(client.config.guild, null, 4), (err) => console.error);
+
+            //Advertir por consola
+            console.error(`${new Date().toLocaleString()} 》Error: El canal de depuración ya no existe o no se puede tener acceso.\n Se ha borrado de la configuración y se ha descargado de la memoria.`);
+        };
+
+        if (channel.id === client.config.guild.loggingChannel) {
+
+            //Borra la configuración y descarga el canal
+            client.config.guild.loggingChannel = '';
+            client.loggingChannel = null;
+
+            //Graba la nueva configuración en el almacenamiento
+            await client.fs.writeFile('./configs/guild.json', JSON.stringify(client.config.guild, null, 4), (err) => console.error);
+
+            //Advertir por consola
+            console.error(`${new Date().toLocaleString()} 》Error: El canal de auditoría ya no existe o no se puede tener acceso.\n Se ha borrado de la configuración y se ha descargado de la memoria.`);
+        };
+    };
+};

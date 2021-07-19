7398b7893e2705c60b59f5e0d30ee6e49d4c7d27
Daniel Perales Mauriz <dperales555@gmail.com>
2021-07-19 02:19:11 +0200

    Código de RG eliminado
    Se eliminan secciones de código y recursos relacionados con las comunidad "República Gamer" cómo parte del proyecto de conversión al modelo Open Source.

--- utils/emotes.js
+++ utils/emotes.js
@@ -8,16 +8,5 @@ exports.run = async (client) => {
         redTick:    await client.emojis.cache.get(client.config.emotes.redTick) || '❌',
         orangeTick: await client.emojis.cache.get(client.config.emotes.orangeTick) || '⚠',
         grayTick:   await client.emojis.cache.get(client.config.emotes.grayTick) || '❕',
-
-        //EMOTES - REPÚBLICA GAMER
-        rg:         await client.emojis.cache.get('498288236607569962'),
-        nitro:      await client.emojis.cache.get('496633448686157826'),
-        rythm:      await client.emojis.cache.get('507187604031275008'),
-        boxbot:     await client.emojis.cache.get('497178946149023744'),
-        chevron1:   await client.emojis.cache.get('497133469110108189'),
-        chevron2:   await client.emojis.cache.get('497133468791341116'),
-        chevron3:   await client.emojis.cache.get('497133468741009411'),
-        chevron4:   await client.emojis.cache.get('497133469059645460'),
-        chevron5:   await client.emojis.cache.get('497133469529538560')
     };
 };

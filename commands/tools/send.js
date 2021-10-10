c409562f59b699c314f8b472dd064578b9402f7c
Daniel Perales Mauriz <dperales555@gmail.com>
2021-10-10 18:37:55 +0200

    Comando "send" arreglado
    Se arregla el comando "send" para que funcione con la nueva versión de Discord.js.

--- commands/tools/send.js
+++ commands/tools/send.js
@@ -3,24 +3,33 @@ exports.run = async (discord, client, message, args, command, commandConfig) => 
     //!send (embed | normal) (texto)
     
     try {
-        let incorrectSyntaxEmbed = new discord.MessageEmbed()
-            .setColor(client.config.colors.error2)
-            .setDescription(`${client.customEmojis.redTick} La sintaxis de este comando es \`-send (embed | normal) (texto)\`.`);
 
-        if (!args[0] || !args[1]) return message.channel.send({ embeds: [incorrectSyntaxEmbed] });
-        
-        let type = args[0];
+        //Comprueba si se han proporcionado los cargumentos correctamente
+        if (!args[0] || !args[1] || (args[0] !== 'embed' && args[0] !== 'normal')) return message.channel.send({ embeds: [
+            new discord.MessageEmbed()
+                .setColor(client.config.colors.secondaryError)
+                .setDescription(`${client.customEmojis.redTick} La sintaxis de este comando es \`${client.config.guild.prefix}send (embed | normal) (texto)\`.`)
+            ]
+        });
+
+        //Almacena el cuerpo del mensaje
         let body = args.slice(1).join(' ');
 
-        if (type !== 'embed' && type !== 'normal') return message.channel.send({ embeds: [incorrectSyntaxEmbed] });
+        //Comprueba si ha de enviar un embed o un mensaje normal
+        if (args[0] === 'embed') {
 
-        if (type === 'embed') {
+            //Genera el embed con el mensaje
             let resultEmbed = new discord.MessageEmbed()
                 .setColor(client.config.colors.primary)
                 .setDescription(body);
+
+            //Envía el embed
             message.channel.send({ embeds: [resultEmbed] });
-        } else if (type === 'normal') {
-            message.channel.send({ embeds: [body] });
+
+        } else if (args[0] === 'normal') {
+
+            //Envía el mensaje en texto plano
+            message.channel.send({ content: body });
         };
     } catch (error) {
         await client.functions.commandErrorHandler(error, message, command, args);

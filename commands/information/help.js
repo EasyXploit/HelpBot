3afcf04ac5f12da4e8ded08fda28c726e0f5248e
Daniel Perales Mauriz <dperales555@gmail.com>
2022-03-19 00:47:42 +0100

    Comando "help" sustituido por "commands"
    Se elimina el comando "help" y se renombra el comando "commands", que ahora pasa a llamarse "help".

--- commands/information/help.js
+++ commands/information/help.js
@@ -1,19 +1,10 @@
 exports.run = async (client, message, args, command, commandConfig) => {
     
-    const packageInfo = require('../../package.json');
-    
     try {
         message.channel.send({ embeds: [new client.MessageEmbed()
-            .setColor(client.config.colors.primary)
-            .setAuthor({ name: packageInfo.normalizedName, iconURL: client.user.avatarURL() })
-            .setTitle(packageInfo.normalizedName)
-            .setURL(packageInfo.homepage)
-            .setThumbnail(client.user.avatarURL())
-            .setDescription(`${packageInfo.description}\n➣Usa el comando \`${client.config.main.prefix}commands\` para descubrir el resto de comandos.`)
-            .addField('✉ Licencia', 'Este proyecto no tiene licencia, por lo que se aplican las leyes de copyright predeterminadas, lo que significa que conservamos todos los derechos de nuestro código fuente y que nadie puede reproducir, distribuir o crear trabajos derivados de este.')
-            .addField('✉ Agradecimientos', '● Maria José Lozano\n● Amish Shah\n● El Pilko (Carlos)', true)
-            .addField('📊 Estadísticas', `${await client.homeGuild.members.fetch().then(members => members.filter(member => !member.user.bot).size)} miembros\nV ${packageInfo.version}`, true)
-            .addField('🗂 Librerias', '● discord.js\n● flaticon', true)
+            .setColor(client.config.colors.information)
+            .setTitle(`${client.customEmojis.grayTick} En desarrollo ...`)
+            .setDescription(`La lista de comandos de **${client.user.username}** se encuentra actualmente en desarrollo.`)
         ]});
     } catch (error) {
         await client.functions.commandErrorHandler(error, message, command, args);
@@ -22,5 +13,5 @@ exports.run = async (client, message, args, command, commandConfig) => {
 
 module.exports.config = {
     name: 'help',
-    aliases: ['info', 'support']
+    aliases: ['commands', 'cmds']
 };

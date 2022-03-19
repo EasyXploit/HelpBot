1b514b02c3396791e1df34ac912d6460630cb183
Daniel Perales Mauriz <dperales555@gmail.com>
2022-03-19 05:32:17 +0100

    Comando "edit" refactorizado
    Se mejora el comando "edit", para añadir comentarios, poder usar menciones en vez de IDs de canales, manejar errores (sobre todo si se trata de mensajes no-editables) y editar tanto embeds como mensajes en texto plano.

--- commands/tools/edit.js
+++ commands/tools/edit.js
@@ -1,27 +1,49 @@
 exports.run = async (client, message, args, command, commandConfig) => {
     
     try {
-        
-        let incorrectSyntaxEmbed = new client.MessageEmbed()
+    
+        //Comprueba si los parámetros se han proporcionado correctamente
+        if (!args[0] || !args[1] || !args[2]) return message.channel.send({ embeds: [ new client.MessageEmbed()
             .setColor(client.config.colors.error)
-            .setDescription(`${client.customEmojis.redTick} La sintaxis de este comando es:\n\`${client.config.main.prefix}${command}${commandConfig.export.parameters.length > 0 ? ' ' + commandConfig.export.parameters : ''}\`.`);
-        
-        if (!args[0] || !args[1] || !args[2]) return message.channel.send({ embeds: [incorrectSyntaxEmbed] });
+            .setDescription(`${client.customEmojis.redTick} La sintaxis de este comando es:\n\`${client.config.main.prefix}${command}${commandConfig.export.parameters.length > 0 ? ' ' + commandConfig.export.parameters : ''}\`.`)
+        ]});
             
-        const channel = message.guild.channels.cache.find(c => c.id === args[0]);
-        if (!channel) return message.channel.send({ embeds: [incorrectSyntaxEmbed] });
+        //Busca el canal en la guild
+        const channel = await client.functions.fetchChannel(message.guild, args[0]);
+
+        //Devuelve un error si no se ha encontrado el canal
+        if (!channel) return message.channel.send({ embeds: [ new client.MessageEmbed()
+            .setColor(client.config.colors.error)
+            .setDescription(`${client.customEmojis.redTick} El canal ${!isNaN(args[0] ? `con ID ${args[0]} ` : args[0])} no existe.`)
+        ]});
         
+        //Busca el mensaje en el canal
         const msg = await channel.messages.fetch(args[1]);
-        if (!msg) return message.channel.send({ embeds: [incorrectSyntaxEmbed] });
+        if (!msg) return message.channel.send({ embeds: [ new client.MessageEmbed()
+            .setColor(client.config.colors.error)
+            .setDescription(`${client.customEmojis.redTick} El mensaje con ID \`${args[1]}\` no existe.`)
+        ]});
 
-        const newContent = message.content.slice(command.length - 2 + args[0].length + 1 + args[0].length + 2);
-        if (!newContent) return message.channel.send({ embeds: [incorrectSyntaxEmbed] });
+        //Comprueba si el mensaje es del bot
+        if (msg.author.id !== client.user.id) return message.channel.send({ embeds: [ new client.MessageEmbed()
+            .setColor(client.config.colors.error)
+            .setDescription(`${client.customEmojis.redTick} No puedo editar un mensaje que no he enviado.`)
+        ]});
 
-        let newEmbed = new client.MessageEmbed()
-            .setColor(client.config.colors.primary)
-            .setDescription(newContent);
-        
-        msg.edit({ embeds: [newEmbed] });
+        //Almacena el nuevo cuerpo del mensaje
+        const newContent = args.splice(2).join(' ');
+
+        //Comprueba si ha de modificar un embed o un mensaje de texto plano
+        if (msg.embeds.length > 0) {
+
+            //Edita el embed con el nuevo contenido
+            msg.edit({ embeds: [ new client.MessageEmbed()
+                .setColor(client.config.colors.primary)
+                .setDescription(newContent)
+            ]});
+
+        } else msg.edit({ content: newContent }); //Edita el embed con el nuevo contenido
+
     } catch (error) {
         await client.functions.commandErrorHandler(error, message, command, args);
     };
@@ -31,5 +53,5 @@ module.exports.config = {
     name: 'edit',
     description: 'Edita un mensaje enviado previamente por el bot.',
     aliases: [],
-    parameters: '<ID del canal> <ID del mensaje> <nuevo contenido>'
+    parameters: '<#canal | id> <ID del mensaje> <nuevo contenido>'
 };

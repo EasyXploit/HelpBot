ee7d27af7712cddedd0be65e3333f913d8eeecf6
Daniel Perales Mauriz <dperales555@gmail.com>
2022-03-19 19:29:31 +0100

    Comando "ping" refactorizado
    Se refactoriza el comando "ping".

--- commands/information/ping.js
+++ commands/information/ping.js
@@ -1,36 +1,36 @@
 exports.run = async (client, message, args, command, commandConfig) => {
     
     try {
-        //TIEMPO DE RESPUESTA DEL BOT
-        let botPing = new Date().getTime() - message.createdTimestamp;
+
+        //Mide el tiempo de respuesta del bot
+        const botPing = new Date().getTime() - message.createdTimestamp;
+
+        //Genera un embed para el tiempo de respuesta
         let botPingEmbed = new client.MessageEmbed()
             .setTitle('Tiempo de respuesta del bot: ')
             .setDescription(`:stopwatch: | ${botPing} ms`);
 
-        if (botPing <= 180) {
-            botPingEmbed.setColor('7ED321');
-        } else if (botPing > 180 && botPing <= 250) {
-            botPingEmbed.setColor('F5A623');
-        } else {
-            botPingEmbed.setColor(client.config.colors.secondaryError);
-        }
+        //Asigna un color al embed en función del valor del ping
+        if (botPing <= 180) botPingEmbed.setColor('7ED321');
+        else if (botPing > 180 && botPing <= 250) botPingEmbed.setColor('F5A623');
+        else botPingEmbed.setColor(client.config.colors.secondaryError);
 
-        //TIEMPO DE RESPUESTA DEL WEBSOCKET
-        let websocketPing = Math.floor(client.ws.ping);
+        //Mide el tiempo de respuesta del websocket
+        const websocketPing = Math.floor(client.ws.ping);
+
+        //Genera un embed para el tiempo de respuesta
         let websocketPingEmbed = new client.MessageEmbed()
             .setTitle('Tiempo de respuesta del Websocket: ')
             .setDescription(`:stopwatch: | ${websocketPing} ms`);
 
-        if (websocketPing <= 180) {
-            websocketPingEmbed.setColor('7ED321');
-        } else if (websocketPing > 180 && websocketPing <= 250) {
-            websocketPingEmbed.setColor('F5A623');
-        } else {
-            websocketPingEmbed.setColor(client.config.colors.secondaryError);
-        }
+        //Asigna un color al embed en función del valor del ping
+        if (websocketPing <= 180) websocketPingEmbed.setColor('7ED321');
+        else if (websocketPing > 180 && websocketPing <= 250) websocketPingEmbed.setColor('F5A623');
+        else websocketPingEmbed.setColor(client.config.colors.secondaryError);
 
-        //ENVIO DE EMBEDS
+        //Envía los embeds generados
         await message.channel.send({ embeds: [ botPingEmbed, websocketPingEmbed] });
+        
     } catch (error) {
         await client.functions.commandErrorHandler(error, message, command, args);
     };

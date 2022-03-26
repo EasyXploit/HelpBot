29f887312d2fc0e856706c3083a37f0cd3003575
Daniel Perales Mauriz <dperales555@gmail.com>
2022-03-27 00:29:29 +0100

    Infracciones paginadas
    Se mejora el comando "infractions" para permitir navegar entre páginas a la hora de visualizar miembros con más de 10 advertencias acumuladas.

--- commands/moderation/infractions.js
+++ commands/moderation/infractions.js
@@ -2,11 +2,10 @@ exports.run = async (client, message, args, command, commandConfig) => {
 
     try {
 
-        let noBotsEmbed = new client.MessageEmbed()
-            .setColor(client.config.colors.secondaryError)
-            .setDescription(`${client.customEmojis.redTick} No puedes obtener información de un bot`);
-
+        //Busca el miembro en cuestión
         const member = await client.functions.fetchMember(message.guild, args[0] || message.author.id);
+
+        //Si no se encuentra, devuelve un error
         if (!member) return await client.functions.syntaxHandler(message.channel, commandConfig);
 
         //Comprueba, si corresponde, que el miembro tenga permiso para ver el historial de otros
@@ -32,68 +31,149 @@ exports.run = async (client, message, args, command, commandConfig) => {
             }).then(msg => {setTimeout(() => msg.delete(), 5000)});
         };
 
-        let user = member.user;
-        if (user.bot) return message.channel.send({ embeds: [noBotsEmbed] });
+        //Comprueba si se trata de un bot
+        if (member.user.bot) return message.channel.send({ embeds: [ new client.MessageEmbed()
+            .setColor(client.config.colors.secondaryError)
+            .setDescription(`${client.customEmojis.redTick} No puedes obtener información de un bot`)
+        ]});
 
-        //Comprueba el número de warns del miembro
-        let warns;
-        if (!client.db.warns[member.id]) {
-            warns = 0
-        } else {
-            warns = client.db.warns[member.id].length;
-        }
+        //Función para cargar un rango de advertencias
+        async function loadWarns(fromRange, toRange) {
 
-        let infractionsCount = {
-            day: 0,
-            week: 0,
-            total: 0
-        }
+            //Almacena la descripción del embed
+            let board = '';
 
-        let lastWarns = '';
+            //Por cada una de lps índices del rango
+            for (let index = fromRange - 1; index < toRange; index++) {
 
+                //Si no hay más advertencias, para el bucle
+                if (!memberWarns[index]) break;
+
+                //Almacena las claves de la advertencia
+                let warn = Object.values(memberWarns[index])[0];
+
+                //Busca y almacena el moderador que aplicó la sanción
+                const moderator = await client.functions.fetchMember(client.homeGuild, warn.moderator) || 'Moderador desconocido';
+
+                //Añade una nueva fila con los detalles de la advertencia
+                board += `\`${Object.keys(memberWarns[index])[0]}\` • ${moderator} • <t:${Math.round(new Date(parseInt(warn.timestamp)) / 1000)}>\n${warn.reason}\n\n`;
+            };
+
+            //Cuando acaba, devuelve la tabla
+            return board;
+        };
+
+        //Función para cargar una página determinada
+        async function loadEmbed(embed) {
+
+            //Almacena la sanción actyal, si aplica
+            let sanction;
+
+            //Comprueba qué tipo de sanción tiene el miembro (si la tiene)
+            if (client.db.mutes[member.id]) sanction = `Silenciado hasta <t:${Math.round(new Date(client.db.mutes[member.id].time) / 1000)}>`;
+            else if (member.roles.cache.has(client.config.dynamic.mutedRoleId)) sanction = 'Silenciado indefinidamente';
+
+            //Genera el embed de las infracciones
+            let infractionsEmbed = new client.MessageEmbed()
+                .setColor(client.config.colors.primary)
+                .setTitle('⚠ Advertencias')
+                .setDescription(`Mostrando las advertencias del miembro **${member.user.tag}**\nSanción actual: \`${sanction || 'Ninguna'}\`.`)
+                .setThumbnail(member.user.displayAvatarURL({dynamic: true}))
+                .addField('Últimas 24h', onDay.toString(), true)
+                .addField('Últimos 7 días', onWeek.toString(), true)
+                .addField('Total', total.toString(), true)
+                .addField('Listado de advertencias', total > 0 ? await loadWarns(10 * actualPage - 9, 10 * actualPage) : 'Ninguna')
+                .setFooter({ text: `Página ${actualPage} de ${totalPages}`, iconURL: client.homeGuild.iconURL({dynamic: true}) });
+
+            //Comprueba si ha de editar o enviar el embed
+            if (embed) await embed.edit({ embeds: [infractionsEmbed] }).then(async embed => { awaitReactions(embed) });
+            else await message.channel.send({ embeds: [infractionsEmbed] }).then(async embed => { awaitReactions(embed) });
+        };
+
+        //Función para manejar el menú de reacciones
+        async function awaitReactions(embed) {
+
+            //Borra todas las reacciones previas (si tiene)
+            if (embed.reactions) await embed.reactions.removeAll();
+
+            //Reacciona con el menú correspondiente según la página
+            if (actualPage > 1) await embed.react('⏮');
+            if (actualPage > 1) await embed.react('◀');
+            if (actualPage < totalPages) await embed.react('▶');
+            if (actualPage < totalPages) await embed.react('⏭');
+
+            //Crea el filtro de reacciones según la página
+            const filter = (reaction, user) => {
+
+                //Crea un array de reacciones válidas
+                let validReactions = [];
+
+                //Si no es la primera página
+                if (actualPage > 1) validReactions.push('⏮', '◀');
+
+                //Si no es la última página
+                if (actualPage < totalPages) validReactions.push('▶', '⏭');
+
+                //Devuelve si el filtro encajó con la reacción
+                return validReactions.includes(reaction.emoji.name) && user.id === message.author.id;
+            };
+
+            //Inicializa un listener de reaccciones en función del filtro
+            embed.awaitReactions({ filter, max: 1, time: 60000, errors: ['time'] }).then(async collected => {
+
+                //Almacena la primera reacción
+                const reaction = collected.first();
+
+                //Según el emoji reaccionado, actualiza el contador consecutivamente
+                if (reaction.emoji.name === '⏮' && actualPage > 1) actualPage = 1;
+                if (reaction.emoji.name === '◀' && actualPage > 1) actualPage = actualPage - 1;
+                if (reaction.emoji.name === '▶' && actualPage < totalPages) actualPage = actualPage + 1;
+                if (reaction.emoji.name === '⏭' && actualPage < totalPages) actualPage = totalPages;
+
+                //Carga la nueva página
+                loadEmbed(embed);
+
+            //Si no se hace nada, borra las reacciones
+            }).catch(() => embed.reactions.removeAll());
+        };
+
+        //Almacena la página actual
+        let actualPage = 1;
+
+        //Almacena el total de sanciones, en 1 día y 1 semana, y las páginas totales
+        let onDay = onWeek = total = totalPages = 0;
+
+        //Almacena las advertencias del miembro
+        let memberWarns = client.db.warns[member.id];
+
+        //Si el miembro tenía advertencias
         if (client.db.warns[member.id]) {
-            infractionsCount.total = Object.keys(client.db.warns[member.id]).length;
 
-            Object.values(client.db.warns[member.id]).forEach(entry => {
-                if (Date.now() - entry.timestamp <= '86400000') infractionsCount.day++
-                if (Date.now() - entry.timestamp <= '604800000') infractionsCount.week++
+            //Mapea las advertencias del miembro
+            memberWarns = Object.entries(client.db.warns[member.id]).reverse().map((warn) => ({ [warn[0]]: warn[1] }));
+
+            //Almacena el total de ellas
+            total = memberWarns.length
+
+            //Almacena el total de páginas
+            totalPages = memberWarns ? Math.ceil(memberWarns.length / 10) : 0;
+
+            //Comprueba si se ha proporcionado una página inicial
+            if (args[1] && !isNaN(args[1]) && args[1] > 0 && args[1] <= totalPages) actualPage = args[1];
+
+            //Por cada advertencia del miembro
+            Object.values(memberWarns).forEach(warn => {
+
+                //Si ocurrió en un día o menos, lo contabiliza
+                if (Date.now() - warn.timestamp <= '86400000') onDay++;
+
+                //Si ocurrió en una semana o menos, lo contabiliza
+                if (Date.now() - warn.timestamp <= '604800000') onWeek++;
             });
+        };
 
-            let userWarns = Object.entries(client.db.warns[member.id]).map((e) => ( { [e[0]]: e[1] } ));
-
-            for (let i = userWarns.length; i >= userWarns.length - 10; i--) {
-                if (!userWarns[i]) continue;
-
-                Object.keys(userWarns[i]).forEach((key) => {
-                    let moderator = message.guild.members.cache.get(userWarns[i][key].moderator);
-                    if (typeof moderator === 'undefined') moderator = 'Moderador desconocido';
-                    lastWarns = lastWarns + `\`${key}\` • ${moderator} • <t:${Math.round(new Date(parseInt(userWarns[i][key].timestamp)) / 1000)}>\n${userWarns[i][key].reason}\n\n`;
-                })
-            }
-
-        }
-
-        //Comprueba si existe el rol silenciado, sino lo crea
-        let role = await client.functions.checkMutedRole(message.guild);
-
-        let sanction;
-        if (client.db.mutes[member.id]) {
-            sanction = `Silenciado hasta <t:${Math.round(new Date(client.db.mutes[member.id].time) / 1000)}>`;
-        } else if (member.roles.cache.has(role.id)) {
-            sanction = 'Silenciado indefinidamente';
-        }
-
-        let resultEmbed = new client.MessageEmbed()
-            .setColor(client.config.colors.primary)
-            .setTitle(`⚠ Infracciones`)
-            .setDescription(`Mostrando las advertencias del miembro **${member.user.tag}**\nSanción actual: \`${sanction || 'Ninguna'}\`.`)
-            .setThumbnail(user.displayAvatarURL({dynamic: true}))
-            .addField(`Últimas 24h`, infractionsCount.day.toString(), true)
-            .addField(`Últimos 7 días`, infractionsCount.week.toString(), true)
-            .addField(`Total`, infractionsCount.total.toString(), true)
-            .addField(`Últimas 10 advertencias`, lastWarns || 'Ninguna');
-        
-        message.channel.send({ embeds: [resultEmbed] });
+        //Genera el primer embed
+        loadEmbed();
         
     } catch (error) {
 

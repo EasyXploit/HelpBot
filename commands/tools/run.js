22ef917ce141421adb61b66549ed8b01ed98fe93
Daniel Perales Mauriz <dperales555@gmail.com>
2022-04-11 22:28:15 +0200

    Comando "run" refactorizado
    Se refactoriza el comando "run".

--- commands/tools/run.js
+++ commands/tools/run.js
@@ -2,108 +2,87 @@ exports.run = async (client, message, args, command, commandConfig) => {
     
     try {
 
-        //Envía un mensaje de error si no se ha proporcionado un comando
+        //Envía un mensaje de error si no se ha proporcionado un parámetro (comando)
         if (!args[0]) return await client.functions.syntaxHandler(message.channel, commandConfig);
 
+        //En función del comando especificado
         switch (args[0]) {
-            case 'welcome':  //Emite el evento "guildMemberAdd"
+            
+            case 'welcome': //Emite el evento "guildMemberAdd"
                 
                 //Devuelve un error si no se ha proporcionado un miembro
-                if (!args[1]) return message.channel.send({ embeds: [
-                    new client.MessageEmbed()
-                        .setColor(client.config.colors.error)
-                        .setDescription(`${client.customEmojis.redTick} No has proporcionado un miembro`)
-                    ]
-                });
+                if (!args[1]) return message.channel.send({ embeds: [ new client.MessageEmbed()
+                    .setColor(client.config.colors.error)
+                    .setDescription(`${client.customEmojis.redTick} No has proporcionado un miembro`)
+                ]});
 
                 //Busca el miembro en la guild
                 const targetWelcomeMember = await client.functions.fetchMember(message.guild, args[1]);
 
                 //Devuelve un error si no se ha proporcionado un miembro válido
-                if (!targetWelcomeMember) return message.channel.send({ embeds: [
-                    new client.MessageEmbed()
-                        .setColor(client.config.colors.error)
-                        .setDescription(`${client.customEmojis.redTick} No has proporcionado un miembro válido`)
-                    ]
-                });
+                if (!targetWelcomeMember) return message.channel.send({ embeds: [ new client.MessageEmbed()
+                    .setColor(client.config.colors.error)
+                    .setDescription(`${client.customEmojis.redTick} No has proporcionado un miembro válido`)
+                ]});
                 
                 //Emite el evento
-                await client.emit(`guildMemberAdd`, targetWelcomeMember);
+                await client.emit('guildMemberAdd', targetWelcomeMember);
 
                 //Envía un mensaje de confirmación
-                await message.channel.send({ embeds: [
-                    new client.MessageEmbed()
-                        .setColor(client.config.colors.secondaryCorrect)
-                        .setDescription(`${client.customEmojis.greenTick} ¡Listo!`)
-                    ]
-                }).then(msg => {setTimeout(() => msg.delete(), 2000)});
+                await message.channel.send({ embeds: [ new client.MessageEmbed()
+                    .setColor(client.config.colors.secondaryCorrect)
+                    .setDescription(`${client.customEmojis.greenTick} ¡Listo!`)
+                ]}).then(msg => {setTimeout(() => msg.delete(), 2000)});
+
+                //Para la ejecución
                 break;
         
-            case 'goodbye':   //Emite el evento "guildMemberRemove"
+            case 'goodbye': //Emite el evento "guildMemberRemove"
                 
                 //Devuelve un error si no se ha proporcionado un miembro
-                if (!args[1]) return message.channel.send({ embeds: [
-                    new client.MessageEmbed()
-                        .setColor(client.config.colors.error)
-                        .setDescription(`${client.customEmojis.redTick} No has proporcionado un miembro`)
-                    ]
-                });
+                if (!args[1]) return message.channel.send({ embeds: [ new client.MessageEmbed()
+                    .setColor(client.config.colors.error)
+                    .setDescription(`${client.customEmojis.redTick} No has proporcionado un miembro`)
+                ]});
 
                 //Busca el miembro en la guild
                 const targetGoodybeMember = await client.functions.fetchMember(message.guild, args[1]);
 
                 //Devuelve un error si no se ha proporcionado un miembro válido
-                if (!targetGoodybeMember) return message.channel.send({ embeds: [
-                    new client.MessageEmbed()
-                        .setColor(client.config.colors.error)
-                        .setDescription(`${client.customEmojis.redTick} No has proporcionado un miembro válido`)
-                    ]
-                });
+                if (!targetGoodybeMember) return message.channel.send({ embeds: [ new client.MessageEmbed()
+                    .setColor(client.config.colors.error)
+                    .setDescription(`${client.customEmojis.redTick} No has proporcionado un miembro válido`)
+                ]});
                 
                 //Emite el evento
-                await client.emit(`guildMemberRemove`, targetGoodybeMember);
+                await client.emit('guildMemberRemove', targetGoodybeMember);
 
                 //Envía un mensaje de confirmación
-                await message.channel.send({ embeds: [
-                    new client.MessageEmbed()
-                        .setColor(client.config.colors.secondaryCorrect)
-                        .setDescription(`${client.customEmojis.greenTick} ¡Listo!`)
-                    ]
-                }).then(msg => {setTimeout(() => msg.delete(), 2000)});
+                await message.channel.send({ embeds: [ new client.MessageEmbed()
+                    .setColor(client.config.colors.secondaryCorrect)
+                    .setDescription(`${client.customEmojis.greenTick} ¡Listo!`)
+                ]}).then(msg => {setTimeout(() => msg.delete(), 2000)});
+
+                //Para la ejecución
                 break;
         
-            case 'assortStats':
-
-                /*
-                    //OBJETIVO:
-                    Mediante la adición de la XP que se genera al cumplir un intervalo de XP por voz,
-                    esta función fuerza al sistema a disparar la actualización de los roles de los
-                    miembros a los que les corresponda subir de nivel.
-
-                    Esto es util en el caso de que se hayan añadido nuevos roles de recompensa
-                    intercalados entre los roles de recompensa actuales.
-
-                    //ACTUALIZACIÓN DE NIVEL:
-                    Si además se proporciona un parámetro numérico adicional, el programa entenderá
-                    que debe actualizar el nivel de los miembros al número proporcionado, en caso de
-                    que su nivel sea superior.
-                */
+            case 'assortStats': //Para encuadrar stats
 
                 //Envía una confirmación de inicio a la consola
                 console.log(`${new Date().toLocaleString()} 》Comenzando operación de cuadre de stats ...\n\n`);
 
                 //Envía una confirmación de inicio al canal
-                await message.channel.send({ embeds: [
-                    new client.MessageEmbed()
-                        .setColor(client.config.colors.information)
-                        .setTitle(`${client.customEmojis.grayTick} Cuadrando stats`)
-                        .setDescription('Comenzando operación de cuadre de la tabla de clasificación.\nRevisa la consola para evaluar el progreso del comando.')
-                    ]
-                });
+                await message.channel.send({ embeds: [ new client.MessageEmbed()
+                    .setColor(client.config.colors.information)
+                    .setTitle(`${client.customEmojis.grayTick} Cuadrando stats`)
+                    .setDescription('Comenzando operación de cuadre de la tabla de clasificación.\nRevisa la consola para evaluar el progreso del comando.')
+                ]});
 
                 //Almacena la tabla de clasificación de la guild
                 const guildStats = client.db.stats[message.guild.id];
-                const IDs = Object.keys(guildStats);    //Almacena los IDs de cada miembro
+
+                //Almacena los IDs de cada miembro
+                const IDs = Object.keys(guildStats);
 
                 //Inicializa un contador
                 let index = 0;
@@ -112,10 +91,10 @@ exports.run = async (client, message, args, command, commandConfig) => {
                 let interval = setInterval(async function() {
 
                     //Busca el miembro por su ID
-                    let member = await client.functions.fetchMember(message.guild, IDs[index]);
+                    const member = await client.functions.fetchMember(message.guild, IDs[index]);
 
                     //Busca las estadísticas del miembro en la tabla de clasificación
-                    let userStats = guildStats[member.id];
+                    const userStats = guildStats[member.id];
 
                     //Si se escpecifica, se cambiará el nivel del miembro por el número indicado
                     if (args[1] && !isNaN(args[1]) && (userStats && userStats.level > args[1])) userStats.level = args[1];
@@ -132,6 +111,7 @@ exports.run = async (client, message, args, command, commandConfig) => {
                             //Añade al miembro la cantidad de XP generada al cumplirse un intervalo de voz, einforma por consola
                             await client.functions.addXP(member, message.guild, 'voice').then(console.log(`Miembro ${member.displayName} actualizado\n- - - - - -`));
                         };
+
                     } else {
 
                         //Advierte por consola, de que el miembro se ha omitido
@@ -151,26 +131,26 @@ exports.run = async (client, message, args, command, commandConfig) => {
                         console.log(`\n\n${new Date().toLocaleString()} 》Operación finalizada`);
 
                         //Envía una confirmación de finalización al canal
-                        await message.channel.send({ embeds: [
-                            new client.MessageEmbed()
-                                .setColor(client.config.colors.secondaryCorrect)
-                                .setTitle(`${client.customEmojis.greenTick} Stats encuadradas`)
-                                .setDescription('La operación de encuadre de estadísticas ha finalizado.\nPor favor, realice una revisión manual para confirmar su efectividad.')
-                            ]
-                        });
+                        await message.channel.send({ embeds: [ new client.MessageEmbed()
+                            .setColor(client.config.colors.secondaryCorrect)
+                            .setTitle(`${client.customEmojis.greenTick} Stats encuadradas`)
+                            .setDescription('La operación de encuadre de estadísticas ha finalizado.\nPor favor, realice una revisión manual para confirmar su efectividad.')
+                        ]});
                     };
                 }, 1000);
+
+                //Para la ejecución
                 break;
             
-            default:
+            default: //En caso de que no se haya proporcionado un comando existente
 
-                //Envía un mensaje de error si no se ha proporcionado un comando válido
-                await message.channel.send({ embeds: [
-                    new client.MessageEmbed()
-                        .setColor(client.config.colors.error)
-                        .setDescription(`${client.customEmojis.redTick} No has especificado una plantilla válida`)
-                    ]
-                });
+                //Envía un mensaje de error
+                await message.channel.send({ embeds: [ new client.MessageEmbed()
+                    .setColor(client.config.colors.error)
+                    .setDescription(`${client.customEmojis.redTick} No has especificado una plantilla válida`)
+                ]});
+
+                //Para la ejecución
                 break;
         };
     } catch (error) {

f1a6bd87aec140589451eb95aa7c1c6665751add
Daniel Perales Mauriz <dperales555@gmail.com>
2022-04-11 20:50:52 +0200

    Comando "roleinfo" refactorizado
    Se refactoriza el comando "roleinfo".

--- commands/tools/roleinfo.js
+++ commands/tools/roleinfo.js
@@ -2,52 +2,33 @@ exports.run = async (client, message, args, command, commandConfig) => {
 
     try {
 
+        //Comprueba si los parámetros se han proporcionado correctamente
         if (!args[0]) return await client.functions.syntaxHandler(message.channel, commandConfig);
 
-        await client.functions.fetchRole(message.guild, args[0]).then(role => {
+        //Busca el rol en la guild
+        const role = await client.functions.fetchRole(message.guild, args[0]);
 
-            let roleNotFoundEmbed = new client.MessageEmbed()
-                .setColor(client.config.colors.secondaryError)
-                .setDescription(`${client.customEmojis.redTick} El rol no se ha podido encontrar`);
+        //Si el rol no existe, devuelve un error
+        if (!role) return message.channel.send({ embeds: [ new client.MessageEmbed()
+            .setColor(client.config.colors.secondaryError)
+            .setDescription(`${client.customEmojis.redTick} El rol **${args[0]}** no existe`)
+        ]});
 
-            if (!role) return message.channel.send({ embeds: [roleNotFoundEmbed] });
+        //Envía un embed con la información del rol
+        await message.channel.send({ embeds: [ new client.MessageEmbed()
+            .setColor(role.hexColor)
+            .setTitle('🔖 Información de rol')
+            .setDescription(`Mostrando información acerca del rol ${role}`)
+            .addField('🏷 Nombre del rol', role.name, true)
+            .addField('🆔 ID del rol', role.id, true)
+            .addField('👥 Miembros con el rol', role.members.size.toString(), true)
+            .addField('🗣 Mencionable', role.mentionable ? 'Si' : 'No', true)
+            .addField('👁️‍ Se muestra', role.hoist ? 'Visible' : 'Oculto', true)
+            .addField('🔰 Color', role.hexColor, true)
+            .addField('📝 Fecha de creación', `<t:${Math.round(role.createdTimestamp / 1000)}>`, true)
+            .addField('⚙ Administración', role.managed ? 'Externa' : 'Local', true)
+        ]});
 
-            let membersWithRole = message.guild.roles.cache.get(role.id).members.size;
-            let mentionable = 'No';
-            let hoisted = 'Oculto';
-            let managed = 'Local';
-
-            if (role.mentionable === true) {
-                mentionable = 'Si'
-            };
-            
-            if (role.hoist === true) {
-                hoisted = 'Visible'
-            };
-            
-            if (role.managed === true) {
-                managed = 'Externa'
-            };
-
-            let resultEmbed = new client.MessageEmbed()
-                .setColor(role.hexColor)
-                .setTitle('🔖 Información de rol')
-                .setDescription(`Mostrando información acerca del rol ${role}`)
-                .addField('🏷 Nombre del rol', role.name, true)
-                .addField('🆔 ID del rol', role.id, true)
-                .addField('👥 Miembros con el rol', membersWithRole.toString(), true)
-                .addField('🗣 Mencionable', mentionable, true)
-                .addField('👁️‍ Se muestra', hoisted, true)
-                .addField('🔰 Color', role.hexColor, true)
-                .addField('📝 Fecha de creación', `<t:${Math.round(role.createdTimestamp / 1000)}>`, true)
-                .addField('⚙ Administración', managed, true)
-
-            message.channel.send({ embeds: [resultEmbed] });
-        }).catch(error => {
-            console.error(`${new Date().toLocaleString()} 》ERROR:`, error.stack);
-        });
-
-        
     } catch (error) {
 
         //Ejecuta el manejador de errores

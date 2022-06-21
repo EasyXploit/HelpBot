25f002c038c5faa0285c4088698b09c1b1d86480
Daniel Perales Mauriz <dperales555@gmail.com>
2022-06-21 16:20:13 +0200

    Corrección de errores
    Se corrige un error en la cadena que se muestra durante el log del evento shardError.

--- events/shardError.js
+++ events/shardError.js
@@ -1,3 +1,3 @@
 exports.run = (error, client, locale) => {
-    console.error(`${new Date().toLocaleString()} 》${locale.errorMessage}:`, error.stack);
+    console.error(`${new Date().toLocaleString()} 》 `, error.stack);
 };

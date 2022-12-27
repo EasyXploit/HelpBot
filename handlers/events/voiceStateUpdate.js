dace1ae2dcc9f6c0ddb371f82a721b6437f03b18
Daniel Perales Mauriz <dperales555@gmail.com>
2022-12-27 01:31:29 +0100

    Voice moves logging logic
    Added a preview version with hardcoded dialogs of the logic to send movement log messages on voice channels.

--- handlers/events/voiceStateUpdate.js
+++ handlers/events/voiceStateUpdate.js
@@ -5,6 +5,43 @@ exports.run = async (oldState, newState, client, locale) => {
         //Aborta si no es un evento de la guild registrada
         if (oldState.guild.id !== client.homeGuild.id) return;
 
+        // ---- VOICE MOVES (DEV BUILD) ----
+
+        //
+        if (client.config.logging.voiceMoves && !(client.config.main.voiceMovesExcludedChannels.includes(oldState.channelId) && client.config.main.voiceMovesExcludedChannels.includes(newState.channelId))) {
+
+            //
+            const oldChannel = oldState.channelId && !client.config.main.voiceMovesExcludedChannels.includes(oldState.channelId) ? `<#${oldState.channel.id}>` : `\`Ninguno\``;
+            const newChannel = newState.channelId && !client.config.main.voiceMovesExcludedChannels.includes(newState.channelId) ? `<#${newState.channel.id}>` : `\`Ninguno\``;
+
+            //
+            let embedFields = [
+                { name: 'Anterior canal', value: oldChannel, inline: true },
+                { name: 'Nuevo canal', value: newChannel, inline: true }
+            ];
+
+            //
+            if (newState.channelId && !client.config.main.voiceMovesExcludedChannels.includes(newState.channelId)) {
+
+                //
+                const channelMembers = Array.from(newState.channel.members, member => newState.channel.members.get(member[0]).user.tag);
+
+                //
+                embedFields.push({ name: `Miembros del nuevo canal (${newState.channel.members.size}/${newState.channel.userLimit != 0 ? newState.channel.userLimit : '∞'})`, value: `\`\`\`${channelMembers.join(', ')}\`\`\``});
+            };
+    
+            //Se envía un registro al canal especificado en la configuración
+            await client.voiceMovesChannel.send({ embeds: [ new client.MessageEmbed()
+                .setColor(client.config.colors.logging)
+                .setAuthor({ name: `${newState.member.user.tag} ha modificado su canal de voz`, iconURL: newState.member.user.displayAvatarURL({dynamic: true}) })
+                .setFields(embedFields)
+                .setTimestamp()
+            ]});
+        };
+
+
+        // ---- ---- ----
+
         //Si el bot está conectado
         if (newState.guild.me.voice.channelId) {
 

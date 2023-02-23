3c2ccd87c552b548576bbdb50657d2012718398b
Daniel Perales Mauriz <dperales555@gmail.com>
2023-02-23 22:31:25 +0100

    Reproduction removed
    Removed all code related to local and YouTube track playback features.

--- functions/utilities/getMultipleRandom.js
+++
@@ -1,9 +0,0 @@
-//Función para aleatorizar un array y obtener n items
-exports.run = async (array, quantity) => {
-
-    //Aleatoriza el array
-    const shuffledArray = [...array].sort(() => 0.5 - Math.random());
-    
-    //Devuelve la cantidad elegida de items
-    return shuffledArray.slice(0, quantity);
-};

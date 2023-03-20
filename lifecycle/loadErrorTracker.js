d3416ac4011ee494f4c64e34d7336db98f426091
Daniel Perales Mauriz <dperales555@gmail.com>
2023-03-20 23:57:46 +0100

    Exception catching fix
    Fixed an issue where exceptions were caught by Sentry when running with the NODE_ENV variable set to "development".

--- lifecycle/loadErrorTracker.js
+++ lifecycle/loadErrorTracker.js
@@ -11,17 +11,15 @@ exports.run = async (locale) => {
         //Se importa de @sentry/tracing para parchear el centro global para que funcione el rastreo
         /*const sentryTracing = */require("@sentry/tracing");
 
-        //Almacena la tasa de recogida de muestras correspondiente en función del entorno
-        const tracesSampleRate = process.env.NODE_ENV === 'development' ? 0 : 1.0
-
         //Inicializa la conexión con Sentry
         sentry.init({
             dsn: 'https://fc5a7a66ed7044218b518005e0a48bc6@o1195666.ingest.sentry.io/4504454240075776', //Proporciona el DSN (Data Source Name)
-            tracesSampleRate: tracesSampleRate  //Proporciona la tasa de recogida de muestras
+            enabled: process.env.NODE_ENV === 'production' ? true : false, //Determina si se ha de recoger muestras o no
+            tracesSampleRate: 1.0  //Proporciona la tasa de recogida de muestras
         });
 
         //Muestra un mensaje de confirmación en la consola
-        console.log(` - ${locale.tracesSampleRate}: ${tracesSampleRate}\n\n${locale.configLoaded}\n`);
+        console.log(` - ${locale.tracesSampleRate}: ${process.env.NODE_ENV === 'production' ? 1.0 : 0}\n\n${locale.configLoaded}\n`);
 
     } catch (error) {
 

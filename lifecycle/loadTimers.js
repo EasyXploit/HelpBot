3391ec6bf9f57abc92bed149964be9d12c371780
Daniel Perales Mauriz <dperales555@gmail.com>
2023-03-27 20:12:22 +0200

    Timers status from DB
    Now the timers status configuration is stored and retrieved from the DB under the "modules" key of the "system" document.

--- lifecycle/loadTimers.js
+++ lifecycle/loadTimers.js
@@ -4,7 +4,7 @@ exports.run = async (client) => {
     const locale = client.locale.lifecycle.loadTimers;
 
     //Aborta si los timers están deshabilitados
-    if (!client.config.main.timers) return;
+    if (!await client.functions.db.getConfig.run('system.modules.timers')) return;
 
     //Aborta si los timers no son un array
     if (!Array.isArray(client.config.timers)) return;

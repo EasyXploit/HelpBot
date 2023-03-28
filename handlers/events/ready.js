eae885a19f729d66b522ddd3c8d5fec1699b86cc
Daniel Perales Mauriz <dperales555@gmail.com>
2023-03-28 19:54:42 +0200

    Backport of better guilds count handling
    This commit backports 2d3a759534c747ab9cdb912269dc830051ecd541 and 5c01f9a84f397f5a6c8bfd580ba1384dadd12e5a.

--- handlers/events/ready.js
+++ handlers/events/ready.js
@@ -4,15 +4,27 @@ exports.run = async (event, client, locale) => {
         
         //Carga el listado de guilds a las que el bot está unido
         const cachedGuilds = client.guilds.cache;
+
+        //Carga el ID de la guild base
+        const homeGuildId = client.config.dynamic.homeGuild;
+
+        //Carga el ID de la guild de servicio
+        const serviceGuildId = client.config.dynamic.serviceGuildId;
+
+        //Almacena el número de guilds válidas (descontando las de servicio)
+        const validGuildsCount = await cachedGuilds.filter(guild => guild.id !== serviceGuildId);
         
         //Comprueba cuantas guilds hay disponibles
-        if (cachedGuilds.size > 1) {    //Si la cantidad es superior a 1
+        if (validGuildsCount > 1) {    //Si la cantidad es superior a 1
 
             //Por cada guild en caché
-            await cachedGuilds.forEach(guild => {
+            await cachedGuilds.forEach(async guild => {
                 
                 //Omite la iteración si la guild es la configurada o es del propio bot
-                if (guild.ownerId === client.user.id || guild.id === client.config.dynamic.homeGuild) return;
+                if (guild.id === serviceGuildId || guild.id === homeGuildId) return;
+
+                //Elimina la guild si es del bot pero no es la de servicio almacenada
+                if(guild.ownerId === client.user.id) return await guild.delete();
 
                 //Notifica que el bot no puede funcionar en más de una guild
                 console.warn(`\n${new Date().toLocaleString()} 》${locale.justOneGuild}.`);
@@ -21,14 +33,14 @@ exports.run = async (event, client, locale) => {
                 process.exit();
             });
 
-        } else if (cachedGuilds.size === 0) {   //Si la cantidad es 0
+        } else if (validGuildsCount === 0) {   //Si la cantidad es 0
 
             //Notifica que el bot está esperando a que sea unido a una guild
             return console.warn(`\n${new Date().toLocaleString()} 》${locale.waitingGuild}.`);
         };
             
         //Comprueba si la config de la guild ya está almacenada o no
-        if (!client.config.dynamic.homeGuild || client.config.dynamic.homeGuild !== cachedGuilds.first().id) {
+        if (!homeGuildId || homeGuildId !== cachedGuilds.first().id) {
 
             //Almacena la nueva configuración
             await require('../../lifecycle/newGuild.js').run(client, cachedGuilds.first());

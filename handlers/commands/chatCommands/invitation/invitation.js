b3447b07123506cb1d8798431d8faab9a6d544fd
Daniel Perales Mauriz <dperales555@gmail.com>
2023-04-12 00:13:27 +0200

    Invitation command improved
    The algorithm of the "invitation" command is improved.

--- handlers/commands/chatCommands/invitation/invitation.js
+++ handlers/commands/chatCommands/invitation/invitation.js
@@ -2,28 +2,31 @@ exports.run = async (interaction, commandConfig, locale) => {
     
     try {
 
-        //Almacena el código de invitación
-        let foundInvite;
+        //Almacena el código de invitación que hay en la BD
+        let inviteCode = await client.functions.db.getConfig('system.inviteCode');
 
-        //Función para crear una invitación
+        //Función para crear una invitación en cualquier canal disponible
         async function createInvite() {
 
             //Almacena el canal para crear la invitación
-            let inviteChannel;
+            let targetChannel;
+
+            //Almacena si faltan permisos en el canal de reglas
+            const missingPermissionOnRulesChannel = await client.functions.utilities.missingPermissions(client.baseGuild.rulesChannel, client.baseGuild.me, ['CREATE_INSTANT_INVITE'], true)
             
             //Comprueba si hay canal de reglas y si se tiene permiso para crear la invitación
-            if (client.baseGuild.rulesChannel && !(await client.functions.utilities.missingPermissions(client.baseGuild.rulesChannel, client.user, ['CREATE_INSTANT_INVITE'], true))) {
+            if (client.baseGuild.rulesChannel && !missingPermissionOnRulesChannel) {
 
                 //Almacena el canal de reglas
-                inviteChannel = client.baseGuild.rulesChannel;
+                targetChannel = client.baseGuild.rulesChannel;
 
             } else {
 
                 //De lo contrario, hace lo propio con el primer canal que lo permita
-                await client.baseGuild.channels.fetch().then( async channels => {
+                await client.baseGuild.channels.fetch().then(async channels => {
 
                     //Filtra los canales para que solo se incluyan los de texto
-                    channels = await channels.filter(channel => channel.type === 'GUILD_TEXT')
+                    channels = await channels.filter(channel => channel.type === 'GUILD_TEXT');
 
                     //Hace un mapa de las IDs de los canales
                     const channelIds = channels.map(channel => channel.id);
@@ -34,58 +37,83 @@ exports.run = async (interaction, commandConfig, locale) => {
                         //Obtiene el canal en base a la ID de la lista
                         const channel = channels.get(channelIds[index]);
 
+                        //Almacena si faltan permisos en el canal
+                        const missingPermission = await client.functions.utilities.missingPermissions(channel, client.baseGuild.me, ['CREATE_INSTANT_INVITE'], true);
+
                         //Si tiene permisos, graba la invitación
-                        if(!await client.functions.utilities.missingPermissions(channel, client.user, ['CREATE_INSTANT_INVITE'], true)) return inviteChannel = channel;
+                        if(!missingPermission) return targetChannel = channel;
                     };
                 });
             };
 
+            //Si no se encontró un canal válido
+            if (!targetChannel) {
+
+                //Muestra un error al usuario
+                await interaction.reply({ embeds: [new client.MessageEmbed()
+                    .setColor(`${await client.functions.db.getConfig('colors.error')}`)
+                    .setDescription(`${client.customEmojis.redTick} ${locale.noTargetChannel}`)
+                ]});
+
+                //Devuelve un estado erróneo
+                return false;
+            };
+
             //Crea una invitación permanente en el canal
-            await inviteChannel.createInvite({maxAge: 0, reason: await client.functions.utilities.parseLocale(locale.inviteReason, { botTag: client.user.tag })}).then(async invite => { foundInvite = invite.code; });
+            await targetChannel.createInvite({maxAge: 0, reason: await client.functions.utilities.parseLocale(locale.inviteReason, { botTag: client.user.tag })}).then(async invite => { inviteCode = invite.code; });
 
             //Graba la invitación en la BD
-            await client.functions.db.setConfig('system.inviteCode', foundInvite);
+            await client.functions.db.setConfig('system.inviteCode', inviteCode);
+
+            //Retorna la invitación
+            return inviteCode;
         };
 
-        //Almacena la invitación obtenida
-        let obtainedInvite;
-
-        //Almacena el código de invitación
-        const inviteCode = await client.functions.db.getConfig('system.inviteCode');
-
-        //Si no hay una invitación grabada
+        //Si no hay una invitación grabada en la BD
         if (!inviteCode) {
 
-            //Comprueba si ya existe una invitación
+            //Almacena todas las invitaciones de la guild
             await client.baseGuild.invites.fetch().then(invites => {
+
+                //Por cada una de las invitaciones
                 invites.forEach(async invite => {
-                    if (invite.inviter === client.user) foundInvite = invite.code;
+
+                    //Si es del bot
+                    if (invite.inviter === client.user) {
+
+                        //Almacena el código de la invitación
+                        inviteCode = invite.code;
+
+                        //Graba la invitación en la BD, si se llegó a crear
+                        await client.functions.db.setConfig('system.inviteCode', inviteCode);
+                    };
                 });
             });
 
             //Crea la invitación si no existe
-            if (!foundInvite) await createInvite();
-
-            //Devuelve la URL, si se puedo obtener un código
-            if (inviteCode) obtainedInvite = `https://discord.gg/${inviteCode}`;
+            if (!inviteCode) inviteCode = await createInvite();
 
         } else {
-            //Busca la invitación
-            const invite = await client.baseGuild.invites.resolve(inviteCode);
 
-            //Crea la invitación si no existe
-            if (!invite) await createInvite();
+            //Busca la invitación existente
+            const existingInvite = await client.baseGuild.invites.resolve(inviteCode);
 
-            //Devuelve la URL, si se puedo obtener un código
-            obtainedInvite = `https://discord.gg/${inviteCode}`;
+            //Crea la invitación si ya no existe
+            if (!existingInvite) inviteCode = await createInvite();
         };
+
+        //Aborta el resto del comando si no se pudo generar una invitación
+        if (!inviteCode) return;
+
+        //Almacena la URL de la invitación
+        const resultInviteURL = `https://discord.gg/${inviteCode}`;
 
         //Genera el embed y lo envía, buscando el contenido necesario con la función
         await interaction.reply({ embeds: [new client.MessageEmbed()
             .setColor(`${await client.functions.db.getConfig('colors.primary')}`)
-            .setAuthor({ name: locale.embed.author, iconURL: client.baseGuild.iconURL({dynamic: true}) })
-            .setDescription(obtainedInvite)
-            .setFooter({ text: `${locale.embed.footer}.` })
+            .setAuthor({ name: locale.resultEmbed.author, iconURL: client.baseGuild.iconURL({dynamic: true}) })
+            .setDescription(resultInviteURL)
+            .setFooter({ text: `${locale.resultEmbed.footer}.` })
         ]});
 
     } catch (error) {

b363b1acb6834340aa83dc86a3b3577746e74300
Daniel Perales Mauriz <dperales555@gmail.com>
2023-04-12 02:53:00 +0200

    Fixed "edit" interaction expiration
    Fixed an issue where the "edit" command interaction would time out if the message had to be searched in all channels.

--- handlers/commands/chatCommands/edit/edit.js
+++ handlers/commands/chatCommands/edit/edit.js
@@ -3,16 +3,13 @@ exports.run = async (interaction, commandConfig, locale) => {
     try {
             
         //Busca el canal en la guild
-        const channel = interaction.options._hoistedOptions[1] ? await client.functions.utilities.fetch('channel', interaction.options._hoistedOptions[1].value) : null;
+        const channel = await client.functions.utilities.fetch('channel', interaction.options._hoistedOptions[1] ? interaction.options._hoistedOptions[1].value : interaction.channelId);
 
         //Devuelve un error si no se ha encontrado el canal
         if (interaction.options._hoistedOptions[1] && !channel) return interaction.reply({ embeds: [ new client.MessageEmbed()
             .setColor(`${await client.functions.db.getConfig('colors.error')}`)
             .setDescription(`${client.customEmojis.redTick} ${await client.functions.utilities.parseLocale(locale.noChannel, { id: interaction.options._hoistedOptions[1].value })}.`)
         ], ephemeral: true});
-
-        //Pospone la respuesta del bot si debe buscar el canal
-        if (!channel) await interaction.deferReply();
         
         //Busca el mensaje en el canal
         const msg = await client.functions.utilities.fetch('message', interaction.options._hoistedOptions[0].value, channel);

a88be540880ffc86841144163dd2e2784fd58131
Daniel Perales Mauriz <dperales555@gmail.com>
2023-09-13 00:26:05 +0200

    Fixed guild count error
    Fixed an issue where valid guilds were not being properly counted in the guildDelete event handler.

--- handlers/events/guildDelete.js
+++ handlers/events/guildDelete.js
@@ -11,9 +11,18 @@ export default async (guild, locale) => {
 
         //Carga el listado de guilds a las que el bot está unido
         const cachedGuilds = client.guilds.cache;
+
+        //Carga el ID de la guild de servicio
+        const serviceGuildId = await client.functions.db.getConfig('system.serviceGuildId');
+
+        //Almacena el número de guilds elegibles (descontando las de servicio)
+        const elegibleGuilds = await cachedGuilds.filter(guild => guild.id !== serviceGuildId);
+
+        //Almacena el recuento de guilds elegibles
+        let elegibleGuildsCount = elegibleGuilds.size;
         
         //Si no hay guilds
-        if (cachedGuilds.size === 0) {
+        if (elegibleGuildsCount === 0) {
 
             //Notifica que se debe volver a iniciar al bot
             logger.warn('The bot must be joined to a guild in order to work, so the program will finish and needs to be started again');

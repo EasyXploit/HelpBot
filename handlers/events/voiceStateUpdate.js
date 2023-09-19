f3922ab68456097eeb6eec2a3a985ea6b5a01ea9
Daniel Perales Mauriz <dperales555@gmail.com>
2023-09-19 21:40:18 +0200

    Translated comments
    Comments from some parts of the repository have been translated into English.

--- handlers/events/voiceStateUpdate.js
+++ handlers/events/voiceStateUpdate.js
@@ -1,48 +1,48 @@
-//Exporta la función de manejo del evento
+// Exports the event management function
 export default async (oldState, newState, locale) => {
     
     try {
 
-        //Comprueba si el bot está listo para manejar eventos
+        // Checks if the bot is ready to handle events
         if (!global.readyStatus) return;
 
-        //Aborta si no es un evento de la guild registrada
+        // Aborts if it is not an event from the base guild
         if (oldState.guild.id !== client.baseGuild.id) return;
 
-        //Almacena los canales excluidos del registro de cambios de estado de voz
+        // Stores the excluded channels from the records of voice state changes
         const excludedChannels = await client.functions.db.getConfig('moderation.voiceMovesExcludedChannels');
 
-        //Si no ocurrió un cambio entre canales ignorados
+        // If a change between ignored channels did not occur
         voiceMovesIf: if (!(excludedChannels.includes(oldState.channelId) && excludedChannels.includes(newState.channelId))) {
 
-            //Omite si solo se trata de un cambio que no implique cambio de canal
+            // Omits if it is only a change that does not imply channel change
             if (oldState.channelId === newState.channelId) break voiceMovesIf;
 
-            //Omite si el miembro se conecta o se desconecta de un canal excluido
+            // Omits if the member connects or disconnects from an excluded channel
             if (!oldState.channelId && excludedChannels.includes(newState.channelId)) break voiceMovesIf;
             if (!newState.channelId && excludedChannels.includes(oldState.channelId)) break voiceMovesIf;
 
-            //Almacena los campos de anterior y nuevo canal, ofuscando los canales ignorados
+            // Stores the fields of previous and new channel, obfuscating the ignored channels
             const oldChannel = oldState.channelId && !excludedChannels.includes(oldState.channelId) ? `<#${oldState.channel.id}>` : `\`${await client.functions.utils.parseLocale(locale.voiceMovesLogging.noChannel)}\``;
             const newChannel = newState.channelId && !excludedChannels.includes(newState.channelId) ? `<#${newState.channel.id}>` : `\`${await client.functions.utils.parseLocale(locale.voiceMovesLogging.noChannel)}\``;
 
-            //Genera los campos de anterior y nuevo canal para el embed de registros
+            // Generates the fields of previous and new channel for the embed of records
             let embedFields = [
                 { name: await client.functions.utils.parseLocale(locale.voiceMovesLogging.oldChannel), value: oldChannel, inline: true },
                 { name: await client.functions.utils.parseLocale(locale.voiceMovesLogging.newChannel), value: newChannel, inline: true }
             ];
 
-            //Si hay un nuevo canal para el miemmbro y este no está ignorado
+            // If there is a new channel for the member and this is not ignored
             if (newState.channelId && !excludedChannels.includes(newState.channelId)) {
 
-                //Genera y almacena un array con los tags de los miembros de dicho canal
+                // Generates and stores an array with the tags of the members of that channel
                 const channelMembers = Array.from(newState.channel.members, member => newState.channel.members.get(member[0]));
 
-                //Añade un bloque de código con los tags de los miembros del canal al embed de registro
+                // Adds a block of code with the tags of the channel members to the records embed
                 embedFields.push({ name: `${await client.functions.utils.parseLocale(locale.voiceMovesLogging.actualMembers)} (${newState.channel.members.size}/${newState.channel.userLimit != 0 ? newState.channel.userLimit : '∞'})`, value: `${channelMembers.join(', ')}`});
             };
             
-            //Se formatea y envía un registro al canal especificado en la configuración
+            //  Formats and sends a record to the channel specified in the configuration
             if (newState.member) await client.functions.managers.sendLog('voiceMoves', 'embed', new discord.EmbedBuilder()
                 .setColor(`${await client.functions.db.getConfig('colors.logging')}`)
                 .setAuthor({ name: await client.functions.utils.parseLocale(locale.voiceMovesLogging.embedAuthor, { memberTag: newState.member.user.tag }), iconURL: newState.member.user.displayAvatarURL() })
@@ -51,79 +51,79 @@ export default async (oldState, newState, locale) => {
             );
         };
 
-        //Aborta el resto de la ejecución si no están habilitadas las recompensas de XP
+        // Aborts the rest of the execution if XP rewards are not enabled
         if (!await client.functions.db.getConfig('leveling.rewardVoice')) return;
 
-        //Crea un objeto en el cliente para los estados de voz de los usuarios
+        // Creates an object in the client for user's voice
         if (!client.usersVoiceStates) client.usersVoiceStates = {};
 
-        //Función para que el miembro deje de ganar XP
+        // Function for the member to stop gaining XP
         async function endVoiceTime() {
 
-            //Almacena el intervalo de ganancia de puntos de EXP
+            // Stores the gain interval of XP
             const XPGainInterval = await client.functions.db.getConfig('leveling.XPGainInterval');
 
-            //Si el timestamp actual es superior a los MS de intervalo de ganancia de XP configurado
+            // If the current timestamp is superior to the MS of the gain interval of XP configured
             if (client.usersVoiceStates[newState.id] && Date.now() > (client.usersVoiceStates[newState.id].lastXpReward + XPGainInterval)) {
 
-                //Almacena al miembro
+                // Stores the member
                 const member = await client.functions.utils.fetch('member', newState.id);
 
-                //Si el miembro está timeouteado o ensordecido, no hace nada
+                // If the member is timeouted or deafen, does nothing
                 if (!member || member.voice.mute || member.voice.deaf) return;
 
-                //Almacena el canal
+                // Stores the channel
                 const channel = await client.functions.utils.fetch('channel', oldState.channelId);
 
-                //Añade XP al miembro por última vez
+                // Adds XP to the member for the last time
                 await client.functions.leveling.addExperience(member, 'voice', channel);
 
-                //Almacena el perfil del miembro, o lo crea
+                // Stores the member's profile, or creates it
                 let memberProfile = await client.functions.db.getData('profile', member.id) || await client.functions.db.genData('profile', { userId: member.id });
 
-                //Actualiza el tiempo de voz del miembro
+                // Updates the voice time of the member
                 memberProfile.stats.aproxVoiceTime += XPGainInterval;
 
-                //Guarda las nuevas estadísticas del miembro en la base de datos
+                // Saves the new statistics of the member in the database
                 await client.functions.db.setData('profile', member.id, memberProfile);
             };
             
-            //Borra el registro del miembro que ha dejado el canal de voz
+            // Deletes the record of the member who has left the voice channel
             delete client.usersVoiceStates[newState.id];
         };
 
-        //Si hay una nueva conexión o una antigua cambia
+        // If there is a new connection or an old changes
         if (newState.channel && newState.channel !== null && newState.channelId !== null) {
 
-            //Almacena el miembro
+            // Stores the member
             const member = newState.member;
 
-            //Acaba el conteo de minutos si el miembro se queda solo o con únicamente bots en la sala
+            // The minutes count ends if the member is left alone or with only bots in the room
             if (newState.channel.members.filter(member => !member.user.bot).size === 1 && client.usersVoiceStates[newState.id]) return endVoiceTime();
 
-            //Si es un bot, el canal de AFK, un canal prohibido o un rol prohibido
+            // If it is a bot, the AFK channel, a forbidden channel or a prohibited role
             if (member.user.bot || (newState.guild.afkChannelId && newState.channelId === newState.guild.afkChannelId)) {
 
-                //Si el miembro tenía un estado de voz almacenado
+                // If the member had a stored voice state
                 if (client.usersVoiceStates[newState.id]) {
 
-                    //Borra el registro del miembro que ha dejado el canal de voz
+                    // Deletes the record of the member who has left the voice channel
                     delete client.usersVoiceStates[newState.id];
                 };
 
-                //Aborta el resto del programa
+                // Aborts the rest of the program
                 return;
             };
 
-            //Si solo se ha cambiado de sala
+            // If has only changed rooms
             if (client.usersVoiceStates[newState.id]) {
 
-                //Cambia el ID del canal actual
+                // Changes the current channel ID
                 client.usersVoiceStates[newState.id].channelID = newState.channelId
 
-            } else { //Si se ha conectado desde 0
+            } else { // If it has been connected from 0
 
-                //Crea una nueva entrada en la lista de estados de voz
+                // Creates a new entry into the list of voice states
                 client.usersVoiceStates[newState.id] = {
                     guild: newState.guild.id,
                     channelID: newState.channelId,
@@ -131,15 +131,15 @@ export default async (oldState, newState, locale) => {
                 };
             };
 
-        } else if (!newState.channel || newState.channelId == null || newState.channel == null) { //Si la conexión desaparece
+        } else if (!newState.channel || newState.channelId == null || newState.channel == null) { // If the connection disappears
 
-            //Finbaliza el intervalo de ganancia de XP para el miembro
+            // Finishes the XP gain interval for the member
             endVoiceTime();
         };
 
     } catch (error) {
 
-        //Ejecuta el manejador de errores
+        // Executes the error handler
         await client.functions.managers.eventError(error);
     };
 };
--- handlers/events/warn.js
+++ handlers/events/warn.js
@@ -1,6 +1,6 @@
-//Exporta la función de manejo del evento
+// Exports the event management function
 export default (warn, locale) => {
 
-    //Muestra un aviso en la consola
+    // Shows a notice in the console
     logger.warn(warn);
 };

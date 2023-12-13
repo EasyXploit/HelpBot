15012a2ce9412a862e62d5124d131b6052569245
Daniel Perales <dperales555@gmail.com>
2023-12-13 20:33:35 +0100

    messageUpdate handling error fiz
    Fixed a problem that occurred when the author field is not present in the new message after editing.

--- handlers/events/messageUpdate.js
+++ handlers/events/messageUpdate.js
@@ -4,7 +4,7 @@ exports.run = async (oldMessage, newMessage, locale) => {
     if (newMessage.guild && newMessage.guild.id !== client.homeGuild.id) return;
 
     //Previene la ejecución si el mensaje fue enviado por un bot o por el sistema
-    if (newMessage.author.bot || newMessage.type !== 'DEFAULT') return;
+    if (!newMessage.author || newMessage.author.bot || newMessage.type !== 'DEFAULT') return;
 
     //Aborta si no se modificó el contenido del mensaje
     if(oldMessage.content === newMessage.content) return;
